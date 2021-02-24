gnostr-org/gnostr

commit 630d737de91484810a458c9eaf6703a4378b5c75

fix some potentials for errors to bubble up  (#547)
* fix some potentials for errors to bubble up (#490)
* fix async diff failing panic

asyncgit/src/diff.rs
@@ -118,8 +118,14 @@ impl AsyncDiff {
                 arc_last,
                 arc_current,
                 hash,
-            )
-            .expect("error getting diff");
+            );
+
+            let notify = if let Err(err) = notify {
+                log::error!("get_diff_helper error: {}", err);
+                true
+            } else {
+                false
+            };
 
             arc_pending.fetch_sub(1, Ordering::Relaxed);
 

src/components/diff.rs
@@ -518,6 +518,16 @@ impl DiffComponent {
         );
     }
 
+    fn stage_unstage_hunk(&mut self) -> Result<()> {
+        if self.current.is_stage {
+            self.unstage_hunk()?;
+        } else {
+            self.stage_hunk()?;
+        }
+
+        Ok(())
+    }
+
     const fn is_stage(&self) -> bool {
         self.current.is_stage
     }
@@ -659,11 +669,12 @@ impl Component for DiffComponent {
                 } else if e == self.key_config.enter
                     && !self.is_immutable
                 {
-                    if self.current.is_stage {
-                        self.unstage_hunk()?;
-                    } else {
-                        self.stage_hunk()?;
-                    }
+                    try_or_popup!(
+                        self,
+                        "hunk error:",
+                        self.stage_unstage_hunk()
+                    );
+
                     Ok(true)
                 } else if e == self.key_config.status_reset_item
                     && !self.is_immutable

Makefile
@@ -5,7 +5,7 @@ profile:
 	cargo run --features=timing,pprof -- -l
 
 debug:
-	cargo run --features=timing -- -l
+	RUST_BACKTRACE=true cargo run --features=timing -- -l
 
 build-release:
 	cargo build --release

commit deb3265eeeb181aec0244af62e69cdaf3f33a66b

fix regression in async diff (introduced in 630d737)

asyncgit/src/diff.rs
@@ -120,11 +120,12 @@ impl AsyncDiff {
                 hash,
             );
 
-            let notify = if let Err(err) = notify {
-                log::error!("get_diff_helper error: {}", err);
-                true
-            } else {
-                false
+            let notify = match notify {
+                Err(err) => {
+                    log::error!("get_diff_helper error: {}", err);
+                    true
+                }
+                Ok(notify) => notify,
             };
 
             arc_pending.fetch_sub(1, Ordering::Relaxed);