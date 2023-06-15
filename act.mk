50d5225de84e0fab0dfd8b731a01a6ff047eaa3e
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-06-15 12:09:28 -0400

    make: act - remove alpine.yml
    deleted:    .github/workflows/alpine.yml

--- act.mk
+++ act.mk
@@ -1,7 +1,5 @@
 #NOTE: using -C for container context
 #The action is run on the submodule .github as an example
-alpine:docker-start## 	run act in .github
-	@export $(cat ~/gh_token.txt) && act -vbr  -W $(PWD)/.github/workflows/$@.yml
 ubuntu-matrix:docker-start## 	run act in .github
 	#cant use -r reuse - blocks apt-get install
 	@export $(cat ~/gh_token.txt) && act -vbr  -W $(PWD)/.github/workflows/$@.yml
