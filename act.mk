922fc00c9d0cae63be5216a5c35df51626738ce3
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-06-15 18:35:06 -0400

    .github/workflows/ubuntu-nostcat.yml: working with cache/restore

--- act.mk
+++ act.mk
@@ -8,6 +8,10 @@ ubuntu-jq:submodules docker-start## 	run act in .github
 	#we use -b to bind the repo to the act container
 	#in the single dep instances we reuse (-r) the container
 	@export $(cat ~/gh_token.txt) && act -vbr  -W $(PWD)/.github/workflows/$@.yml
+ubuntu-nostcat:submodules docker-start## 	run act in .github
+	#we use -b to bind the repo to the act container
+	#in the single dep instances we reuse (-r) the container
+	@export $(cat ~/gh_token.txt) && act -vbr  -W $(PWD)/.github/workflows/$@.yml
 
 	#the matrix/pre/release builds are for the resulting app builds
 ubuntu-matrix:submodules docker-start## 	run act in .github
