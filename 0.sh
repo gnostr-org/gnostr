c95b2f663fb5de12e2041b4c93ebeff0f9cc411f
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-06-18 11:51:44 -0400

    0.sh: gnostr tag

--- 0.sh
+++ 0.sh
@@ -4,9 +4,9 @@ MESSAGE_HASH=$(nostril --hash $1)
 export $MESSAGE_HASH
 
 fi
-nostril --sec $(nostril --hash 0) --created-at $(date +%s) --envelope --content "$MESSAGE_HASH:$1"
+nostril --sec $(nostril --hash 0) --created-at $(date +%s) --tag gnostr gnostr --envelope --content "$MESSAGE_HASH:$1"
 if hash nostcat; then
-nostril --sec $(nostril --hash 0) --created-at $(date +%s) --envelope --content "$MESSAGE_HASH:$1" | nostcat -u wss://relay.damus.io
+nostril --sec $(nostril --hash 0) --created-at $(date +%s) --tag gnostr gnostr --envelope --content "$MESSAGE_HASH:$1" | nostcat -u wss://relay.damus.io
 else
 	echo "try:\nmake nostcat"
 fi
