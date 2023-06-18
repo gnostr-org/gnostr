b6e135c183a2c32ee11db4a5401623edcb5d1e8c
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-06-18 11:40:46 -0400

    0.sh: basic test using nostril --hash 0

--- 0.sh
+++ 0.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
-nostril --sec $(nostril --hash 0) --created-at 0000000000 --envelope --content "0"
+if hash nostril; then
+MESSAGE_HASH=$(nostril --hash $1)
+export $MESSAGE_HASH
+
+fi
+nostril --sec $(nostril --hash 0) --created-at $(date +%s) --envelope --content "$MESSAGE_HASH:$1"
+if hash nostcat; then
+nostril --sec $(nostril --hash 0) --created-at $(date +%s) --envelope --content "$MESSAGE_HASH:$1" | nostcat -u wss://relay.damus.io
+else
+	echo "try:\nmake nostcat"
+fi
+
+nostril --hash 0
