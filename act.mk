543248cdabade9a632bd2ec61b0b53f5d151439b
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-06-19 19:34:23 -0400

    act.mk: change context to deps/nostcat
    .github/workflows/ubuntu-nostcat.yml

--- act.mk
+++ act.mk
@@ -11,7 +11,8 @@ ubuntu-jq:submodules docker-start## 	run act in .github
 ubuntu-nostcat:submodules docker-start## 	run act in .github
 	#we use -b to bind the repo to the act container
 	#in the single dep instances we reuse (-r) the container
-	@export $(cat ~/gh_token.txt) && act -vbr  -W $(PWD)/.github/workflows/$@.yml
+	@export $(cat ~/gh_token.txt) && act -vbr -C deps/nostcat \
+		-W $(PWD)/.github/workflows/$@.yml
 
 	#the matrix/pre/release builds are for the resulting app builds
 ubuntu-matrix:initialize docker-start## 	run act in .github
