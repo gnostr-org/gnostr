bda5c1f5416076f34cd52ad77abdd10049a53676
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-01-15 20:55:00 -0500

    build.rs:build_bins_command

--- build.rs
+++ build.rs
@@ -4,6 +4,39 @@ use std::process::Command;
 
 fn main() {
 
+    //build bins
+	let build_bins_command = if cfg!(target_os = "windows") {
+		Command::new("cmd")
+			.args(["/C", "cargo", "install", "--path", "bins"])
+			.output()
+			.expect("failed to execute process")
+	} else if cfg!(target_os = "macos") {
+		Command::new("sh")
+			.arg("-c")
+			.args(["cargo", "install", "--path", "bins"])
+			.output()
+			.expect("failed to execute process")
+	} else if cfg!(target_os = "linux") {
+		Command::new("sh")
+			.arg("-c")
+			.args(["cargo", "install", "--path", "bins"])
+			.output()
+			.expect("failed to execute process")
+	} else {
+		Command::new("sh")
+			.arg("-c")
+			.args(["cargo", "install", "--path", "bins"])
+			.output()
+			.expect("failed to execute process")
+	};
+
+	let _build_bins = String::from_utf8(build_bins_command.stdout)
+		.map_err(|non_utf8| {
+			String::from_utf8_lossy(non_utf8.as_bytes()).into_owned()
+		})
+		.unwrap();
+
+    //build tui
 	let build_tui_command = if cfg!(target_os = "windows") {
 		Command::new("cmd")
 			.args(["/C", "cargo", "install", "--path", "tui"])
