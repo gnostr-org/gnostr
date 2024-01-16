388db3aff480b0b44dbe78cc41f6e30c4321c599
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-01-15 21:32:29 -0500

    build.rs

--- build.rs
+++ build.rs
@@ -11,26 +11,52 @@ fn main() {
 			.output()
 			.expect("failed to execute process")
 	} else if cfg!(target_os = "macos") {
-		Command::new("sh")
-			.arg("-c")
-			.args(["cargo", "install", "--path", "bins"])
+		Command::new("cargo")
+			.args(["install", "--path", "bins"])
 			.output()
 			.expect("failed to execute process")
 	} else if cfg!(target_os = "linux") {
-		Command::new("sh")
-			.arg("-c")
-			.args(["cargo", "install", "--path", "bins"])
+		Command::new("cargo")
+			.args(["install", "--path", "bins"])
 			.output()
 			.expect("failed to execute process")
 	} else {
-		Command::new("sh")
-			.arg("-c")
-			.args(["cargo", "install", "--path", "bins"])
+		Command::new("cargo")
+			.args(["install", "--path", "bins"])
 			.output()
 			.expect("failed to execute process")
 	};
 
 	let _build_bins = String::from_utf8(build_bins_command.stdout)
+		.map_err(|non_utf8| {
+			String::from_utf8_lossy(non_utf8.as_bytes()).into_owned()
+		})
+		.unwrap();
+
+    //build bits
+	let build_bits_command = if cfg!(target_os = "windows") {
+		Command::new("cmd")
+			.args(["/C", "cargo", "install", "--path", "bits/crates/rqbit"])
+			.output()
+			.expect("failed to execute process")
+	} else if cfg!(target_os = "macos") {
+		Command::new("cargo")
+			.args(["install", "--path", "bits/crates/rqbit"])
+			.output()
+			.expect("failed to execute process")
+	} else if cfg!(target_os = "linux") {
+		Command::new("cargo")
+			.args(["install", "--path", "bits/crates/rqbit"])
+			.output()
+			.expect("failed to execute process")
+	} else {
+		Command::new("cargo")
+			.args(["install", "--path", "bits/crates/rqbit"])
+			.output()
+			.expect("failed to execute process")
+	};
+
+	let _build_bits = String::from_utf8(build_bits_command.stdout)
 		.map_err(|non_utf8| {
 			String::from_utf8_lossy(non_utf8.as_bytes()).into_owned()
 		})
@@ -43,21 +69,18 @@ fn main() {
 			.output()
 			.expect("failed to execute process")
 	} else if cfg!(target_os = "macos") {
-		Command::new("sh")
-			.arg("-c")
-			.args(["cargo", "install", "--path", "tui"])
+		Command::new("cargo")
+			.args(["install", "--path", "tui"])
 			.output()
 			.expect("failed to execute process")
 	} else if cfg!(target_os = "linux") {
-		Command::new("sh")
-			.arg("-c")
-			.args(["cargo", "install", "--path", "tui"])
+		Command::new("cargo")
+			.args(["install", "--path", "tui"])
 			.output()
 			.expect("failed to execute process")
 	} else {
-		Command::new("sh")
-			.arg("-c")
-			.args(["cargo", "install", "--path", "tui"])
+		Command::new("cargo")
+			.args(["install", "--path", "tui"])
 			.output()
 			.expect("failed to execute process")
 	};
@@ -75,15 +98,13 @@ fn main() {
 			.output()
 			.expect("failed to execute process")
 	} else if cfg!(target_os = "macos") {
-		Command::new("sh")
-			.arg("-c")
-			.args(["cargo", "install", "--path", "modal"])
+		Command::new("cargo")
+			.args(["install", "--path", "modal"])
 			.output()
 			.expect("failed to execute process")
 	} else if cfg!(target_os = "linux") {
-		Command::new("sh")
-			.arg("-c")
-			.args(["cargo", "install", "--path", "modal"])
+		Command::new("cargo")
+			.args(["install", "--path", "modal"])
 			.output()
 			.expect("failed to execute process")
 	} else {
