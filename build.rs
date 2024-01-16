593f0abdea53b6770867371e0d506768b5bb9f1e
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-01-15 20:55:27 -0500

    build.rs:build_tui_command

--- build.rs
+++ build.rs
@@ -67,5 +67,40 @@ fn main() {
 			String::from_utf8_lossy(non_utf8.as_bytes()).into_owned()
 		})
 		.unwrap();
+
+    //build modal
+	let build_modal_command = if cfg!(target_os = "windows") {
+		Command::new("cmd")
+			.args(["/C", "cargo", "install", "--path", "modal"])
+			.output()
+			.expect("failed to execute process")
+	} else if cfg!(target_os = "macos") {
+		Command::new("sh")
+			.arg("-c")
+			.args(["cargo", "install", "--path", "modal"])
+			.output()
+			.expect("failed to execute process")
+	} else if cfg!(target_os = "linux") {
+		Command::new("sh")
+			.arg("-c")
+			.args(["cargo", "install", "--path", "modal"])
+			.output()
+			.expect("failed to execute process")
+	} else {
+		Command::new("sh")
+			.arg("-c")
+			.args(["cargo", "install", "--path", "modal"])
+			.output()
+			.expect("failed to execute process")
+	};
+
+	let _build_modal = String::from_utf8(build_modal_command.stdout)
+		.map_err(|non_utf8| {
+			String::from_utf8_lossy(non_utf8.as_bytes()).into_owned()
+		})
+		.unwrap();
+
+
+
 }
 
