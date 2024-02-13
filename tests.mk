4008e846e5981addd158aaea5cb0d24743ada4de
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-02-13 12:10:09 -0500

    tests.mk:report CASE 1 and CASE 2

--- tests.mk
+++ tests.mk
@@ -1,3 +1,7 @@
 gnostr-verify-keypair:gnostr gnostr-sha256 gnostr-install
+##CASE 1
+	echo CASE 1
 	gnostr-verify-keypair $(shell gnostr --sec $(shell gnostr-sha256 $(shell gnostr-weeble)) | jq .pubkey | sed 's/\"//g') $(shell gnostr-sha256 $(shell gnostr-weeble)) || $(MAKE) bins
+##CASE 2
+	echo CASE 2
 	gnostr-verify-keypair $(shell gnostr --sec $(shell gnostr --hash $(shell gnostr-weeble)) | jq .pubkey | sed 's/\"//g') $(shell gnostr-sha256 $(shell gnostr-weeble)) || $(MAKE) bins
