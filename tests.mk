aa1a5d3f7b95b209624d4350393cd52fa9db6a26
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-02-13 11:36:33 -0500

    tests.mk:gnostr-verify-keys:gnostr-sha256

--- tests.mk
+++ tests.mk
@@ -1,2 +1,2 @@
-gnostr-verify-keys:gnostr gnostr-install
+gnostr-verify-keys:gnostr gnostr-sha256 gnostr-install
 	gnostr-verify-keypair $(shell gnostr --sec $(shell gnostr-sha256 $(shell gnostr-weeble)) | jq .pubkey | sed 's/\"//g') $(shell gnostr-sha256 $(shell gnostr-weeble)) || $(MAKE) bins
