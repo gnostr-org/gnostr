c1fa00fb27e6cd3657945c565abc6e4f70eb038a
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-02-13 12:06:42 -0500

    tests.mk:gnostr-verifiy-keypairs:
    gnostr --hash test against gnostr-sha256

--- tests.mk
+++ tests.mk
@@ -1,2 +1,3 @@
 gnostr-verify-keypair:gnostr gnostr-sha256 gnostr-install
 	gnostr-verify-keypair $(shell gnostr --sec $(shell gnostr-sha256 $(shell gnostr-weeble)) | jq .pubkey | sed 's/\"//g') $(shell gnostr-sha256 $(shell gnostr-weeble)) || $(MAKE) bins
+	gnostr-verify-keypair $(shell gnostr --sec $(shell gnostr --hash $(shell gnostr-weeble)) | jq .pubkey | sed 's/\"//g') $(shell gnostr-sha256 $(shell gnostr-weeble)) || $(MAKE) bins
