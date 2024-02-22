gnostr-org/gnostr

commit 5c0552a7c18991973de97338355f2b0c8f0b322d

fix: applying commits that exist in other branches
previously these commits would be skipped

src/git.rs
@@ -370,13 +370,16 @@ impl RepoActions for Repo {
         branch_name: &str,
         patch_and_ancestors: Vec<nostr::Event>,
     ) -> Result<Vec<nostr::Event>> {
-        // filter out existing ancestors
+        let branch_tip = self.get_tip_of_local_branch(branch_name)?;
+        // filter out existing ancestors in branch
         let mut patches_to_apply: Vec<nostr::Event> = patch_and_ancestors
             .into_iter()
             .filter(|e| {
-                !self
-                    .does_commit_exist(&get_commit_id_from_patch(e).unwrap())
-                    .unwrap()
+                let commit_id = get_commit_id_from_patch(e).unwrap();
+                !branch_tip.to_string().eq(&commit_id)
+                    || !self
+                        .ancestor_of(&branch_tip, &str_to_sha1(&commit_id).unwrap())
+                        .unwrap()
             })
             .collect();
 
@@ -395,16 +398,6 @@ impl RepoActions for Repo {
             bail!("cannot find parent commit ({parent_commit_id}). run git pull and try again.")
         }
 
-        // check for rebase or changes
-        if let Ok(current_tip) = self.get_tip_of_local_branch(branch_name) {
-            if !current_tip.to_string().eq(&parent_commit_id) {
-                // TODO: either changes have been made on the local branch or
-                // the latest commit in the prpoosal has rebased onto a newer
-                // commit that you havn't pulled yet ask user
-                // whether they want to proceed
-            }
-        }
-
         // checkout branch
         if !self.get_checked_out_branch_name()?.eq(&branch_name) {
             self.create_branch_at_commit(branch_name, &parent_commit_id)?;
@@ -415,7 +408,13 @@ impl RepoActions for Repo {
         patches_to_apply.reverse();
 
         for patch in &patches_to_apply {
-            apply_patch(self, patch)?;
+            let commit_id = get_commit_id_from_patch(patch)?;
+            // only create new commits - otherwise make them the tip
+            if self.does_commit_exist(&commit_id)? {
+                self.create_branch_at_commit(branch_name, &commit_id)?;
+            } else {
+                apply_patch(self, patch)?;
+            }
         }
         Ok(patches_to_apply)
     }

commit 76cd5b260e8f66fa35fe5581f97a4f80e78cb98e

fix: apply patches error
introduced in
5c0552a7c18991973de97338355f2b0c8f0b322d

src/git.rs
@@ -370,16 +370,21 @@ impl RepoActions for Repo {
         branch_name: &str,
         patch_and_ancestors: Vec<nostr::Event>,
     ) -> Result<Vec<nostr::Event>> {
-        let branch_tip = self.get_tip_of_local_branch(branch_name)?;
+        let branch_tip_result = self.get_tip_of_local_branch(branch_name);
+
         // filter out existing ancestors in branch
         let mut patches_to_apply: Vec<nostr::Event> = patch_and_ancestors
             .into_iter()
             .filter(|e| {
                 let commit_id = get_commit_id_from_patch(e).unwrap();
-                !branch_tip.to_string().eq(&commit_id)
-                    || !self
-                        .ancestor_of(&branch_tip, &str_to_sha1(&commit_id).unwrap())
-                        .unwrap()
+                if let Ok(branch_tip) = branch_tip_result {
+                    !branch_tip.to_string().eq(&commit_id)
+                        && !self
+                            .ancestor_of(&branch_tip, &str_to_sha1(&commit_id).unwrap())
+                            .unwrap()
+                } else {
+                    true
+                }
             })
             .collect();
 
@@ -387,7 +392,8 @@ impl RepoActions for Repo {
             if let Ok(last_patch) = patches_to_apply.last().context("no patches") {
                 last_patch
             } else {
-                self.checkout(branch_name).context("the parent commit of the proposal doesnt exist in your local repoistory. Try a git pull on master first.")?;
+                self.checkout(branch_name)
+                    .context("no patches and so cannot create a proposal branch")?;
                 return Ok(vec![]);
             },
             "parent-commit",
@@ -399,9 +405,7 @@ impl RepoActions for Repo {
         }
 
         // checkout branch
-        if !self.get_checked_out_branch_name()?.eq(&branch_name) {
-            self.create_branch_at_commit(branch_name, &parent_commit_id)?;
-        }
+        self.create_branch_at_commit(branch_name, &parent_commit_id)?;
         self.checkout(branch_name)?;
 
         // apply commits
@@ -1938,6 +1942,99 @@ mod tests {
                 );
                 Ok(())
             }
+        }
+    }
+    mod ancestor_of {
+        use super::*;
+
+        #[test]
+        fn deep_ancestor_returns_true() -> Result<()> {
+            let test_repo = GitTestRepo::default();
+            let from_main_in_feature_history = test_repo.populate()?;
+
+            // create feature branch and add 2 commits
+            test_repo.create_branch("feature")?;
+
+            test_repo.checkout("feature")?;
+            std::fs::write(test_repo.dir.join("t3.md"), "some content")?;
+            test_repo.stage_and_commit("add t3.md")?;
+            std::fs::write(test_repo.dir.join("t4.md"), "some content")?;
+            let ahead_2_oid = test_repo.stage_and_commit("add t4.md")?;
+
+            let git_repo = Repo::from_path(&test_repo.dir)?;
+
+            assert!(git_repo.ancestor_of(
+                &oid_to_sha1(&ahead_2_oid),
+                &oid_to_sha1(&from_main_in_feature_history)
+            )?);
+            Ok(())
+        }
+
+        #[test]
+        fn commit_parent_returns_true() -> Result<()> {
+            let test_repo = GitTestRepo::default();
+            test_repo.populate()?;
+
+            // create feature branch and add 2 commits
+            test_repo.create_branch("feature")?;
+
+            test_repo.checkout("feature")?;
+            std::fs::write(test_repo.dir.join("t3.md"), "some content")?;
+            let ahead_1_oid = test_repo.stage_and_commit("add t3.md")?;
+            std::fs::write(test_repo.dir.join("t4.md"), "some content")?;
+            let ahead_2_oid = test_repo.stage_and_commit("add t4.md")?;
+
+            let git_repo = Repo::from_path(&test_repo.dir)?;
+
+            assert!(git_repo.ancestor_of(&oid_to_sha1(&ahead_2_oid), &oid_to_sha1(&ahead_1_oid))?);
+            Ok(())
+        }
+
+        #[test]
+        fn same_commit_returns_false() -> Result<()> {
+            let test_repo = GitTestRepo::default();
+            test_repo.populate()?;
+
+            // create feature branch and add 2 commits
+            test_repo.create_branch("feature")?;
+
+            test_repo.checkout("feature")?;
+            std::fs::write(test_repo.dir.join("t3.md"), "some content")?;
+            test_repo.stage_and_commit("add t3.md")?;
+            std::fs::write(test_repo.dir.join("t4.md"), "some content")?;
+            let ahead_2_oid = test_repo.stage_and_commit("add t4.md")?;
+
+            let git_repo = Repo::from_path(&test_repo.dir)?;
+
+            assert!(!git_repo.ancestor_of(&oid_to_sha1(&ahead_2_oid), &oid_to_sha1(&ahead_2_oid))?);
+            Ok(())
+        }
+
+        #[test]
+        fn commit_not_in_history_returns_false() -> Result<()> {
+            let test_repo = GitTestRepo::default();
+            test_repo.populate()?;
+
+            // create feature branch and add 2 commits
+            test_repo.create_branch("feature")?;
+
+            // create commit not in feature history
+            std::fs::write(test_repo.dir.join("notfeature.md"), "some content")?;
+            let on_main_after_feature = test_repo.stage_and_commit("add notfeature.md")?;
+
+            test_repo.checkout("feature")?;
+            std::fs::write(test_repo.dir.join("t3.md"), "some content")?;
+            test_repo.stage_and_commit("add t3.md")?;
+            std::fs::write(test_repo.dir.join("t4.md"), "some content")?;
+            let ahead_2_oid = test_repo.stage_and_commit("add t4.md")?;
+
+            let git_repo = Repo::from_path(&test_repo.dir)?;
+
+            assert!(!git_repo.ancestor_of(
+                &oid_to_sha1(&ahead_2_oid),
+                &oid_to_sha1(&on_main_after_feature)
+            )?);
+            Ok(())
         }
     }
 }