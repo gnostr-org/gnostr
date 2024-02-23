gnostr-org/gnostr

commit ed3e697281f9189ecd64cca70e626f928b0d11db

refactor: remove confusing options, improve help
from_branch and to_branch have been replaced
by specifying revision ranges

src/sub_commands/send.rs
@@ -777,7 +777,11 @@ fn identify_ahead_behind(
                 .context(format!("cannot find from_branch '{name}'"))?,
         ),
         None => (
-            "head".to_string(),
+            if let Ok(name) = git_repo.get_checked_out_branch_name() {
+                name
+            } else {
+                "head".to_string()
+            },
             git_repo
                 .get_head_commit()
                 .context("failed to get head commit")
@@ -872,35 +876,6 @@ mod tests {
                     .to_string(),
                 "a destination branch (to_branch) is not specified and the defaults (main or master) do not exist",
             );
-            Ok(())
-        }
-
-        #[test]
-        fn when_from_branch_is_none_return_as_head() -> Result<()> {
-            let test_repo = GitTestRepo::default();
-            let git_repo = Repo::from_path(&test_repo.dir)?;
-
-            test_repo.populate()?;
-            // create feature branch with 1 commit ahead
-            test_repo.create_branch("feature")?;
-            test_repo.checkout("feature")?;
-            std::fs::write(test_repo.dir.join("t3.md"), "some content")?;
-            let head_oid = test_repo.stage_and_commit("add t3.md")?;
-
-            // make feature branch 1 commit behind
-            test_repo.checkout("main")?;
-            std::fs::write(test_repo.dir.join("t4.md"), "some content")?;
-            let main_oid = test_repo.stage_and_commit("add t4.md")?;
-            // checkout feature
-            test_repo.checkout("feature")?;
-
-            let (from_branch, to_branch, ahead, behind) =
-                identify_ahead_behind(&git_repo, &None, &None)?;
-
-            assert_eq!(from_branch, "head");
-            assert_eq!(ahead, vec![oid_to_sha1(&head_oid)]);
-            assert_eq!(to_branch, "main");
-            assert_eq!(behind, vec![oid_to_sha1(&main_oid)]);
             Ok(())
         }
 

tests/send.rs
@@ -39,7 +39,7 @@ fn when_no_commits_ahead_of_main_return_error() -> Result<()> {
     test_repo.checkout("feature")?;
 
     let mut p = CliTester::new_from_dir(&test_repo.dir, ["send"]);
-    p.expect("Error: 'head' is 0 commits ahead of 'main' so no patches were created")?;
+    p.expect("Error: 'feature' is 0 commits ahead of 'main' so no patches were created")?;
     Ok(())
 }
 
@@ -73,7 +73,7 @@ mod when_commits_behind_ask_to_proceed {
         ahead: u8,
     ) -> Result<CliTesterConfirmPrompt> {
         p.expect_confirm(
-            format!("'head' is {behind} commits behind 'main' and {ahead} ahead. Consider rebasing before sending patches. Proceed anyway?").as_str(),
+            format!("'feature' is {behind} commits behind 'main' and {ahead} ahead. Consider rebasing before sending patches. Proceed anyway?").as_str(),
             Some(false),
         )
     }
@@ -108,7 +108,7 @@ mod when_commits_behind_ask_to_proceed {
         let mut p = CliTester::new_from_dir(&test_repo.dir, ["send"]);
         expect_confirm_prompt(&mut p, BEHIND_LEN, AHEAD_LEN)?.succeeds_with(Some(true))?;
         p.expect(
-            format!("creating patch for {AHEAD_LEN} commits from 'head' that are {BEHIND_LEN} behind 'main'",)
+            format!("creating patch for {AHEAD_LEN} commits from 'feature' that are {BEHIND_LEN} behind 'main'",)
                 .as_str(),
         )?;
         p.exit()?;
@@ -131,7 +131,7 @@ fn cli_message_creating_patches() -> Result<()> {
 
     let mut p = CliTester::new_from_dir(&test_repo.dir, ["send"]);
 
-    p.expect("creating patch for 2 commits from 'head' that can be merged into 'main'")?;
+    p.expect("creating patch for 2 commits from 'feature' that can be merged into 'main'")?;
     p.exit()?;
     Ok(())
 }
@@ -184,7 +184,7 @@ fn cli_tester_create_proposal(git_repo: &GitTestRepo, include_cover_letter: bool
 }
 
 fn expect_msgs_first(p: &mut CliTester, include_cover_letter: bool) -> Result<()> {
-    p.expect("creating patch for 2 commits from 'head' that can be merged into 'main'\r\n")?;
+    p.expect("creating patch for 2 commits from 'feature' that can be merged into 'main'\r\n")?;
     p.expect("searching for profile and relay updates...\r\n")?;
     p.expect("\r")?;
     p.expect("logged in as fred\r\n")?;

tests/list.rs
@@ -1710,8 +1710,6 @@ mod when_main_branch_is_uptodate {
                             c.succeeds_with(0, true, None)?;
                             p.expect("finding commits...\r\n")?;
                             p.expect("updated proposal available (2 ahead 0 behind 'main'). existing version is 2 ahead 1 behind 'main'\r\n")?;
-                            // its got here but tmpgit-32.. indicates that creatubg the eature
-                            // branch in line 1291 didnt work so the choices will be different
                             let mut c = p.expect_choice(
                                 "",
                                 vec![

tests/push.rs
@@ -591,7 +591,7 @@ mod when_branch_is_checked_out {
                         p.expect("preparing to force push proposal revision...\r\n")?;
 
                         // standard output from `ngit send`
-                        p.expect("creating patch for 2 commits from 'head' that can be merged into 'main'\r\n")?;
+                        p.expect(format!("creating patch for 2 commits from '{FEATURE_BRANCH_NAME_1}' that can be merged into 'main'\r\n"))?;
                         p.expect("searching for profile and relay updates...\r\n")?;
                         p.expect("\r")?;
                         p.expect("logged in as fred\r\n")?;

commit 679bd730010ba8983ea6190f12b131e4585a7bc7

fix(push): remove force push bug
introduced in ed3e697281f9189ecd64cca70e626f928b0d11db

src/sub_commands/send.rs
@@ -47,7 +47,9 @@ pub async fn launch(cli_args: &Cli, args: &SubCommandArgs) -> Result<()> {
     let git_repo = Repo::discover().context("cannot find a git repository")?;
 
     let mut commits: Vec<Sha1Hash> = {
-        if args.since_or_revision_range.eq("master..HEAD") {
+        if args.since_or_revision_range.is_empty()
+            || args.since_or_revision_range.eq("master..HEAD")
+        {
             let (from_branch, to_branch, ahead, behind) =
                 identify_ahead_behind(&git_repo, &None, &None)?;
 
@@ -794,7 +796,7 @@ fn identify_ahead_behind(
         None => {
             let (name, commit) = git_repo
                 .get_main_or_master_branch()
-                .context("a destination branch (to_branch) is not specified and the defaults (main or master) do not exist")?;
+                .context("the default branches (main or master) do not exist")?;
             (name.to_string(), commit)
         }
     };
@@ -867,7 +869,7 @@ mod tests {
                 identify_ahead_behind(&git_repo, &None, &None)
                     .unwrap_err()
                     .to_string(),
-                "a destination branch (to_branch) is not specified and the defaults (main or master) do not exist",
+                "the default branches (main or master) do not exist",
             );
             Ok(())
         }

tests/send.rs
@@ -4,29 +4,11 @@ use serial_test::serial;
 use test_utils::{git::GitTestRepo, relay::Relay, *};
 
 #[test]
-fn when_to_branch_doesnt_exist_return_error() -> Result<()> {
-    let test_repo = GitTestRepo::default();
-    test_repo.populate()?;
-    let mut p = CliTester::new_from_dir(&test_repo.dir, ["send", "--to-branch", "nonexistant"]);
-    p.expect("Error: cannot find to_branch 'nonexistant'")?;
-    Ok(())
-}
-
-#[test]
-fn when_no_to_branch_specified_and_no_main_or_master_branch_return_error() -> Result<()> {
+fn when_no_main_or_master_branch_return_error() -> Result<()> {
     let test_repo = GitTestRepo::new("notmain")?;
     test_repo.populate()?;
     let mut p = CliTester::new_from_dir(&test_repo.dir, ["send"]);
-    p.expect("Error: a destination branch (to_branch) is not specified and the defaults (main or master) do not exist")?;
-    Ok(())
-}
-
-#[test]
-fn when_from_branch_doesnt_exist_return_error() -> Result<()> {
-    let test_repo = GitTestRepo::default();
-    test_repo.populate()?;
-    let mut p = CliTester::new_from_dir(&test_repo.dir, ["send", "--from-branch", "nonexistant"]);
-    p.expect("Error: cannot find from_branch 'nonexistant'")?;
+    p.expect("Error: the default branches (main or master) do not exist")?;
     Ok(())
 }
 