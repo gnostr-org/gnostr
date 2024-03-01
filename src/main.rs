1120d0b0be4daf9ff859f1762699eab80abcbacb
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-03-01 18:41:48 -0500

    gnostr:re-add:-r --relay arg

--- src/main.rs
+++ src/main.rs
@@ -26,11 +26,37 @@
 use std::env;
 //use std::io::Read;
 use std::process;
+use std::str::FromStr;
 
 use inline_c::assert_c;
 //use gnostr_bins;
 
 use std::process::{Command, Stdio};
+
+use getopts::Options;
+use gnostr_bins::get_relays;
+
+extern "C" {
+    fn double_input(input: libc::c_int) -> libc::c_int;
+}
+
+extern "C" {
+    ///static void gnostr_sha256(int argc, const char* argv[], struct args *args)
+    fn gnostr_sha256(input: libc::c_int) -> libc::c_int;
+}
+extern "C" {
+    ///static int copyx(unsigned char *output, const unsigned char *x32, const unsigned char *y32, void *data)
+    fn copyx(input: libc::c_int) -> libc::c_int;
+}
+extern "C" {
+    ///static void try_subcommand(int argc, const char* argv[])
+    fn try_subcommand(input: libc::c_int) -> libc::c_int;
+}
+extern "C" {
+    ///static void print_hex(unsigned char* data, size_t size)
+    fn print_hex(input: libc::c_int) -> libc::c_int;
+}
+
 
 fn gen_keys(){
 
@@ -44,103 +70,116 @@ use rand_core::OsRng;
 
 }
 
-//COMMAND:
-//CONTEXT
-//gnostr --sec e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855 | gnostr-post-event --relay wss://relay.damus.io
-#[allow(dead_code)]
-fn command_example() {
-    let gnostr_sec = Command::new("gnostr")
-        .arg("--sec")
-        .arg("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855")
-        .arg("-t")
-        .arg("gnostr")
-        .stdout(Stdio::piped())
-        .output()
-        .expect("url failed");
-    let mut gnostr_event = String::from_utf8(gnostr_sec.stdout).unwrap();
-    gnostr_event.pop();
-    println!("{:?}", gnostr_event);
-
-    let gnostr_post_event = Command::new("gnostr-post-event")
-        .arg("--relay")
-        .arg("wss://relay.damus.io")
-        .arg(gnostr_event)
-        .stdout(Stdio::piped())
-        .output()
-        .expect("picture failed");
-    let post = String::from_utf8(gnostr_post_event.stdout).unwrap();
-    println!("{}", post);
+fn print_type_of<T>(_: &T) -> String {
+    format!("{}", std::any::type_name::<T>())
 }
 
+///gnostr-bins::get_relays()
+pub fn relays(_program: &str, _opts: &Options) {
+    let relays = get_relays();
+    println!("{}", format!("{  }", relays.unwrap()));
+}
+
+pub fn print_usage(program: &str, opts: &Options) {
+    let brief = format!("Usage: {} FILE [options]", program);
+    print!("{}", opts.usage(&brief));
+}
 
 fn main() {
 
     let args_vector: Vec<String> = env::args().collect();
     //println!("args_vector = {:?}", args_vector);
     //println!("args_vector.len() = {:?}", args_vector.len());
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+    //REF: https://docs.rs/getopts/latest/getopts/struct.Options.html
+    let mut opts = Options::new();
 
-    if args_vector.len() == 1 {
+    opts.optopt("o", "", "set output file name", "NAME");
+    opts.optopt(
+        "i",
+        "input",
+        "Specify the maximum number of commits to show (default: 10)",
+        "NUMBER",
+    );
 
-    }
+    opts.optflag("h", "help", "print this help menu");
+    opts.optflag("r", "relays", "print a json object of relays");
 
-    if args_vector.len() != 1 {
-    //catch help
-    if args_vector[1] == "-h" {
-        println!("-h HELP!");
-        process::exit(0);
-    }
-    if args_vector[1] == "--help" {
-        println!("--help HELP!");
-        process::exit(0);
-    }
-    //catch version
-    if args_vector[1] == "-v" {
-        println!("-v VERSION!");
-        process::exit(0);
-    }
-    if args_vector[1] == "--version" {
-        println!("--version VERSION!");
-        process::exit(0);
-    }
-    //catch sec
-    if args_vector[1] == "--sec" {
-        println!("--sec CALLED!");
-        process::exit(0);
-    }
-    //catch gen
-    if args_vector[1] == "--gen" {
-        //println!("--gen CALLED!");
-        gen_keys();
-        process::exit(0);
-    }
-    //catch genkey
-    if args_vector[1] == "--genkey" {
-        //println!("--genkey CALLED!");
-        gen_keys();
-        process::exit(0);
-    }
+    if args.len() >= 1 {
+        let matches = match opts.parse(&args[1..]) {
+            Ok(m) => m,
+            Err(f) => {
+                println!("Error: {}", f.to_string());
+                panic!("{}", f.to_string())
+            }
+        };
+        if matches.opt_present("h") {
+            print_usage(&program, &opts);
+            process::exit(0);
+        }
+        if matches.opt_present("r") {
+            relays(&program, &opts);
+            process::exit(0);
+        }
 
+//fn add_one(mut x: Option<i32>) -> i32 {
+//    let x = if x == None {
+//        0
+//    } else {
+//        x.unwrap()
+//    };
+//    return x + 1;
+//}
 
-    //catch genkeys
-    if args_vector[1] == "--genkeys" {
-        //println!("--genkeys CALLED!");
-        gen_keys();
-        process::exit(0);
+        let s = &"hello world".to_string();
+        let cloned_s = s.clone();
+        println!("{:?}", print_type_of(&s));
+        println!("{:?}", print_type_of(&cloned_s));
+        let _output = matches.opt_str("o");
+        println!("{:?}", print_type_of(&_output));
+        //leave input as &Option<String>
+        let mut _input = matches.opt_str("i");
+
+        //https://doc.rust-lang.org/std/primitive.str.html#method.parse
+        let four = "4".parse::<u32>();
+        println!("four {:?}", print_type_of(&four.as_ref().unwrap()));
+        assert_eq!(Ok(4), four);
+        let mut mut_four = "4".parse::<u32>();
+        println!("mut_four {:?}", print_type_of(&mut_four.as_mut().unwrap()));
+        assert_eq!(Ok(4), mut_four);
+
+        let test = _input.as_mut().unwrap().parse::<u32>();
+        println!("test {:?}", print_type_of(&test));
+        println!("test {:?}", print_type_of(&test.as_ref().unwrap()));
+        assert_eq!(Ok(4), test);
+
+        let input = _input.as_mut().unwrap().parse::<i32>();
+        println!("input {:?}", print_type_of(&input));
+        println!("input {:?}", print_type_of(&input.as_ref().unwrap()));
+        assert_eq!(Ok(4), input);
+
+        let input32 = _input.as_mut().unwrap().parse::<i32>();
+        println!("input32 {:?}", print_type_of(&input32));
+        println!("input32 {:?}", print_type_of(&input32.as_ref().unwrap()));
+        assert_eq!(Ok(4), input32);
+
+        let _input32 = _input.as_mut().unwrap().parse::<i32>();
+        println!("&_input32 {:?}", print_type_of(&_input32));
+        //let mut input: i32 = _input32.unwrap_or(i32::MAX);
+        //let mut input: i32 = _input32.as_deref().unwrap_or(i32::MAX);
+        let mut input: &i32 = _input32.as_ref().unwrap_or(&i32::MAX);
+        println!("&input {:?}", print_type_of(&input));
+        //deref &str
+        //let mut _value: i32 = input32.as_deref().as_deref().unwrap_or("100");
+        //let mut _value: i32 = &input32.as_deref().unwrap_or("100");
+        //
+        //REF: https://doc.rust-lang.org/book/ch19-01-unsafe-rust.html
+        let mut _value: i32 = input32.unwrap_or(i32::MAX);
+        let result = unsafe {
+            double_input(_value);
+        };
+        println!("{:?}",result);
+        println!("{:?}",_input);
     }
-
-    //command_example();
-
-    } else { // end if args_vector.len() == 1
-      println!("default HELP!"); }
-
-    (assert_c! {
-        #include <stdio.h>
-        int main() { printf("1:Hello, Gnostr!"); return 0; }
-    }
-    )
-    .success()
-    .stdout("1:Hello, Gnostr!");
-    //rust
-    println!("3:Hello, Gnostr!");
-
 }
