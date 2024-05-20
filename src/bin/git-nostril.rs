43ff4ebecb3281d1f165179a920eed92e1ecf9a9
randymcmillan <randy.lee.mcmillan@gmail.com>
2024-05-20 17:44:49 -0400

    src/bin/git-nostril.rs:parsing and passing correctly

--- src/bin/git-nostril.rs
+++ src/bin/git-nostril.rs
@@ -15,8 +15,8 @@ use markdown::to_html;
 fn main() -> io::Result<()> {
 
 
+    //capture git-nostril --sec <private_key>
     let args_vec: Vec<String> = env::args().collect();
-
     let app = &args_vec[0];
     let sec = &args_vec[1];
     let private_key = &args_vec[2];
@@ -26,24 +26,36 @@ fn main() -> io::Result<()> {
     println!("private_key={}", private_key);
 
 
-  let mut args_string = String::new();
 
-  //Need push sequence here to format string correctly
-  for arg in args().skip(1) {
-  //print!("{}",arg);
-    args_string.push_str(&arg);
-    args_string.push(' ');
-  }
-  // Remove trailing space (if any)
-  args_string.pop();
+    //skip git-nostril --sec <private_key>
+    //and captures everything else
+    let args: Vec<String> = env::args().skip(3).collect();
 
-  //print!("{:?}",Some(args()));
-  //print!("\n{:}\n",format!("{:?}",args()));
-  //print!("{}\n",format!("{:?}",args_vec[0]));
-  //print!("{}\n",format!("{:?}",args_vec[1]));
-  //print!("{}\n",format!("{:?}",args_vec[2]));
-  //print!("{:}\n",format!("{:?}",args_string));
-  //print!("{:}\n",format!("{:?}",&args_string.replace("\"","")));
+
+
+
+  //let mut args_string = String::new();
+
+  ////Need push sequence here to format string correctly
+  ////for arg in args().skip(1) {
+  ////
+  ////enforce positional args --sec <private_key>
+  ////
+  //for arg in args().skip(3) {
+  ////print!("{}",arg);
+  //  args_string.push_str(&arg);
+  //  args_string.push(' ');
+  //}
+  //// Remove trailing space (if any)
+  /////args_string.pop();
+
+  ////print!("{:?}",Some(args()));
+  ////print!("\n{:}\n",format!("{:?}",args()));
+  ////print!("{}\n",format!("{:?}",args_vec[0]));
+  ////print!("{}\n",format!("{:?}",args_vec[1]));
+  ////print!("{}\n",format!("{:?}",args_vec[2]));
+  ////print!("{:}\n",format!("{:?}",args_string));
+  ////print!("{:}\n",format!("{:?}",&args_string.replace("\"","")));
   //print!("{}\n",args_string);
 
 
@@ -59,14 +71,18 @@ fn main() -> io::Result<()> {
     .map_err(|non_utf8| String::from_utf8_lossy(non_utf8.as_bytes()).into_owned())
     .unwrap();
     println!("{}", nostril);
+    println!("{:?}", args_vec);
+    println!("{:?}", args);
+    //print!("args_string={}\n",&&format!("{}",&args_string));
 
     let event = Command::new("nostril")
         //.arg("--sec")
-        .args(&[
-            "--sec",
-            "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
-        ])
-        //.args(&mut args())
+        //.args(&[
+        //    &sec,
+        //    &private_key,
+        //    &&format!("\"{}\"",args_string),
+        //])
+        .args(&args)
         //.arg(args_vec[1])
         //.arg(args_vec[2])
         //.arg(private_key)
