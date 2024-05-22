1cd23d1b8c12ab724e330761e2dda51c99d70445
randymcmillan <randy.lee.mcmillan@gmail.com>
2024-05-22 10:57:50 -0400

    gnostr:add to repo

---
+++ gnostr/CMakeFiles/nostril.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/randymcmillan/gnostr.org/gnostr/gnostr/aes.c" "CMakeFiles/nostril.dir/aes.c.o" "gcc" "CMakeFiles/nostril.dir/aes.c.o.d"
+  "/Users/randymcmillan/gnostr.org/gnostr/gnostr/base64.c" "CMakeFiles/nostril.dir/base64.c.o" "gcc" "CMakeFiles/nostril.dir/base64.c.o.d"
+  "/Users/randymcmillan/gnostr.org/gnostr/gnostr/nostril.c" "CMakeFiles/nostril.dir/nostril.c.o" "gcc" "CMakeFiles/nostril.dir/nostril.c.o.d"
+  "/Users/randymcmillan/gnostr.org/gnostr/gnostr/sha256.c" "CMakeFiles/nostril.dir/sha256.c.o" "gcc" "CMakeFiles/nostril.dir/sha256.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ gnostr/CMakeFiles/nostril.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nostril.dir/aes.c.o"
+  "CMakeFiles/nostril.dir/aes.c.o.d"
+  "CMakeFiles/nostril.dir/base64.c.o"
+  "CMakeFiles/nostril.dir/base64.c.o.d"
+  "CMakeFiles/nostril.dir/nostril.c.o"
+  "CMakeFiles/nostril.dir/nostril.c.o.d"
+  "CMakeFiles/nostril.dir/sha256.c.o"
+  "CMakeFiles/nostril.dir/sha256.c.o.d"
+  "nostril"
+  "nostril.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/nostril.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
