9bd78a7495da68e841d80fab17b7becf881ce463
randymcmillan <randy.lee.mcmillan@gmail.com>
2024-06-21 10:03:59 -0400

    cargo.mk

--- cargo.mk
+++ cargo.mk
@@ -1,9 +1,9 @@
-cargo-build-release::cargo-br
-cargo-br:
-	cargo build --release -vv
-cargo-install:cargo-i
-cargo-i:
-	cargo install --path . -vv $(FORCE)
+## cargo-build-release::cargo-br
+## cargo-br:
+## 	cargo build --release -vv
+## cargo-install:cargo-i
+## cargo-i:
+## 	cargo install --path . -vv $(FORCE)
 ##
 ##===============================================================================
 ##make cargo-*
