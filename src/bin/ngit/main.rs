d4b5de0239c8635d83a6ab13390a4e3ec31c3195
randymcmillan <randymcmillan@protonmail.com>
2024-09-18 22:35:17 -0400

    src/bin

--- src/bin/ngit/main.rs
+++ src/bin/ngit/main.rs
@@ -7,20 +7,32 @@ use clap::Parser;
 use cli::{Cli, Commands};
 
 mod cli;
-use ngit::{cli_interactor, client, git, git_events, login, repo_ref};
+use ngit::{
+	cli_interactor, client, git, git_events, login, repo_ref,
+};
 
 mod sub_commands;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let cli = Cli::parse();
-    match &cli.command {
-        Commands::Fetch(args) => sub_commands::fetch::launch(&cli, args).await,
-        Commands::Login(args) => sub_commands::login::launch(&cli, args).await,
-        Commands::Init(args) => sub_commands::init::launch(&cli, args).await,
-        Commands::Send(args) => sub_commands::send::launch(&cli, args, false).await,
-        Commands::List => sub_commands::list::launch().await,
-        Commands::Pull => sub_commands::pull::launch().await,
-        Commands::Push(args) => sub_commands::push::launch(&cli, args).await,
-    }
+	let cli = Cli::parse();
+	match &cli.command {
+		Commands::Fetch(args) => {
+			sub_commands::fetch::launch(&cli, args).await
+		}
+		Commands::Login(args) => {
+			sub_commands::login::launch(&cli, args).await
+		}
+		Commands::Init(args) => {
+			sub_commands::init::launch(&cli, args).await
+		}
+		Commands::Send(args) => {
+			sub_commands::send::launch(&cli, args, false).await
+		}
+		Commands::List => sub_commands::list::launch().await,
+		Commands::Pull => sub_commands::pull::launch().await,
+		Commands::Push(args) => {
+			sub_commands::push::launch(&cli, args).await
+		}
+	}
 }
