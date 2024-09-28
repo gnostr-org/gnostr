07a1c6baedd4f474c949c787f73cd0f04d4bdce6
randymcmillan <randymcmillan@protonmail.com>
2024-09-28 04:05:46 -0400

    make:cargo-i

--- cargo.mk
+++ cargo.mk
@@ -32,7 +32,7 @@ cargo-build:### 	cargo build
 cargo-i:cargo-install
 cargo-install:### 	cargo install --path .
 	@. $(HOME)/.cargo/env
-	@cargo install --path alacritty $(FORCE)
+	@cargo install --path . $(FORCE)
 cargo-br:cargo-build-release### 	cargo-br
 ## 	cargo-br q=true
 cargo-build-release:### 	cargo-build-release
