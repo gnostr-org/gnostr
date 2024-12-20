39d0c4771cc3650047ce38581b1017cae9514abb
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-12-20 10:59:08 -0500

    src/main.rs:intermediate

--- src/main.rs
+++ src/main.rs
@@ -153,7 +153,67 @@ match_args {{increase|decrease}} <integer>
 }
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn tui() -> Result<()> {
+	let app_start = Instant::now();
+
+	let cliargs = process_cmdline()?;
+
+	asyncgit::register_tracing_logging();
+
+	if !valid_path(&cliargs.repo_path) {
+		eprintln!(
+			"invalid path\nplease run gitui inside of a non-bare git repository"
+		);
+		return Ok(());
+	}
+
+	let key_config = KeyConfig::init()
+		.map_err(|e| {
+			eprintln!("KeyConfig loading error: {e}")
+		})
+		.unwrap_or_default();
+	let theme = Theme::init(&cliargs.theme);
+
+	setup_terminal()?;
+	defer! {
+		shutdown_terminal();
+	}
+
+	set_panic_handlers()?;
+
+	let mut terminal = start_terminal(io::stdout())?;
+	let mut repo_path = cliargs.repo_path;
+	let input = Input::new();
+
+	let updater = if cliargs.notify_watcher {
+		Updater::NotifyWatcher
+	} else {
+		Updater::Ticker
+	};
+
+	loop {
+		let quit_state = run_app(
+			app_start,
+			repo_path.clone(),
+			theme.clone(),
+			key_config.clone(),
+			&input,
+			updater,
+			&mut terminal,
+		)?;
+
+		match quit_state {
+			QuitState::OpenSubmodule(p) => {
+				repo_path = p;
+			}
+                        _ => break Ok(()),
+		}
+	}
+
+
+}
+
+fn main() -> Result<()> {
 	use std::env;
 	let args: Vec<String> = env::args().collect();
 	dbg!(&args);
@@ -236,63 +296,8 @@ async fn main() -> Result<()> {
 		// all the other cases
 		_ => {
 			// show a help message
-			//let _ = help();
+			let _ = help();
 
-			let app_start = Instant::now();
-
-			let cliargs = process_cmdline()?;
-
-			asyncgit::register_tracing_logging();
-
-			if !valid_path(&cliargs.repo_path) {
-				eprintln!(
-					"invalid path\nplease run gitui inside of a non-bare git repository"
-				);
-				return Ok(());
-			}
-
-			let key_config = KeyConfig::init()
-				.map_err(|e| {
-					eprintln!("KeyConfig loading error: {e}")
-				})
-				.unwrap_or_default();
-			let theme = Theme::init(&cliargs.theme);
-
-			setup_terminal()?;
-			defer! {
-				shutdown_terminal();
-			}
-
-			set_panic_handlers()?;
-
-			let mut terminal = start_terminal(io::stdout())?;
-			let mut repo_path = cliargs.repo_path;
-			let input = Input::new();
-
-			let updater = if cliargs.notify_watcher {
-				Updater::NotifyWatcher
-			} else {
-				Updater::Ticker
-			};
-
-			loop {
-				let quit_state = run_app(
-					app_start,
-					repo_path.clone(),
-					theme.clone(),
-					key_config.clone(),
-					&input,
-					updater,
-					&mut terminal,
-				)?;
-
-				match quit_state {
-					QuitState::OpenSubmodule(p) => {
-						repo_path = p;
-					}
-					_ => break,
-				}
-			}
 		}
 	};
 
