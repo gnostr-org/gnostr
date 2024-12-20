a3691b496b77547e2180037fe986b741d9c10832
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-12-20 10:34:16 -0500

    match args:match 2 => match &cli.command

--- src/main.rs
+++ src/main.rs
@@ -158,19 +158,52 @@ async fn main() -> Result<()> {
 	let args: Vec<String> = env::args().collect();
 	dbg!(&args);
 
+	let cli = Cli::parse();
+
 	match args.len() {
 		// no arguments passed
 		1 => {
 			println!(
 				"My name is 'match_args'. Try passing some arguments!"
 			);
+
+			//invoke tui
 		}
 		// one argument passed
-		2 => match args[1].parse() {
-			Ok(42) => println!("This is the answer!"),
-			_ => println!("This is not the answer."),
+		2 => match &cli.command {
+			Commands::Fetch(args) => {
+				sub_commands::fetch::launch(&cli, args)
+					.await
+					.expect("REASON")
+			}
+			Commands::Login(args) => {
+				sub_commands::login::launch(&cli, args)
+					.await
+					.expect("REASON")
+			}
+			Commands::Init(args) => {
+				sub_commands::init::launch(&cli, args)
+					.await
+					.expect("REASON")
+			}
+			Commands::Send(args) => {
+				sub_commands::send::launch(&cli, args, false)
+					.await
+					.expect("REASON")
+			}
+			Commands::List => {
+				sub_commands::list::launch().await.expect("REASON")
+			}
+			Commands::Pull => {
+				sub_commands::pull::launch().await.expect("REASON")
+			}
+			Commands::Push(args) => {
+				sub_commands::push::launch(&cli, args)
+					.await
+					.expect("REASON")
+			}
+			_ => println!("Ain't special"),
 		},
-		// one command and one argument passed
 		3 => {
 			let cmd = &args[1];
 			let num = &args[2];
@@ -200,43 +233,7 @@ async fn main() -> Result<()> {
 			// show a help message
 			let _ = help();
 		}
-	}
-
-	let cli = Cli::parse();
-	match &cli.command {
-		Commands::Fetch(args) => {
-			sub_commands::fetch::launch(&cli, args)
-				.await
-				.expect("REASON")
-		}
-		Commands::Login(args) => {
-			sub_commands::login::launch(&cli, args)
-				.await
-				.expect("REASON")
-		}
-		Commands::Init(args) => {
-			sub_commands::init::launch(&cli, args)
-				.await
-				.expect("REASON")
-		}
-		Commands::Send(args) => {
-			sub_commands::send::launch(&cli, args, false)
-				.await
-				.expect("REASON")
-		}
-		Commands::List => {
-			sub_commands::list::launch().await.expect("REASON")
-		}
-		Commands::Pull => {
-			sub_commands::pull::launch().await.expect("REASON")
-		}
-		Commands::Push(args) => {
-			sub_commands::push::launch(&cli, args)
-				.await
-				.expect("REASON")
-		}
-		_ => println!("Ain't special"),
-	}
+	};
 
 	let app_start = Instant::now();
 
