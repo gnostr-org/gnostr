a076169151497eaf34bc31e1b8c46d4f100009f1
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2025-01-02 19:26:13 -0500

    diff --git a/src/sub_commands/ngit.rs b/src/sub_commands/ngit.rs index f63151a..8967005 100644 --- a/src/sub_commands/ngit.rs +++ b/src/sub_commands/ngit.rs @@ -1,42 +1,11 @@  #![cfg_attr(not(test), warn(clippy::pedantic))]  #![cfg_attr(not(test), warn(clippy::expect_used))] -use clap::{Args, Parser, Subcommand}; -use ngit::sub_commands::*; +use clap::{Args, Parser}; +//use ngit::sub_commands::*;  use ngit::Cli; +use ngit::Commands;  use nostr_sdk::prelude::*;
    -//#[derive(Parser)]
    -//#[command(author, version, about, long_about = None)]
    -//#[command(propagate_version = true)]
    -//pub struct Cli {
    -//    #[command(subcommand)]
    -//    command: Commands,
    -//    /// nsec or hex private key
    -//    #[arg(short, long, global = true)]
    -//    nsec: Option<String>,
    -//    /// password to decrypt nsec
    -//    #[arg(short, long, global = true)]
    -//    password: Option<String>,
    -//    /// disable spinner animations
    -//    #[arg(long, action)]
    -//    disable_cli_spinners: bool,
    -//}
    -
    -#[derive(Subcommand)]
    -enum Commands {
    -    /// signal you are this repo's maintainer accepting proposals via nostr
    -    Init(init::SubCommandArgs),
    -    /// issue commits as a proposal
    -    Send(send::SubCommandArgs),
    -    /// list proposals; checkout, apply or download selected
    -    List,
    -    /// send proposal revision
    -    Push(push::SubCommandArgs),
    -    /// fetch and apply new proposal commits / revisions linked to branch
    -    Pull,
    -    /// run with --nsec flag to change npub
    -    Login(login::SubCommandArgs),
    -}
     #[derive(Args)]
     #[command(author, version, about, long_about = None)]
     #[command(propagate_version = true)]
    @@ -58,7 +27,7 @@ pub async fn ngit(sub_command_args: &NgitSubCommand) -> Result<()> {
         let cli = Cli::parse();
         match &sub_command_args.command {
             Commands::Login(args) => ngit::sub_commands::login::launch(&cli, &args).await?,
    -        Commands::Init(args) => ngit::sub_commands::init::launch(&cli, &args).await?,
    +        Commands::Init(args) => ngit::sub_commands::init::launch(&cli, &&args).await?,
             Commands::Send(args) => ngit::sub_commands::send::launch(&cli, &args).await?,
             Commands::List => ngit::sub_commands::list::launch().await?,
             Commands::Pull => ngit::sub_commands::pull::launch().await?,

--- src/sub_commands/ngit.rs
+++ src/sub_commands/ngit.rs
@@ -1,42 +1,11 @@
 #![cfg_attr(not(test), warn(clippy::pedantic))]
 #![cfg_attr(not(test), warn(clippy::expect_used))]
-use clap::{Args, Parser, Subcommand};
-use ngit::sub_commands::*;
+use clap::{Args, Parser};
+//use ngit::sub_commands::*;
 use ngit::Cli;
+use ngit::Commands;
 use nostr_sdk::prelude::*;
 
-//#[derive(Parser)]
-//#[command(author, version, about, long_about = None)]
-//#[command(propagate_version = true)]
-//pub struct Cli {
-//    #[command(subcommand)]
-//    command: Commands,
-//    /// nsec or hex private key
-//    #[arg(short, long, global = true)]
-//    nsec: Option<String>,
-//    /// password to decrypt nsec
-//    #[arg(short, long, global = true)]
-//    password: Option<String>,
-//    /// disable spinner animations
-//    #[arg(long, action)]
-//    disable_cli_spinners: bool,
-//}
-
-#[derive(Subcommand)]
-enum Commands {
-    /// signal you are this repo's maintainer accepting proposals via nostr
-    Init(init::SubCommandArgs),
-    /// issue commits as a proposal
-    Send(send::SubCommandArgs),
-    /// list proposals; checkout, apply or download selected
-    List,
-    /// send proposal revision
-    Push(push::SubCommandArgs),
-    /// fetch and apply new proposal commits / revisions linked to branch
-    Pull,
-    /// run with --nsec flag to change npub
-    Login(login::SubCommandArgs),
-}
 #[derive(Args)]
 #[command(author, version, about, long_about = None)]
 #[command(propagate_version = true)]
@@ -58,7 +27,7 @@ pub async fn ngit(sub_command_args: &NgitSubCommand) -> Result<()> {
     let cli = Cli::parse();
     match &sub_command_args.command {
         Commands::Login(args) => ngit::sub_commands::login::launch(&cli, &args).await?,
-        Commands::Init(args) => ngit::sub_commands::init::launch(&cli, &args).await?,
+        Commands::Init(args) => ngit::sub_commands::init::launch(&cli, &&args).await?,
         Commands::Send(args) => ngit::sub_commands::send::launch(&cli, &args).await?,
         Commands::List => ngit::sub_commands::list::launch().await?,
         Commands::Pull => ngit::sub_commands::pull::launch().await?,
