af86969c432ff87ff8c312fc550a5a9bb328e249
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2025-01-08 20:20:24 -0500

    gnostr-ngit-subcommand-intermediate
    diff --git a/.justfile b/.justfile
    index 587897e..51c9ae2 100644
    --- a/.justfile
    +++ b/.justfile
    @@ -26,6 +26,7 @@ deny:
    
     cargo-test:
         cargo test
    +    gnostr ngit --init -p nsec --hex --
    
     cargo-diet:
         nix develop .#lintShell --command cargo diet
    diff --git a/src/sub_commands/ngit.rs b/src/sub_commands/ngit.rs
    index a35659c..8fe6f44 100644
    --- a/src/sub_commands/ngit.rs
    +++ b/src/sub_commands/ngit.rs
    @@ -55,6 +55,9 @@ pub struct NgitSubCommand {
     }
    
     pub async fn ngit(sub_command_args: &NgitSubCommand) -> Result<()> {
    +    let args: Vec<String> = env::args().collect();
    +    println!("ngit:args={:?}", args);
    +    println!("ngit:sub_command_args={:?}", sub_command_args);
         //let cli = NgitCli::parse();
         if sub_command_args.init {
             println!("ngit:sub_command_args.init={}", sub_command_args.init);
    @@ -179,33 +182,33 @@ pub async fn ngit(sub_command_args: &NgitSubCommand) -> Result<()> {
                 let _ = send::launch(&cli, &args).await;
             } else if sub_command_args.list {
                 // list proposals; checkout, apply or download selected
    -            //
    +            //
                 // Usage: ngit list [OPTIONS]
    -            //
    +            //
                 // Options:
                 //   -n, --nsec <NSEC>          nsec or hex private key
                 //   -p, --password <PASSWORD>  password to decrypt nsec
                 //   -h, --help                 Print help
                 //   -V, --version              Print version
    -            //
    +            //
                 let _ = list::launch().await;
             } else if sub_command_args.pull {
                 // fetch and apply new proposal commits / revisions linked to branch
    -            //
    +            //
                 // Usage: ngit pull [OPTIONS]
    -            //
    +            //
                 // Options:
                 //   -n, --nsec <NSEC>          nsec or hex private key
                 //   -p, --password <PASSWORD>  password to decrypt nsec
                 //   -h, --help                 Print help
                 //   -V, --version              Print version
    -            //
    +            //
                 let _ = pull::launch().await;
             } else if sub_command_args.push {
                 // send proposal revision
    -            //
    +            //
                 // Usage: ngit push [OPTIONS]
    -            //
    +            //
                 // Options:
                 //       --force                send proposal revision from checked out proposal branch
                 //       --no-cover-letter      dont prompt for cover letter when force pushing
    @@ -213,7 +216,7 @@ pub async fn ngit(sub_command_args: &NgitSubCommand) -> Result<()> {
                 //   -p, --password <PASSWORD>  password to decrypt nsec
                 //   -h, --help                 Print help
                 //   -V, --version              Print version
    -            //
    +            //
                 let args = ngit::sub_commands::push::PushSubCommandArgs {
                     force: true,
                     no_cover_letter: true,

--- src/sub_commands/ngit.rs
+++ src/sub_commands/ngit.rs
@@ -55,6 +55,9 @@ pub struct NgitSubCommand {
 }
 
 pub async fn ngit(sub_command_args: &NgitSubCommand) -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+    println!("ngit:args={:?}", args);
+    println!("ngit:sub_command_args={:?}", sub_command_args);
     //let cli = NgitCli::parse();
     if sub_command_args.init {
         println!("ngit:sub_command_args.init={}", sub_command_args.init);
@@ -179,33 +182,33 @@ pub async fn ngit(sub_command_args: &NgitSubCommand) -> Result<()> {
             let _ = send::launch(&cli, &args).await;
         } else if sub_command_args.list {
             // list proposals; checkout, apply or download selected
-            // 
+            //
             // Usage: ngit list [OPTIONS]
-            // 
+            //
             // Options:
             //   -n, --nsec <NSEC>          nsec or hex private key
             //   -p, --password <PASSWORD>  password to decrypt nsec
             //   -h, --help                 Print help
             //   -V, --version              Print version
-            // 
+            //
             let _ = list::launch().await;
         } else if sub_command_args.pull {
             // fetch and apply new proposal commits / revisions linked to branch
-            // 
+            //
             // Usage: ngit pull [OPTIONS]
-            // 
+            //
             // Options:
             //   -n, --nsec <NSEC>          nsec or hex private key
             //   -p, --password <PASSWORD>  password to decrypt nsec
             //   -h, --help                 Print help
             //   -V, --version              Print version
-            // 
+            //
             let _ = pull::launch().await;
         } else if sub_command_args.push {
             // send proposal revision
-            // 
+            //
             // Usage: ngit push [OPTIONS]
-            // 
+            //
             // Options:
             //       --force                send proposal revision from checked out proposal branch
             //       --no-cover-letter      dont prompt for cover letter when force pushing
@@ -213,7 +216,7 @@ pub async fn ngit(sub_command_args: &NgitSubCommand) -> Result<()> {
             //   -p, --password <PASSWORD>  password to decrypt nsec
             //   -h, --help                 Print help
             //   -V, --version              Print version
-            // 
+            //
             let args = ngit::sub_commands::push::PushSubCommandArgs {
                 force: true,
                 no_cover_letter: true,
