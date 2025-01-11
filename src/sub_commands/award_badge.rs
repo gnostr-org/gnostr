345e250077fda705429de3dd5e8f3edf7b4f78b3
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2025-01-11 14:40:20 -0500

    src/sub_commands/award_badge.rs

--- src/sub_commands/award_badge.rs
+++ src/sub_commands/award_badge.rs
@@ -1,6 +1,8 @@
 use anyhow::{Context, Result};
 use nostr::{FromBech32, PublicKey, ToBech32};
 
+use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
+
 use super::send::send_events;
 #[cfg(not(test))]
 use crate::client::Client;
@@ -34,30 +36,33 @@ pub struct AwardBadgeSubCommandArgs {
     #[arg(long, action = clap::ArgAction::Append)]
     ptag: Option<String>,
     //ptag: Option<Vec<String>>,
-    #[clap(short, long)]
-    /// name of repository
-    pub title: Option<String>,
-    #[clap(short, long)]
-    /// optional description
-    pub description: Option<String>,
-    #[clap(long)]
-    /// git server url users can clone from
-    pub clone_url: Vec<String>,
-    #[clap(long, value_parser, num_args = 1..)]
-    /// homepage
-    pub web: Vec<String>,
+    //#[clap(short, long)]
+    ///// name of repository
+    //pub title: Option<String>,
+    //#[clap(short, long)]
+    ///// optional description
+    //pub description: Option<String>,
+    //#[clap(long)]
+    ///// git server url users can clone from
+    //pub clone_url: Vec<String>,
+    //#[clap(long, value_parser, num_args = 1..)]
+    ///// homepage
+    //pub web: Vec<String>,
     #[clap(long, value_parser, num_args = 1..)]
     /// relays contributors push patches and comments to
     pub relays: Vec<String>,
     #[clap(short, long, value_parser, num_args = 1..)]
     /// npubs of other maintainers
     pub other_maintainers: Vec<String>,
-    #[clap(long)]
-    /// usually root commit but will be more recent commit for forks
-    pub earliest_unique_commit: Option<String>,
-    #[clap(short, long)]
-    /// shortname with no spaces or special characters
-    pub identifier: Option<String>,
+    //#[clap(long)]
+    ///// usually root commit but will be more recent commit for forks
+    //pub earliest_unique_commit: Option<String>,
+    //#[clap(short, long)]
+    ///// shortname with no spaces or special characters
+    //pub identifier: Option<String>,
+    /// awarde_badge.rs Proof of work difficulty target¬                                         
+    #[arg(long, action = clap::ArgAction::Append, default_value_t = 0u8)]
+    difficulty_target: u8,
 }
 
 #[allow(clippy::too_many_lines)]
@@ -66,27 +71,86 @@ pub async fn launch(cli_args: &Cli, args: &AwardBadgeSubCommandArgs) -> Result<(
         panic!("No relays specified, at least one relay is required!")
     }
 
-    let git_repo = Repo::discover().context("cannot find a git repository")?;
-
-    let root_commit = git_repo
-        .get_root_commit()
-        .context("failed to get root commit of the repository")?;
-
-    // TODO: check for empty repo
-    // TODO: check for existing maintaiers file
-
     #[cfg(not(test))]
     let mut client = Client::default();
     #[cfg(test)]
     let mut client = <MockConnect as std::default::Default>::default();
-
     let (keys, user_ref) = login::launch(&cli_args.nsec, &cli_args.password, Some(&client)).await?;
-
     client.set_keys(&keys).await;
+
+
+    let my_keys = Keys::parse("0000000000000000000000000000000000000000000000000000000000000001")?;
+    let mut nostr_sdk_client = nostr_sdk::Client::new(&my_keys);
+
+    let proxy = Some(SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::LOCALHOST, 9050)));
+
+
+        // Add relays
+    nostr_sdk_client.add_relay("wss://relay.damus.io").await?;
+    nostr_sdk_client.add_relay_with_opts(
+        "wss://nos.lol", 
+        RelayOptions::new().proxy(proxy).write(true)
+    ).await?;
+    nostr_sdk_client.add_relay_with_opts(
+        "ws://jgqaglhautb4k6e6i2g34jakxiemqp6z4wynlirltuukgkft2xuglmqd.onion",
+        RelayOptions::new().proxy(proxy),
+    ).await?;
+
+
+    nostr_sdk_client.connect().await;
+
+    let metadata = Metadata::new()
+        .name("username")
+        .display_name("My Username")
+        .about("Description")
+        .picture(Url::parse("https://example.com/avatar.png")?)
+        .banner(Url::parse("https://example.com/banner.png")?)
+        .nip05("username@example.com")
+        .lud16("yuki@getalby.com")
+        .custom_field("custom_field", "my value");
+
+    // Update metadata
+    nostr_sdk_client.set_metadata(&metadata).await?;
+
+    let weeble_string = weeble().await;
+    let wobble_string = wobble().await;
+    let blockheight_string = blockheight().await;
+
+     use std::borrow::Cow;
+     let weeble_vec: Vec<String> = Tag::custom(TagKind::Custom(Cow::Borrowed("weeble")), [&weeble_string]).to_vec();
+     let weeble_tag: Tag = Tag::parse(&weeble_vec).unwrap();
+
+     let wobble_vec: Vec<String> = Tag::custom(TagKind::Custom(Cow::Borrowed("wobble")), [&wobble_string]).to_vec();
+     let wobble_tag: Tag = Tag::parse(&wobble_vec).unwrap();
+
+     let blockheight_vec: Vec<String> = Tag::custom(TagKind::Custom(Cow::Borrowed("blockheight")), [&blockheight_string]).to_vec();
+     let blockheight_tag: Tag = Tag::parse(&blockheight_vec).unwrap();
+
+     let gnostr: Vec<String> = Tag::custom(TagKind::Custom(Cow::Borrowed("client")), ["gnostr"]).to_vec();
+     let gnostr_tag: Tag = Tag::parse(&gnostr).unwrap();
+     //assert_eq!(gnostr_tag.content(), Some("gnostr"));
+     let t: Tag = Tag::parse(&["gnostr", &weeble_string]).unwrap();
+     //assert_eq!(t.content(), Some("gnostr"));
+
+
+    // Publish a text note
+    //nostr_sdk_client.publish_text_note("My first text note from rust-nostr!", [gnostr_tag, weeble_tag, blockheight_tag, wobble_tag]).await?;
+    nostr_sdk_client.publish_text_note(format!("{}/{}/{}", weeble_string, blockheight_string, wobble_string), [gnostr_tag, weeble_tag, blockheight_tag, wobble_tag]).await?;
+
+    // Create a POW text note
+    //let event: Event = EventBuilder::text_note("POW text note from nostr-sdk", []).to_pow_event(&my_keys, 20)?;
+    //client.send_event(event).await?; // Send to all relays
+    // client.send_event_to(["wss://relay.damus.io"], event).await?; // Send to specific relay
+    
+
+
+
+
+    let git_repo = Repo::discover().context("cannot find a git repository")?;
 
     let repo_ref = if let Ok(rep_ref) = repo_ref::fetch(
         &git_repo,
-        root_commit.to_string(),
+        "".to_string(),
         &client,
         user_ref.relays.write(),
         false,
@@ -98,153 +162,16 @@ pub async fn launch(cli_args: &Cli, args: &AwardBadgeSubCommandArgs) -> Result<(
         None
     };
 
-    let repo_config_result = get_repo_config_from_yaml(&git_repo);
-    // TODO: check for other claims
+	let repo_config_result = get_repo_config_from_yaml(&git_repo);
+    let root_commit = git_repo
+        .get_root_commit()
+        .context("failed to get root commit of the repository")?;
 
-    let badge_event_id = match &args.badge_event_id {
-        Some(beid) => beid.clone(),
-        None => Interactor::default().input(
-            PromptInputParms::default()
-                .with_prompt("badge_event_id")
-                .with_default(if let Some(repo_ref) = &repo_ref {
-                    repo_ref.name.clone()
-                } else {
-                    format!(
-                        "{:?}/{:?}/{:?}-{}",
-                        weeble().await,
-                        blockheight().await,
-                        wobble().await,
-                        String::new()
-                    )
-                }),
-        )?,
-    };
-    let ptag = match &args.ptag {
-        Some(t) => t.clone(),
-        None => Interactor::default().input(
-            PromptInputParms::default()
-                .with_prompt("ptag")
-                .with_default(if let Some(repo_ref) = &repo_ref {
-                    repo_ref.name.clone()
-                } else {
-                    format!(
-                        "{:?}/{:?}/{:?}-{}",
-                        weeble().await,
-                        blockheight().await,
-                        wobble().await,
-                        String::new()
-                    )
-                }),
-        )?,
-    };
-    let name = match &args.title {
-        Some(t) => t.clone(),
-        None => Interactor::default().input(
-            PromptInputParms::default()
-                .with_prompt("name")
-                .with_default(if let Some(repo_ref) = &repo_ref {
-                    repo_ref.name.clone()
-                } else {
-                    format!(
-                        "{:?}/{:?}/{:?}-{}",
-                        weeble().await,
-                        blockheight().await,
-                        wobble().await,
-                        String::new()
-                    )
-                }),
-        )?,
-    };
+    // TODO: check for empty repo
+    // TODO: check for existing maintaiers file
 
-    let identifier = match &args.identifier {
-        Some(t) => t.clone(),
-        None => Interactor::default().input(
-            PromptInputParms::default()
-                .with_prompt("identifier")
-                .with_default(if let Some(repo_ref) = &repo_ref {
-                    repo_ref.identifier.clone()
-                } else {
-                    name.clone()
-                        .replace(' ', "-")
-                        .replace('"', "")
-                        .chars()
-                        .map(|c| {
-                            if c.is_ascii_alphanumeric() || c.eq(&'/') {
-                                c
-                            } else {
-                                '-'
-                            }
-                        })
-                        .collect()
-                }),
-        )?,
-    };
 
-    let description = match &args.description {
-        Some(t) => t.clone(),
-        None => Interactor::default().input(
-            PromptInputParms::default()
-                .with_prompt("description")
-                .with_default(if let Some(repo_ref) = &repo_ref {
-                    repo_ref.description.clone()
-                } else {
-                    format!(
-                        "{:?}/{:?}/{:?}-{}",
-                        weeble().await,
-                        blockheight().await,
-                        wobble().await,
-                        String::new()
-                    )
-                }),
-        )?,
-    };
-
-    let git_server = if args.clone_url.is_empty() {
-        Interactor::default()
-            .input(
-                PromptInputParms::default()
-                    .with_prompt("clone url")
-                    .with_default(if let Some(repo_ref) = &repo_ref {
-                        repo_ref.git_server.clone().join(" ")
-                    } else if let Ok(git_repo) = git_repo.get_origin_url() {
-                        git_repo
-                    } else {
-                        format!(
-                            "{:?}/{:?}/{:?}-{}",
-                            weeble().await,
-                            blockheight().await,
-                            wobble().await,
-                            String::new()
-                        )
-                    }),
-            )?
-            .split(' ')
-            .map(std::string::ToString::to_string)
-            .collect()
-    } else {
-        args.clone_url.clone()
-    };
-
-    let web: Vec<String> = if args.web.is_empty() {
-        Interactor::default()
-            .input(
-                PromptInputParms::default()
-                    .with_prompt("web")
-                    .optional()
-                    .with_default(if let Some(repo_ref) = &repo_ref {
-                        repo_ref.web.clone().join(" ")
-                    } else {
-                        format!("https://gitworkshop.dev/repo/{}", &identifier)
-                    }),
-            )?
-            .split(' ')
-            .map(std::string::ToString::to_string)
-            .collect()
-    } else {
-        args.web.clone()
-    };
-
-    let maintainers: Vec<PublicKey> = {
+let maintainers: Vec<PublicKey> = {
         let mut dont_ask = !args.other_maintainers.is_empty();
         let mut maintainers_string = if !args.other_maintainers.is_empty() {
             [args.other_maintainers.clone()].concat().join(" ")
@@ -305,9 +232,10 @@ pub async fn launch(cli_args: &Cli, args: &AwardBadgeSubCommandArgs) -> Result<(
             break maintainers;
         }
     };
+	
 
-    // TODO: check if relays are free to post to so contributors can submit patches
-    // TODO: recommend some reliable free ones
+
+
     let relays: Vec<String> = if args.relays.is_empty() {
         Interactor::default()
             .input(
@@ -328,87 +256,6 @@ pub async fn launch(cli_args: &Cli, args: &AwardBadgeSubCommandArgs) -> Result<(
         args.relays.clone()
     };
 
-    let earliest_unique_commit = match &args.earliest_unique_commit {
-        Some(t) => t.clone(),
-        None => {
-            let mut earliest_unique_commit = if let Some(repo_ref) = &repo_ref {
-                repo_ref.root_commit.clone()
-            } else {
-                root_commit.to_string()
-            };
-            loop {
-                earliest_unique_commit = Interactor::default().input(
-                    PromptInputParms::default()
-                        .with_prompt("earliest unique commit")
-                        .with_default(earliest_unique_commit.clone()),
-                )?;
-                if let Ok(exists) = git_repo.does_commit_exist(&earliest_unique_commit) {
-                    if exists {
-                        break earliest_unique_commit;
-                    }
-                    println!("commit does not exist on current repository");
-                } else {
-                    println!("commit id not formatted correctly");
-                }
-                if earliest_unique_commit.len().ne(&40) {
-                    println!("commit id must be 40 characters long");
-                }
-            }
-        }
-    };
-
-    println!("publishing repostory reference...");
-
-    let identifier = String::from(format!(
-        "{:}/{:}/{:}/{}",
-        weeble().await,
-        blockheight().await,
-        wobble().await,
-        identifier
-    ));
-    let description = String::from(format!(
-        "{:}/{:}/{:}/{}",
-        weeble().await,
-        blockheight().await,
-        wobble().await,
-        description
-    ));
-    let name = String::from(format!(
-        "{:}/{:}/{:}/{}",
-        weeble().await,
-        blockheight().await,
-        wobble().await,
-        name
-    ));
-    let web = vec![
-        String::from(format!(
-            "{:}/{:}/{:}",
-            weeble().await,
-            blockheight().await,
-            wobble().await
-        )),
-        web[0].clone(),
-    ];
-    let repo_event = RepoRef {
-        identifier,
-        name,
-        description,
-        root_commit: earliest_unique_commit,
-        git_server,
-        web,
-        relays: relays.clone(),
-        maintainers: maintainers.clone(),
-    }
-    .to_event(&keys)?;
-
-    send_events(
-        &client,
-        vec![repo_event],
-        user_ref.relays.write(),
-        relays.clone(),
-        !cli_args.disable_cli_spinners,
-    )
-    .await?;
 
     // if yaml file doesnt exist or needs updating
     if match &repo_config_result {
@@ -431,5 +278,64 @@ pub async fn launch(cli_args: &Cli, args: &AwardBadgeSubCommandArgs) -> Result<(
             "this optional file enables existing contributors to automatically fetch your repo event (instead of one from a pubkey pretending to be the maintainer)"
         );
     }
+
+
+
+
+
+    let repo_ref = if let Ok(rep_ref) = repo_ref::fetch(
+        &git_repo,
+        root_commit.to_string(),
+        &client,
+        user_ref.relays.write(),
+        false,
+    )
+    .await
+    {
+        Some(rep_ref)
+    } else {
+        None
+    };
+
+    let repo_config_result = get_repo_config_from_yaml(&git_repo);
+    // TODO: check for other claims
+
+    let badge_event_id = match &args.badge_event_id {
+        Some(beid) => beid.clone(),
+        None => Interactor::default().input(
+            PromptInputParms::default()
+                .with_prompt("badge_event_id")
+                .with_default(if let Some(repo_ref) = &repo_ref {
+                    repo_ref.name.clone()
+                } else {
+                    format!(
+                        "{:?}/{:?}/{:?}-{}",
+                        weeble().await,
+                        blockheight().await,
+                        wobble().await,
+                        String::new()
+                    )
+                }),
+        )?,
+    };
+    let ptag = match &args.ptag {
+        Some(t) => t.clone(),
+        None => Interactor::default().input(
+            PromptInputParms::default()
+                .with_prompt("ptag")
+                .with_default(if let Some(repo_ref) = &repo_ref {
+                    repo_ref.name.clone()
+                } else {
+                    format!(
+                        "{:?}/{:?}/{:?}-{}",
+                        weeble().await,
+                        blockheight().await,
+                        wobble().await,
+                        String::new()
+                    )
+                }),
+        )?,
+    };
+
     Ok(())
 }
