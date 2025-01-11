000001e0cc7ba8130aebf1292fc864035e372484
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2025-01-10 20:39:33 -0500

    07:0006675b

--- crates/legit/cargo.mk
+++ crates/legit/cargo.mk
@@ -1,31 +1,23 @@
-##
-##make cargo-*
-cargo-help:### 	cargo-help
+cargo-help:### 	
 	@awk 'BEGIN {FS = ":.*?###"} /^[a-zA-Z_-]+:.*?###/ {printf "\033[36m%-15s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
-
-cargo-install:### 	cargo install --path .
-#@. $(HOME)/.cargo/env
-	#@cargo install --path $(PWD)
-	@cargo install --locked --path $(PWD) --force
-
-cargo-check:### 	cargo-check
-## cargo c
+cargo-build:###	cargo-build
+	@. $(HOME)/.cargo/env
+	@cargo b
+cargo-install:###	cargo-install
+	@. $(HOME)/.cargo/env
+	@cargo install --path $(PWD)
+	@echo "export PATH=$(CARGO_PATH)/bin:$(PATH)"
+cargo-build-release:###	cargo-build-release
+	@. $(HOME)/.cargo/env
+	@cargo b --release
+cargo-check:###	cargo-check
 	@. $(HOME)/.cargo/env
 	@cargo c
-
-cargo-bench:### 	cargo-bench
-## cargo bench
+cargo-bench:###	cargo-bench
 	@. $(HOME)/.cargo/env
 	@cargo bench
-
-cargo-test-benches:### 	cargo-test-benches
-	cargo test --benches
-
-cargo-report:### 	cargo-report
-	@. $(HOME)/.cargo/env && cargo report future-incompatibilities --id 1
-
-cargo-doc:### 	cargo-doc
-	@. $(HOME)/.cargo/env && cargo doc --no-deps #--open
-
+cargo-test:###	cargo-test
+	@. $(HOME)/.cargo/env
+	@cargo test
 # vim: set noexpandtab:
 # vim: set setfiletype make
--- crates/legit/src/worker.rs
+++ crates/legit/src/worker.rs
@@ -1,70 +1,52 @@
+use std::sync::mpsc;
 use crypto::digest::Digest;
 use crypto::sha1;
-use std::sync::mpsc;
-//use time;
+use time;
 
 pub struct Worker {
-    id: u32,
-    digest: sha1::Sha1,
-    tx: mpsc::Sender<(u32, String, String)>,
-    target: String,
-    tree: String,
-    parent: String,
-    author: String,
-    repo: String,
-    pwd_hash: String,
+    id:      u32,
+    digest:  sha1::Sha1,
+    tx:      mpsc::Sender<(u32, String, String)>,
+    target:  String,
+    tree:    String,
+    parent:  String,
+    author:  String,
     message: String,
-    timestamp: time::Tm,
-    weeble: String,
-    wobble: String,
-    blockheight: String,
+    timestamp: time::Tm
 }
 
 impl Worker {
-    pub fn new(
-        id: u32,
-        //digest: sha1::Sha1,
-        target: String,
-        tree: String,
-        parent: String,
-        author: String,
-        repo: String,
-        pwd_hash: String,
-        message: String,
-        timestamp: time::Tm,
-        weeble: String,
-        wobble: String,
-        blockheight: String,
-        tx: mpsc::Sender<(u32, String, String)>,
-    ) -> Worker {
+    pub fn new(id:        u32,
+               target:    String,
+               tree:      String,
+               parent:    String,
+               author:    String,
+               message:   String,
+               timestamp: time::Tm,
+               tx:        mpsc::Sender<(u32, String, String)>) -> Worker {
         Worker {
-            id,
-            digest: sha1::Sha1::new(),
-            target,
-            tree,
-            parent,
-            author,
-            repo,
-            pwd_hash,
-            message,
-            timestamp,
-            weeble,
-            wobble,
-            blockheight,
-            tx,
+            id:        id,
+            digest:    sha1::Sha1::new(),
+            tx:        tx,
+            target:    target,
+            tree:      tree,
+            parent:    parent,
+            author:    author,
+            message:   message,
+            timestamp: timestamp
         }
     }
 
     pub fn work(&mut self) {
         let tstamp = format!("{}", self.timestamp.strftime("%s %z").unwrap());
 
-        let mut value = 0u32;
+        let mut value  = 0u32;
         loop {
             let (raw, blob) = self.generate_blob(value, &tstamp);
             let result = self.calculate(&blob);
 
             if result.starts_with(&self.target) {
-                self.tx.send((self.id, raw, result));
+                self.tx.send((self.id, raw, result)).unwrap();
                 break;
             }
 
@@ -73,62 +55,20 @@ impl Worker {
     }
 
     fn generate_blob(&mut self, value: u32, tstamp: &str) -> (String, String) {
-        if cfg!(debug_assertions) {
-            print!("self.message={}\n", self.message);
-
-            print!("self.tree={}\n", self.tree);
-            print!("self.parent={}\n", self.parent);
-            print!("self.author={}\n", self.author);
-            print!("self.author={}\n", self.author);
-            //print!("self.committer={}\n",self.committer);
-            print!("self.tree={}\n", self.tree);
-            print!("self.parent={}\n", self.parent);
-            print!("self.weeble.trim()={}\n", self.weeble.trim());
-            print!("self.blockheight.trim()={}\n", self.blockheight.trim());
-            print!("self.wobble.trim()={}\n", self.wobble.trim());
-            print!("self.id={}\n", self.id);
-            print!("self.value={}\n", value);
-            print!("self.message={}\n", self.message);
-        }
-
-        let raw = format!(
-			"tree {}\n\
-			parent {}\n\
-			author {} {}\n\
-			committer {} {}\n\n\
-            {}/{}/{}:{}\n\n\"tree\":\"{}\",\"parent\":\"{}\",\"weeble\":\"{:04}\",\"blockheight\":\"{:06}\",\"wobble\":\"{:}\",\"bit\":\"{:02}\",\"nonce\":\"{:08x}\",\"message\":\"{:}\"",
-
-            //below are in essential format
-			self.tree,
-			self.parent,
-			self.author, tstamp, //author
-			self.author, tstamp, //committer
-            //above are in essential format
-
-			//first element is commit subject line
-			self.weeble.trim(),
-			self.blockheight.trim(),
-			self.wobble.trim(),
-			self.message,
-
-            //event body
-			self.tree,
-			self.parent,
-			self.weeble.trim(),
-			self.blockheight.trim(),
-			self.wobble.trim(),
-			self.id, value,
-			self.message
-		);
-        if cfg!(debug_assertions) {
-            print!("raw={}\n", raw);
-        }
-
-        //be careful when changing - fails silently when wrong.
+        let raw = format!("tree {}\n\
+                           parent {}\n\
+                           author {} {}\n\
+                           committer {} {}\n\n\
+						   {}\n\n{:02}:{:08x}",
+                          self.tree,
+                          self.parent,
+                          self.author, tstamp,
+                          self.author, tstamp,
+                          self.message,
+                          self.id,
+                          value
+						  );
         let blob = format!("commit {}\0{}", raw.len(), raw);
-        if cfg!(debug_assertions) {
-            print!("blob={}\n", blob);
-        }
 
         (raw, blob)
     }
