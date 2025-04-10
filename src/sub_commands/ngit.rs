d1c50a7846afeeacae33b675bcf41ea203e7987e
randymcmillan <randymcmillan@protonmail.com>
2025-04-10 13:47:11 -0400

    v0.0.50
    build.rs

--- src/sub_commands/ngit.rs
+++ src/sub_commands/ngit.rs
@@ -1,15 +1,13 @@
 #![cfg_attr(not(test), warn(clippy::pedantic))]
 #![cfg_attr(not(test), warn(clippy::expect_used))]
-use clap::{Args, Parser};
-//use ngit::sub_commands::*;
-use gnostr_ngit::Cli as NgitCli;
-use gnostr_ngit::Commands as NgitCommands;
+use clap::Args;
 use gnostr_ngit::sub_commands::init;
-use gnostr_ngit::sub_commands::push;
-use gnostr_ngit::sub_commands::pull;
-use gnostr_ngit::sub_commands::send;
 use gnostr_ngit::sub_commands::list;
 use gnostr_ngit::sub_commands::login;
+use gnostr_ngit::sub_commands::pull;
+use gnostr_ngit::sub_commands::push;
+use gnostr_ngit::sub_commands::send;
+use gnostr_ngit::Commands as NgitCommands;
 use nostr_sdk::prelude::*;
 
 #[derive(Args, Debug)]
@@ -30,12 +28,7 @@ pub struct NgitSubCommand {
 }
 
 pub async fn ngit(sub_command_args: &NgitSubCommand) -> Result<()> {
-
-	//println!("args={:?}", sub_command_args);
-	let args = sub_command_args;
-    //let cli = NgitCli::parse();
-    //println!("{:?}", cli);
-	use gnostr_ngit::Commands;
+    use gnostr_ngit::Commands;
     match &sub_command_args.command {
         Commands::Login(args) => login::launch(&args).await?,
         Commands::Init(args) => init::launch(&args).await?,
@@ -46,16 +39,3 @@ pub async fn ngit(sub_command_args: &NgitSubCommand) -> Result<()> {
     }
     Ok(())
 }
-
-//#[tokio::main]
-//async fn main() -> Result<()> {
-//    let cli = Cli::parse();
-//    match &cli.command {
-//        Commands::Login(args) => sub_commands::login::launch(&cli, args).await,
-//        Commands::Init(args) => sub_commands::init::launch(&cli, args).await,
-//        Commands::Send(args) => sub_commands::send::launch(&cli, args).await,
-//        Commands::List => sub_commands::list::launch().await,
-//        Commands::Pull => sub_commands::pull::launch().await,
-//        Commands::Push(args) => sub_commands::push::launch(&cli, args).await,
-//    }
-//}
