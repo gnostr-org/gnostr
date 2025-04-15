99e70598bdb609f5da77f5659978b35acfb05c47
randymcmillan <randymcmillan@protonmail.com>
2025-04-15 16:28:36 -0400

    crates/ngit:remove

--- crates/ngit/src/git_remote_nostr/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod fetch;
-pub mod list;
-#main.rs
-pub mod push;
-pub mod utils;
--- crates/ngit/src/ngit/sub_commands/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod export_keys;
-pub mod init;
-pub mod list;
-pub mod login;
-pub mod logout;
-pub mod send;
