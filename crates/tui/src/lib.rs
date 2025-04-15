aab6699dcbcedbccbebf3997ec2ca369630151f8
randymcmillan <randymcmillan@protonmail.com>
2025-04-15 17:16:46 -0400

    crates/ngit:crates/tui
    new state

---
+++ crates/tui/src/lib.rs
@@ -0,0 +1,26 @@
+pub mod app;
+pub mod bug_report;
+pub mod components;
+pub mod notify_mutex;
+pub mod popups;
+pub mod string_utils;
+pub mod ui;
+
+pub mod args;
+pub mod clipboard;
+pub mod input;
+pub mod main;
+pub mod options;
+pub mod queue;
+pub mod strings;
+pub mod watcher;
+pub mod bin;
+pub mod cmdbar;
+pub mod keys;
+pub mod ngit;
+pub mod popup_stack;
+pub mod spinnerl
+pub mod tabs;
+
+
+
