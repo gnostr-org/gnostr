c50946d352c921abf022dcc3712f85d8832bf604
randymcmillan <randymcmillan@protonmail.com>
2025-04-19 09:20:02 -0400

    crates/tui/src/bin/ngit.rs:pre remove

--- crates/tui/src/bin/ngit.rs
+++ crates/tui/src/bin/ngit.rs
@@ -17,21 +17,21 @@ async fn main() -> Result<()> {
 	let cli = Cli::parse();
 	match &cli.command {
 		Commands::Fetch(args) => {
-			sub_commands::fetch::launch(&cli, args).await
+			sub_commands::fetch::launch(args).await
 		}
 		Commands::Login(args) => {
-			sub_commands::login::launch(&cli, args).await
+			sub_commands::login::launch(args).await
 		}
 		Commands::Init(args) => {
-			sub_commands::init::launch(&cli, args).await
+			sub_commands::init::launch(args).await
 		}
 		Commands::Send(args) => {
-			sub_commands::send::launch(&cli, args, false).await
+			sub_commands::send::launch(args, false).await
 		}
 		Commands::List => sub_commands::list::launch().await,
 		Commands::Pull => sub_commands::pull::launch().await,
 		Commands::Push(args) => {
-			sub_commands::push::launch(&cli, args).await
+			sub_commands::push::launch(args).await
 		}
 	}
 }
