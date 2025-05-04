fb767280b09949b69400608589b152fba17c3d93
randymcmillan <randymcmillan@protonmail.com>
2025-05-04 14:12:04 -0400

    src/lib/components/chat_details/style.rs
    diff --git a/src/lib/components/chat_details/style.rs b/src/lib/components/chat_details/style.rs
    index 03e49659..fdb16dae 100644
    --- a/src/lib/components/chat_details/style.rs
    +++ b/src/lib/components/chat_details/style.rs
    @@ -12,26 +12,28 @@ pub enum Detail {
         Message,
     }
    
    +//displayed in home topiclist view
     pub fn style_detail<'a>(theme: &'a SharedTheme, field: &Detail) -> Span<'a> {
         match field {
             Detail::Author => Span::styled(
    -            Cow::from(strings::commit::details_author()),
    +            Cow::from(strings::commit::chat_details_author()),
                 theme.text(false, false),
             ),
             Detail::Date => Span::styled(
    -            Cow::from(strings::commit::details_date()),
    +            Cow::from(strings::commit::chat_details_date()),
                 theme.text(false, false),
             ),
             Detail::Committer => Span::styled(
    -            Cow::from(strings::commit::details_committer()),
    +            Cow::from(strings::commit::chat_details_committer()),
                 theme.text(false, false),
             ),
    +        //
             Detail::Sha => Span::styled(
    -            Cow::from(strings::commit::details_tags()),
    +            Cow::from(strings::commit::chat_details_sha()),
                 theme.text(false, false),
             ),
             Detail::Message => Span::styled(
    -            Cow::from(strings::commit::details_message()),
    +            Cow::from(strings::commit::chat_details_message()),
                 theme.text(false, false),
             ),
         }

--- src/lib/components/chat_details/style.rs
+++ src/lib/components/chat_details/style.rs
@@ -12,26 +12,28 @@ pub enum Detail {
     Message,
 }
 
+//displayed in home topiclist view
 pub fn style_detail<'a>(theme: &'a SharedTheme, field: &Detail) -> Span<'a> {
     match field {
         Detail::Author => Span::styled(
-            Cow::from(strings::commit::details_author()),
+            Cow::from(strings::commit::chat_details_author()),
             theme.text(false, false),
         ),
         Detail::Date => Span::styled(
-            Cow::from(strings::commit::details_date()),
+            Cow::from(strings::commit::chat_details_date()),
             theme.text(false, false),
         ),
         Detail::Committer => Span::styled(
-            Cow::from(strings::commit::details_committer()),
+            Cow::from(strings::commit::chat_details_committer()),
             theme.text(false, false),
         ),
+        //
         Detail::Sha => Span::styled(
-            Cow::from(strings::commit::details_tags()),
+            Cow::from(strings::commit::chat_details_sha()),
             theme.text(false, false),
         ),
         Detail::Message => Span::styled(
-            Cow::from(strings::commit::details_message()),
+            Cow::from(strings::commit::chat_details_message()),
             theme.text(false, false),
         ),
     }
