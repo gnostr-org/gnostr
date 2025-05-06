730b7fceb4c276a30e29a6d1701d57e564c26071
randymcmillan <randymcmillan@protonmail.com>
2025-05-06 12:44:27 -0400

    make:cargo-dist

--- install_script.sh
+++ install_script.sh
@@ -26,6 +26,13 @@ rm $MAKEFILE 2>/dev/null || true
 touch $MAKEFILE
 
 tee -a $MAKEFILE <<EOF
+ifeq (\$(TAG),)
+TAG := v\$(shell cat Cargo.toml | grep 'version = "' | head -n 1 | sed 's/version = "\(.*\)".*/\1/')
+endif
+export TAG
+
+$(shell echo $TAG)
+
 help:
 	@awk 'BEGIN {FS = ":.*?## "} /^[a-zA-Z_-]+:.*?##/ {printf "\033[36m%-15s\033[0m %s\n", $\$1, $\$2}' \$(MAKEFILE_LIST)
 	@echo
@@ -98,8 +105,9 @@ cargo-run: 	### 	cargo-run
 	cargo run --bin gnostr -- -h
 
 ##===============================================================================
-cargo-dist: 	### 	cargo-dist -h
-	cargo dist -h
+cargo-dist: 	### 	make cargo-dist TAG=\$(TAG)
+	$(shell echo $TAG)
+	@dist host --steps=create --tag=\$(TAG) --allow-dirty --output-format=json > plan-dist-manifest.json
 cargo-dist-build: 	### 	cargo-dist-build
 	RUSTFLAGS="--cfg tokio_unstable" cargo dist build
 cargo-dist-manifest: 	### 	cargo dist manifest --artifacts=all
