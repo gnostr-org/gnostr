ae177d21f6b7cb58fb7fb0efafdc0a9b03f726ea
randymcmillan <randymcmillan@protonmail.com>
2025-05-17 16:52:21 -0400

    src/bin/sniper.rs:intermediate

--- src/bin/sniper.rs
+++ src/bin/sniper.rs
@@ -2,6 +2,7 @@ use futures::{stream, StreamExt};
 use reqwest::header::ACCEPT;
 use serde::{Deserialize, Serialize};
 use std::{
+    env,
     fs::File,
     io::{self, BufRead, BufReader, Write},
     path::Path,
@@ -56,8 +57,10 @@ async fn main() -> Result<(), reqwest::Error> {
     let args: Vec<String> = env::args().collect();
     let mut nip_lower: i32 = 1;
     let mut nip_upper: i32 = 1;
+    let mut first_argument = "";
+    let mut second_argument = "";
     if args.len() > 1 {
-        let first_argument = &args[1];
+        first_argument = &args[1];
 
         match first_argument.parse::<i32>() {
             Ok(number) => {
@@ -85,7 +88,7 @@ async fn main() -> Result<(), reqwest::Error> {
         }
     }
     if args.len() > 2 {
-        let second_argument = &args[2];
+        second_argument = &args[2];
 
         match second_argument.parse::<i32>() {
             Ok(number) => {
@@ -112,111 +115,147 @@ async fn main() -> Result<(), reqwest::Error> {
         }
     }
 
-    let file = "relays.yaml";
-    use std::fs::File;
-    let _file = File::create(file).expect("failed to create relays.yaml");
+    let file_path = "./relays.yaml".to_string();
 
-    let relays = load_file(file).unwrap();
-    let client = reqwest::Client::new();
-    let bodies = stream::iter(relays)
-        .map(|url| {
-            let client = &client;
-            async move {
-                let resp = client
-                    .get(&url.replace("wss://", "https://"))
-                    .header(ACCEPT, "application/nostr+json")
-                    .send()
-                    .await?;
-                let text = resp.text().await?;
+    //TODO:gnostr-sniper --refresh
+    let _gnostr_crawler = gnostr_crawler();
+    let file = File::open(file_path.clone()).expect("");
 
-                let r: Result<(String, String), reqwest::Error> = Ok((url.clone(), text.clone()));
+    let reader = io::BufReader::new(&file);
+    for line_result in reader.lines() {
+        //append_to_file(&filename, line_result.expect(""));
+        let modified_line = line_result
+            .expect("use http/https:// when querying supported nips")
+            .replace("wss://", "https://")
+            .replace("ws://", "http://");
 
-                //shitlist
-                if !url.contains("monad.jb55.com")
-                    && !url.contains("onlynotes")
-                    && !url.contains("archives")
-                    && !url.contains("relay.siamstr.com")
-                    && !url.contains("no.str")
-                    && !url.contains("multiplexer.huszonegy.world")
-                    && !url.contains("relay.0xchat.com")
-                    && !url.contains("snort.social")
-                    && !url.contains("mguy")
-                    && !url.contains("stoner.com")
-                    && !url.contains("nostr.nodeofsven.com")
-                    && !url.contains("nvote.co")
-                    && !url.contains("utxo")
-                    && !url.contains("relay.lexingtonbitcoin.org")
-                    && !url.contains("nostr.info")
-                    && !url.contains("nostr.band")
-                    && !url.contains("bitcoin.ninja")
-                    && !url.contains("brb.io")
-                    && !url.contains("nbo.angani.co")
-                    && !url.contains("nostr.relayer.se")
-                    && !url.contains("relay.nostr.nu")
-                    && !url.contains("knostr.neutrine.com")
-                    && !url.contains("nostr.easydns.ca")
-                    && !url.contains("relay.nostrgraph.net")
-                    && !url.contains("gruntwerk.org")
-                    && !url.contains("nostr.noones.com")
-                    && !url.contains("relay.nonce.academy")
-                    && !url.contains("relay.r3d.red")
-                    && !url.contains("nostr.bitcoiner.social")
-                    && !url.contains("btc.klendazu.com")
-                    && !url.contains("vulpem.com")
-                    && !url.contains("bch.ninja")
-                    && !url.contains("sg.qemura.xyz")
-                    && !url.contains("relay.schnitzel.world")
-                    && !url.contains("nostr.datamagik.com")
-                    && !url.contains("nostrid")
-                    && !url.contains("damus.io")
-                    && !url.contains(".local")
-                {
-                    //we want a view of the network
+        debug!("132:{}", modified_line);
+        let mut file = File::create(file_path.clone() + ".txt")
+            .expect("create relays.yaml.txt and modify protocol (ws/wss to http/https)");
+        if !modified_line.contains("monad.jb55.com")
+            && !modified_line.contains("onlynotes")
+            && !modified_line.contains("archives")
+            && !modified_line.contains("relay.siamstr.com")
+            && !modified_line.contains("no.str")
+            && !modified_line.contains("multiplexer.huszonegy.world")
+            && !modified_line.contains("relay.0xchat.com")
+            && !modified_line.contains("snort.social")
+            && !modified_line.contains("mguy")
+            && !modified_line.contains("stoner.com")
+            && !modified_line.contains("nostr.nodeofsven.com")
+            && !modified_line.contains("nvote.co")
+            && !modified_line.contains("utxo")
+            && !modified_line.contains("relay.lexingtonbitcoin.org")
+            && !modified_line.contains("nostr.info")
+            && !modified_line.contains("nostr.band")
+            && !modified_line.contains("bitcoin.ninja")
+            && !modified_line.contains("brb.io")
+            && !modified_line.contains("nbo.angani.co")
+            && !modified_line.contains("nostr.relayer.se")
+            && !modified_line.contains("relay.nostr.nu")
+            && !modified_line.contains("knostr.neutrine.com")
+            && !modified_line.contains("nostr.easydns.ca")
+            && !modified_line.contains("relay.nostrgraph.net")
+            && !modified_line.contains("gruntwerk.org")
+            && !modified_line.contains("nostr.noones.com")
+            && !modified_line.contains("relay.nonce.academy")
+            && !modified_line.contains("relay.r3d.red")
+            && !modified_line.contains("nostr.bitcoiner.social")
+            && !modified_line.contains("btc.klendazu.com")
+            && !modified_line.contains("vulpem.com")
+            && !modified_line.contains("bch.ninja")
+            && !modified_line.contains("sg.qemura.xyz")
+            && !modified_line.contains("relay.schnitzel.world")
+            && !modified_line.contains("nostr.datamagik.com")
+            && !modified_line.contains("nostrid")
+            && !modified_line.contains("damus.io")
+            && !modified_line.contains(".local")
+            && !modified_line.contains("onlynotes")
+            && !modified_line.contains("archives")
+            && !modified_line.contains("relay.siamstr.com")
+            && !modified_line.contains("no.str")
+            && !modified_line.contains("multiplexer.huszonegy.world")
+            && !modified_line.contains("relay.0xchat.com")
+            && !modified_line.contains("snort.social")
+            && !modified_line.contains("mguy")
+            && !modified_line.contains("stoner.com")
+            && !modified_line.contains("nostr.info")
+            && !modified_line.contains(".local")
+        //we want a view of the network
+        {
+            file.write(modified_line.as_bytes()).expect("");
+            //file.write(b"\n").expect("");
+        }
+        ////writeln!(file, "{}", line).expect("");
+
+        let file_path = "./relays.yaml.txt".to_string();
+
+        //}
+
+        let relays = load_file(&file_path).unwrap();
+        // Nip you are looking for on relays
+        //println!("{:?}", relays);
+        let nip = 11;
+
+        let client = reqwest::Client::new();
+        let bodies = stream::iter(relays)
+            .map(|url| {
+                let client = &client;
+                async move {
+                    let resp = client
+                        .get(&url)
+                        .header(ACCEPT, "application/nostr+json")
+                        .send()
+                        .await?;
+                    let text = resp.text().await?;
+
+                    let r: Result<(String, String), reqwest::Error> =
+                        Ok((url.clone(), text.clone()));
+                    //tracing::info!("{:?}", r);
+                    //print!("{{\"relay\":\"{}\",", url);
+                    debug!("{}", text);
+                    r
                 }
-                r
-            }
-        })
-        .buffer_unordered(CONCURRENT_REQUESTS);
+            })
+            .buffer_unordered(CONCURRENT_REQUESTS);
 
-    bodies
-        .for_each(|b| async {
-            if let Ok((url, json)) = b {
-                let data: Result<Relay, serde_json::Error> = serde_json::from_str(&json);
-                if let Ok(relay_info) = data {
-                    for n in &relay_info.supported_nips {
-                        if n == &nip_lower.clone() {
-                            debug!("contact:{:?}", &relay_info.contact);
-                            debug!("description:{:?}", &relay_info.description);
-                            debug!("name:{:?}", &relay_info.name);
-                            debug!("software:{:?}", &relay_info.software);
-                            debug!("version:{:?}", &relay_info.version);
-
-                            let dir_name = format!("{}", nip_lower);
-                            let path = Path::new(&dir_name);
-
-                            if !path.exists() {
-                                match fs::create_dir(path) {
-                                    Ok(_) => debug!("created ./{}", nip_lower),
-                                    Err(e) => eprintln!("Error creating directory: {}", e),
-                                }
+        bodies
+            .for_each(|b| async {
+                if let Ok((url, json)) = b {
+                    let data: Result<Relay, serde_json::Error> = serde_json::from_str(&json);
+                    if let Ok(json) = data {
+                        print!("{{\"227:url\":\"{}\",", url.replace("https", "wss"));
+                        print!("\"228:supported_nips\":[");
+                        //debug!("len:{} ", json.supported_nips.len());
+                        let mut nip_count = json.supported_nips.len();
+                        for n in &json.supported_nips {
+                            //debug!("nip_count:{}", nip_count);
+                            if nip_count > 1 {
+                                print!("{:<2}", format!("{},", n));
                             } else {
-                                debug!("{dir_name} already exists...");
+                                print!("{:<2}", format!("{}", n));
+                            }
+                            if n == &nip {
+                                debug!("{} Supports nip{nip}", url);
                             }
 
+                            //print!("242:{} ", file_path);
                             let file_name = url
                                 .replace("https://", "")
                                 .replace("http://", "")
                                 .replace("wss://", "")
                                 .replace("ws://", "")
                                 + ".json";
-                            let file_path = path.join(&file_name);
-                            let file_path_str = file_path.display().to_string();
-                            debug!("{}", file_path_str);
+                            let file_path = file_path.clone() + &file_name;
+                            //print!("250:{} ", file_path);
+                            let file_path_str = file_path.to_string();
+                            //print!("252:{} ", file_path_str);
 
-                            match fs::File::create(&file_path) {
+                            use tracing::error;
+                            match File::create(&file_path) {
                                 Ok(mut file) => {
-                                    debug!("{}", &file_path_str);
-                                    match file.write_all(json.as_bytes()) {
+                                    debug!("257:{}", &file_path_str);
+                                    match file.write_all(json.contact.as_bytes()) {
                                         Ok(_) => debug!("wrote relay metadata:{}", &file_path_str),
                                         Err(e) => {
                                             error!("Failed to write to {}: {}", &file_path_str, e)
@@ -225,20 +264,18 @@ async fn main() -> Result<(), reqwest::Error> {
                                 }
                                 Err(e) => error!("Failed to create file {}: {}", &file_path_str, e),
                             }
-
-                            println!(
-                                "{}/{}",
-                                nip_lower,
-                                url.replace("https://", "")
-                                    .replace("wss://", "")
-                                    .replace("ws://", "")
-                            );
+                            //print!("]}}");
+                            //println!();
+                            nip_count = nip_count - 1;
                         }
+                        print!("]}}");
+                        println!();
+                        //nip_count = nip_count - 1;
                     }
                 }
-            }
-        })
-        .await;
-
+            })
+            .await;
+        //}
+    }
     Ok(())
 }
