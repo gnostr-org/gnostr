34c5fd175d89bf56154c77cb9d72fa73aea1b1cb
randymcmillan <randymcmillan@protonmail.com>
2025-05-30 18:17:31 -0400

    src/bin/git-ssh.rs:initial config

---
+++ src/lib/ssh/mod.rs
@@ -0,0 +1,23 @@
+use log::info;
+use state::State;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+
+mod config;
+mod git;
+mod site;
+mod ssh;
+mod state;
+mod utils;
+mod vars;
+
+pub async fn start() -> anyhow::Result<()> {
+    info!("Loading state...");
+    let state = State::new().await?;
+    let state = Arc::new(Mutex::new(state));
+
+    info!("Starting server...");
+    let _ = sd_notify::notify(true, &[sd_notify::NotifyState::Ready]);
+    ssh::start_server(state).await?;
+    Ok(())
+}
