c27e55a9d89f5de20ad7dac3ca292df7c4e3bd38
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2025-06-13 19:58:35 -0400

    tests:src:cargo t passing

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -96,7 +96,8 @@ use directories::ProjectDirs;
 
 ///
 pub fn get_dirs() -> Result<ProjectDirs> {
-    ProjectDirs::from("", "", "ngit").ok_or(anyhow!(
+	//maintain compat with ngit
+    ProjectDirs::from("ngit", "gnostr", ".gnostr").ok_or(anyhow!(
         "should find operating system home directories with rust-directories crate"
     ))
 }
