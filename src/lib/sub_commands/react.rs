0e9ff6a6cabe5faddeb28382dc16b42abd612ce6
randymcmillan <randymcmillan@protonmail.com>
2025-06-17 17:58:18 -0400

    src/lib/sub_commands/react.rs:relays = BOOTSTRAP_RELAYS.to_vec();

--- src/lib/sub_commands/react.rs
+++ src/lib/sub_commands/react.rs
@@ -4,6 +4,7 @@ use std::time::Duration;
 use clap::Args;
 use nostr_sdk_0_32_0::prelude::*;
 
+use gnostr_crawler::processor::BOOTSTRAP_RELAYS;
 use crate::utils::{create_client, parse_private_key};
 
 #[derive(Args)]
@@ -24,12 +25,12 @@ pub struct ReactionSubCommand {
 
 pub async fn react_to_event(
     private_key: Option<String>,
-    relays: Vec<String>,
+    mut relays: Vec<String>,
     difficulty_target: u8,
     sub_command_args: &ReactionSubCommand,
 ) -> Result<()> {
     if relays.is_empty() {
-        panic!("No relays specified, at least one relay is required!")
+        relays = BOOTSTRAP_RELAYS.to_vec();
     }
 
     let keys = parse_private_key(private_key, false).await?;
@@ -49,7 +50,7 @@ pub async fn react_to_event(
         subscription = Filter::new().event(event_id);
     }
 
-    //println!("{:?}", subscription);
+    println!("{:?}", subscription);
     let events = client
         .get_events_of_with_opts(
             vec![subscription],
