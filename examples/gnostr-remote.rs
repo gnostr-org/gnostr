35b8d6f7920cb25878ffbc46183e9a2951515143
randymcmillan <randymcmillan@protonmail.com>
2025-06-17 12:26:22 -0400

    src/lib/remote:poc for distributed/decent CI service

---
+++ examples/gnostr-remote.rs
@@ -0,0 +1,40 @@
+use gnostr::remote::host;
+use gnostr::remote::message_stream;
+use gnostr::remote::messages;
+use gnostr::remote::options;
+use gnostr::remote::remote_runner;
+use gnostr::remote::tests;
+use clap::Parser;
+
+use gnostr::remote::options::Opt;
+use anyhow::Result;
+use log::LevelFilter;
+use simple_logger::SimpleLogger;
+
+fn main() -> Result<()> {
+    let opt = Opt::parse();
+
+    let level = match opt.log_level.as_str() {
+        "error" => LevelFilter::Error,
+        "warn" => LevelFilter::Warn,
+        "info" => LevelFilter::Info,
+        "debug" => LevelFilter::Debug,
+        "trace" => LevelFilter::Trace,
+        _ => LevelFilter::Warn,
+    };
+
+    SimpleLogger::new()
+        .with_level(level)
+        .with_colors(true)
+        .init()?;
+
+    if opt.remote_runner {
+        println!("Starting remote-runner");
+        remote_runner::update(&opt);
+    } else {
+        println!("Starting host");
+        host::host_loop(&opt, opt.target.as_ref().unwrap())?;
+    }
+
+    Ok(())
+}
