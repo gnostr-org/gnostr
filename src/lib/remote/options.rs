52586b5f8e4143522f50eb799c1011bd00e12253
randymcmillan <randymcmillan@protonmail.com>
2025-06-18 13:52:25 -0400

    src/lib/remote/options.rs:clap config

--- src/lib/remote/options.rs
+++ src/lib/remote/options.rs
@@ -1,5 +1,5 @@
+pub use clap::value_parser;
 pub use clap::Parser;
-
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Opt {
@@ -8,13 +8,13 @@ pub struct Opt {
     /// Supported levels: error, warn, info, debug, trace
     pub log_level: String,
     #[arg(short, long)]
-    /// Run as remote runner. This is the machine where the executable(s) will be run.
-    #[arg(short, long, default_value = "true")]
+    /// Run as remote runner (default). This is the machine where the executable(s) will be run.
+    #[arg(long, default_value_t = false, value_parser = value_parser!(bool))]
     pub remote_runner: bool,
-    #[arg(short, long, default_value = "8888")]
+    #[arg(short, long, default_value_t = 8888)]
     /// Select a TCP port to talk over. Has to be same on both sides.
     pub port: u16,
-    #[arg(short, long)]
+    #[arg(short, long, default_value = "127.0.0.1")]
     /// The remote runner to connect to.
     pub target: Option<String>,
     #[arg(short, long)]
