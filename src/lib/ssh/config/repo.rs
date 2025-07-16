4b2a5c9d7f90ac26f55e11f1093ab483adf537a4
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2025-07-16 08:19:44 -0400

    src/lib/ssh:logging config

--- src/lib/ssh/config/repo.rs
+++ src/lib/ssh/config/repo.rs
@@ -10,6 +10,7 @@ use toml::Table;
 
 use crate::{ssh::git::Repo, ssh::vars::*};
 use dirs::home_dir;
+use log::{debug, info};
 
 #[derive(Serialize, Deserialize)]
 pub struct RepoConfig {
@@ -24,14 +25,14 @@ pub struct RepoConfig {
 fn _get_config_file_path() {
     if let Some(config_file) = dirs::home_dir() {
         let config_file_path = home_dir().expect("REASON").join(&REPO_CONFIG_FILE);
-        println!("Full path to config file: {:?}", config_file_path);
+        info!("Using config file: {:?}", config_file_path);
 
         // You can then use `config_file_path` to read or write to the file.
         // For example, to check if it exists:
         if config_file_path.exists() {
-            println!("Config file exists!");
+            info!("Config file exists!");
         } else {
-            println!("Config file does not exist.");
+            info!("Config file does not exist.");
         }
     } else {
         eprintln!("Error: Could not determine home directory.");
@@ -40,16 +41,21 @@ fn _get_config_file_path() {
 
 pub async fn load_repo_config(repo_path: &Path) -> anyhow::Result<RepoConfig> {
     let config_file_path = home_dir().expect("REASON").join(&REPO_CONFIG_FILE);
-    println!("Full path to config file: {:?}", config_file_path);
+    info!("Using config file: {:?}", config_file_path);
 
     if config_file_path.exists() {
-        println!("Config file exists!");
+        debug!("Config file exists!");
     } else {
-        println!("Config file does not exist.");
+        debug!("Config file does not exist.");
     }
 
     let temp_dir = tempdir()?;
     let clone_dir = temp_dir.path().join(repo_path);
+    info!(
+        "cloning:repo_path:{} to clone_dir:{}",
+        repo_path.display(),
+        &clone_dir.display()
+    );
     Repo::clone(repo_path, &clone_dir).await?;
 
     let text =
@@ -58,6 +64,11 @@ pub async fn load_repo_config(repo_path: &Path) -> anyhow::Result<RepoConfig> {
 }
 
 pub async fn new_repo_config(repo_path: &Path, username: &str) -> anyhow::Result<()> {
+    debug!(
+        "new_repo_config:repo_path:{} username:{}",
+        repo_path.display(),
+        username
+    );
     let config_name = PathBuf::from(REPO_CONFIG_FILE);
 
     let temp_dir = tempdir()?;
