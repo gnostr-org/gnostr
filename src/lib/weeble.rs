3635e0c8a8deeeaf256e5853e9ac77f075c457dd
randymcmillan <randymcmillan@protonmail.com>
2025-07-17 20:39:49 -0400

    src/lib/mod.rs:weeble/wobble async

--- src/lib/weeble.rs
+++ src/lib/weeble.rs
@@ -1,4 +1,5 @@
 use log::debug;
+use reqwest;
 use reqwest::Url;
 use std::io::Read;
 use std::time::SystemTime;
@@ -19,6 +20,33 @@ pub fn weeble() -> Result<f64, ascii::AsciiChar> {
 
     let mut tmp_string = String::new();
     res.read_to_string(&mut tmp_string).unwrap();
+    let tmp_u64 = tmp_string.parse::<u64>().unwrap_or(0);
+
+    //TODO:impl gnostr-weeble_millis
+    //gnostr-chat uses millis
+    //let weeble = now_millis as f64 / tmp_u64 as f64;
+    let weeble = seconds as f64 / tmp_u64 as f64;
+    return Ok(weeble.floor());
+}
+/// pub async fn weeble_async() -> Result<f64, ascii::AsciiChar>
+///
+pub async fn weeble_async() -> Result<f64, ascii::AsciiChar> {
+    //! weeble = utc_secs / blockheight
+    let since_the_epoch = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .expect("get millis error");
+    let seconds = since_the_epoch.as_secs();
+    let subsec_millis = since_the_epoch.subsec_millis() as u64;
+    let _now_millis = seconds * 1000 + subsec_millis;
+    debug!("now millis: {}", seconds * 1000 + subsec_millis);
+
+    let url = Url::parse("https://mempool.space/api/blocks/tip/height").unwrap();
+    let res = reqwest::get(url)
+        .await
+        .map_err(|_| ascii::AsciiChar::Null)?; // Error handling
+
+    let tmp_string = res.text().await.map_err(|_| ascii::AsciiChar::Null)?; // Error handling
+
     let tmp_u64 = tmp_string.parse::<u64>().unwrap_or(0);
 
     //TODO:impl gnostr-weeble_millis
--- src/lib/wobble.rs
+++ src/lib/wobble.rs
@@ -1,11 +1,12 @@
 use log::debug;
+use reqwest;
 use reqwest::Url;
 use std::io::Read;
 use std::time::SystemTime;
 /// pub fn wobble() -> Result<f64, ascii::AsciiChar>
 ///
 pub fn wobble() -> Result<f64, ascii::AsciiChar> {
-    //! wobble = utc_secs / blockheight
+    //! wobble = utc_secs % blockheight
     let since_the_epoch = SystemTime::now()
         .duration_since(SystemTime::UNIX_EPOCH)
         .expect("get millis error");
@@ -27,10 +28,39 @@ pub fn wobble() -> Result<f64, ascii::AsciiChar> {
     let wobble = seconds as f64 % tmp_u64 as f64;
     return Ok(wobble.floor());
 }
+/// pub async fn wobble_async() -> Result<f64, ascii::AsciiChar>
+///
+pub async fn wobble_async() -> Result<f64, ascii::AsciiChar> {
+    //! wobble = utc_secs % blockheight
+    let since_the_epoch = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .expect("get millis error");
+    let seconds = since_the_epoch.as_secs();
+    let subsec_millis = since_the_epoch.subsec_millis() as u64;
+    let _now_millis = seconds * 1000 + subsec_millis;
+    debug!("now millis: {}", seconds * 1000 + subsec_millis);
+
+    let url = Url::parse("https://mempool.space/api/blocks/tip/height").unwrap();
+
+    let res = reqwest::get(url)
+        .await
+        .map_err(|_| ascii::AsciiChar::Null)?; // Error handling
+
+    // Use .text().await to read the body asynchronously
+    let tmp_string = res.text().await.map_err(|_| ascii::AsciiChar::Null)?; // Error handling
+
+    let tmp_u64 = tmp_string.parse::<u64>().unwrap_or(0);
+
+    //TODO:impl gnostr-wobble_millis
+    //gnostr-chat uses millis
+    //let wobble = now_millis as f64 % tmp_u64 as f64;
+    let wobble = seconds as f64 % tmp_u64 as f64;
+    return Ok(wobble.floor());
+}
 /// pub fn wobble_millis() -> Result<f64, ascii::AsciiChar>
 ///
 pub fn wobble_millis() -> Result<f64, ascii::AsciiChar> {
-    //! wobble = utc_secs / blockheight
+    //! wobble = utc_secs % blockheight
     let since_the_epoch = SystemTime::now()
         .duration_since(SystemTime::UNIX_EPOCH)
         .expect("get millis error");
