1526053b907a48b5835fc3d10e82d89b1f38451e
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2025-07-22 08:03:55 -0400

    src/lib/utils.rs:cfg(test)

--- src/lib/utils.rs
+++ src/lib/utils.rs
@@ -142,61 +142,6 @@ pub fn byte_array_to_hex_string(byte_array: &[u8; 32]) -> String {
     hex_string
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[tokio::test]
-    async fn test_parse_key_hex_input() {
-        let hex_key =
-            String::from("f4deaad98b61fa24d86ef315f1d5d57c1a6a533e1e87e777e5d0b48dcd332cdb");
-        let result = parse_key_or_id_to_hex_string(hex_key.clone()).await;
-
-        assert!(result.is_ok());
-        assert_eq!(result.unwrap(), hex_key);
-    }
-
-    #[tokio::test]
-    async fn test_parse_key_bech32_note_input() {
-        let bech32_note_id =
-            String::from("note1h445ule4je70k7kvddate8kpsh2fd6n77esevww5hmgda2qwssjsw957wk");
-
-        let result = parse_key_or_id_to_hex_string(bech32_note_id).await;
-
-        assert!(result.is_ok());
-        assert_eq!(
-            result.unwrap(),
-            String::from("bd6b4e7f35967cfb7acc6b7abc9ec185d496ea7ef6619639d4bed0dea80e8425")
-        );
-    }
-
-    #[tokio::test]
-    async fn test_parse_bech32_public_key_input() {
-        let bech32_encoded_key =
-            String::from("npub1ktt8phjnkfmfrsxrgqpztdjuxk3x6psf80xyray0l3c7pyrln49qhkyhz0");
-        let result = parse_key_or_id_to_hex_string(bech32_encoded_key).await;
-
-        assert!(result.is_ok());
-        assert_eq!(
-            result.unwrap(),
-            String::from("b2d670de53b27691c0c3400225b65c35a26d06093bcc41f48ffc71e0907f9d4a")
-        );
-    }
-
-    #[tokio::test]
-    async fn test_parse_bech32_private_key() {
-        let bech32_encoded_key =
-            String::from("nsec1hdeqm0y8vgzuucqv4840h7rlpy4qfu928ulxh3dzj6s2nqupdtzqagtew3");
-        let result = parse_key_or_id_to_hex_string(bech32_encoded_key).await;
-
-        assert!(result.is_ok());
-        assert_eq!(
-            result.unwrap(),
-            String::from("bb720dbc876205ce600ca9eafbf87f092a04f0aa3f3e6bc5a296a0a983816ac4")
-        );
-    }
-}
-
 /// Synchronous HTTP request using ureq.
 /// Handles errors gracefully instead of panicking.
 pub fn ureq_sync(url: String) -> Result<String, String> {
@@ -329,5 +274,55 @@ mod tests {
         let result = ureq_sync(url);
         assert!(result.is_err());
         println!("Sync failure error: {:?}", result.err());
+    }
+
+    #[tokio::test]
+    async fn test_parse_key_hex_input() {
+        let hex_key =
+            String::from("f4deaad98b61fa24d86ef315f1d5d57c1a6a533e1e87e777e5d0b48dcd332cdb");
+        let result = parse_key_or_id_to_hex_string(hex_key.clone()).await;
+
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), hex_key);
+    }
+
+    #[tokio::test]
+    async fn test_parse_key_bech32_note_input() {
+        let bech32_note_id =
+            String::from("note1h445ule4je70k7kvddate8kpsh2fd6n77esevww5hmgda2qwssjsw957wk");
+
+        let result = parse_key_or_id_to_hex_string(bech32_note_id).await;
+
+        assert!(result.is_ok());
+        assert_eq!(
+            result.unwrap(),
+            String::from("bd6b4e7f35967cfb7acc6b7abc9ec185d496ea7ef6619639d4bed0dea80e8425")
+        );
+    }
+
+    #[tokio::test]
+    async fn test_parse_bech32_public_key_input() {
+        let bech32_encoded_key =
+            String::from("npub1ktt8phjnkfmfrsxrgqpztdjuxk3x6psf80xyray0l3c7pyrln49qhkyhz0");
+        let result = parse_key_or_id_to_hex_string(bech32_encoded_key).await;
+
+        assert!(result.is_ok());
+        assert_eq!(
+            result.unwrap(),
+            String::from("b2d670de53b27691c0c3400225b65c35a26d06093bcc41f48ffc71e0907f9d4a")
+        );
+    }
+
+    #[tokio::test]
+    async fn test_parse_bech32_private_key() {
+        let bech32_encoded_key =
+            String::from("nsec1hdeqm0y8vgzuucqv4840h7rlpy4qfu928ulxh3dzj6s2nqupdtzqagtew3");
+        let result = parse_key_or_id_to_hex_string(bech32_encoded_key).await;
+
+        assert!(result.is_ok());
+        assert_eq!(
+            result.unwrap(),
+            String::from("bb720dbc876205ce600ca9eafbf87f092a04f0aa3f3e6bc5a296a0a983816ac4")
+        );
     }
 }
