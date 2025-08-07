7072df087136805500fcea6e322b048f0b9a06e2
randymcmillan <randymcmillan@protonmail.com>
2025-08-07 17:31:07 -0400

    crawler:test commit size for dist-commit-list

---
+++ crawler/src/main.rs
@@ -0,0 +1,44 @@
+use gnostr_crawler::processor::Processor;
+use gnostr_crawler::processor::APP_SECRET_KEY;
+use gnostr_crawler::processor::BOOTSTRAP_RELAY1;
+use gnostr_crawler::processor::BOOTSTRAP_RELAY2;
+use gnostr_crawler::processor::BOOTSTRAP_RELAY3;
+//use gnostr_crawler::processor::BOOTSTRAP_RELAY4;
+
+use gnostr_crawler::processor::BOOTSTRAP_RELAYS;
+
+use gnostr_crawler::relay_manager::RelayManager;
+use gnostr_crawler::CliArgs;
+
+use clap::Parser;
+use nostr_sdk::prelude::{FromBech32, Keys, SecretKey};
+
+#[tokio::main]
+async fn main() {
+    //env_logger::init();
+    let args = CliArgs::parse();
+
+    match gnostr_crawler::run(&args) {
+        Ok(()) => {
+            let app_secret_key = SecretKey::from_bech32(APP_SECRET_KEY);
+            let app_keys = Keys::new(app_secret_key.expect("REASON"));
+            let processor = Processor::new();
+            let mut relay_manager = RelayManager::new(app_keys, processor);
+
+            let _ = relay_manager
+                .run(vec![
+                    BOOTSTRAP_RELAY1,
+                    BOOTSTRAP_RELAY2,
+                    BOOTSTRAP_RELAY3,
+                    BOOTSTRAP_RELAYS
+                        .get(3)
+                        .expect("BOOTSTRAP_RELAYS should have at least 4 elements")
+                        .as_str(),
+                ])
+                .await;
+
+            relay_manager.processor.dump();
+        }
+        Err(e) => println!("error: {}", e),
+    }
+}
---
+++ crawler/src/relays.rs
@@ -0,0 +1,87 @@
+//use log::info;
+//use log::{debug, trace};
+use nostr_sdk::prelude::Url;
+use std::collections::HashSet;
+
+/// Maintain a list of all encountered relays
+pub struct Relays {
+    r: HashSet<Url>,
+}
+
+impl Default for Relays {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Relays {
+    pub fn new() -> Self {
+        Self {
+            r: HashSet::default(),
+        }
+    }
+
+    pub fn add(&mut self, s1: &str) -> bool {
+        let mut res = false;
+        if let Ok(u) = Url::parse(s1) {
+            res = self.r.insert(u);
+            if res {
+                self.print();
+            }
+        }
+        res
+    }
+
+    pub fn count(&self) -> usize {
+        self.r.len()
+    }
+
+    pub fn de_dup(&self, list: &Vec<Url>) -> Vec<Url> {
+        let list: Vec<Url> = list.clone().into_iter().collect();
+        list
+    }
+
+    pub fn get_some(&self, max_count: usize) -> Vec<Url> {
+        let mut res = Vec::new();
+        for u in &self.r {
+            res.push(u.clone());
+            if res.len() >= max_count {
+                return res;
+            }
+        }
+        res = self.de_dup(&res);
+        res
+    }
+
+    pub fn print(&self) {
+        for u in &self.r {
+            let mut relay = format!("{}", u.to_string());
+            if relay.ends_with('/') {
+                relay.pop();
+                println!("{}", relay);
+            } else {
+                println!("{}", relay);
+            }
+        }
+    }
+
+    pub fn dump_json_object(&self) {
+        let mut count = 0;
+        print!("[\"RELAYS\",");
+        for u in &self.r {
+            print!("{{\"{}\":\"{}\"}},", count, u);
+            count += 1;
+        }
+        print!("{{\"{}\":\"wss://relay.gnostr.org\"}}", count);
+        print!("]");
+    }
+
+    pub fn dump_list(&self) {
+        let mut count = 0;
+        for u in &self.r {
+            print!("78:{{\"{}\":\"{}\"}}", count, u);
+            count += 1;
+        }
+        print!("{{\"{}\":\"wss://relay.gnostr.org\"}}", count);
+    }
+}
