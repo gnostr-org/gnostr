f9a2d58691a355de8e16487b666c6acda941e2a1
randymcmillan <randymcmillan@protonmail.com>
2025-08-07 08:47:55 -0400

    branch.rs

---
+++ asyncgit/src/gitui/ops/branch.rs
@@ -0,0 +1,136 @@
+use super::{selected_rev, Action, OpTrait};
+use crate::{
+    gitui::git::{get_current_branch_name, is_branch_merged},
+    gitui::gitui_error::Error,
+    gitui::items::TargetData,
+    gitui::menu::arg::Arg,
+    gitui::state::{PromptParams, State},
+    gitui::term::Term,
+    gitui::Res,
+};
+use std::{process::Command, rc::Rc};
+
+pub(crate) fn init_args() -> Vec<Arg> {
+    vec![]
+}
+
+pub(crate) struct Checkout;
+impl OpTrait for Checkout {
+    fn get_action(&self, _target: Option<&TargetData>) -> Option<Action> {
+        Some(Rc::new(move |state: &mut State, term: &mut Term| {
+            let rev = state.prompt(
+                term,
+                &PromptParams {
+                    prompt: "Checkout",
+                    create_default_value: Box::new(selected_rev),
+                    ..Default::default()
+                },
+            )?;
+
+            checkout(state, term, &rev)?;
+            Ok(())
+        }))
+    }
+
+    fn display(&self, _state: &State) -> String {
+        "Checkout branch/revision".into()
+    }
+}
+
+fn checkout(state: &mut State, term: &mut Term, rev: &str) -> Res<()> {
+    let mut cmd = Command::new("git");
+    cmd.args(["checkout", rev]);
+
+    state.close_menu();
+    state.run_cmd(term, &[], cmd)?;
+    Ok(())
+}
+
+pub(crate) struct CheckoutNewBranch;
+impl OpTrait for CheckoutNewBranch {
+    fn get_action(&self, _target: Option<&TargetData>) -> Option<Action> {
+        Some(Rc::new(|state: &mut State, term: &mut Term| {
+            let branch_name = state.prompt(
+                term,
+                &PromptParams {
+                    prompt: "Create and checkout branch:",
+                    ..Default::default()
+                },
+            )?;
+
+            checkout_new_branch_prompt_update(state, term, &branch_name)?;
+            Ok(())
+        }))
+    }
+
+    fn display(&self, _state: &State) -> String {
+        "Checkout new branch".into()
+    }
+}
+
+fn checkout_new_branch_prompt_update(
+    state: &mut State,
+    term: &mut Term,
+    branch_name: &str,
+) -> Res<()> {
+    let mut cmd = Command::new("git");
+    cmd.args(["checkout", "-b", branch_name]);
+
+    state.close_menu();
+    state.run_cmd(term, &[], cmd)?;
+    Ok(())
+}
+
+pub(crate) struct Delete;
+impl OpTrait for Delete {
+    fn get_action(&self, target: Option<&TargetData>) -> Option<Action> {
+        let default = match target {
+            Some(TargetData::Branch(b)) => Some(b.clone()),
+            _ => None,
+        };
+
+        Some(Rc::new(move |state: &mut State, term: &mut Term| {
+            let default = default.clone();
+
+            let branch_name = state.prompt(
+                term,
+                &PromptParams {
+                    prompt: "Delete",
+                    create_default_value: Box::new(move |_| default.clone()),
+                    ..Default::default()
+                },
+            )?;
+
+            delete(state, term, &branch_name)?;
+            Ok(())
+        }))
+    }
+
+    fn display(&self, _state: &State) -> String {
+        "Delete branch".into()
+    }
+}
+
+fn delete(state: &mut State, term: &mut Term, branch_name: &str) -> Res<()> {
+    if branch_name.is_empty() {
+        return Err(Error::BranchNameRequired);
+    }
+
+    if get_current_branch_name(&state.repo).unwrap() == branch_name {
+        return Err(Error::CannotDeleteCurrentBranch);
+    }
+
+    let mut cmd = Command::new("git");
+    cmd.args(["branch", "-d"]);
+
+    if !is_branch_merged(&state.repo, branch_name).unwrap_or(false) {
+        state.confirm(term, "Branch is not fully merged. Really delete? (y or n)")?;
+        cmd.arg("-f");
+    }
+
+    cmd.arg(branch_name);
+
+    state.close_menu();
+    state.run_cmd(term, &[], cmd)?;
+    Ok(())
+}
