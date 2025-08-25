226c55adad3529aa11c22d16d6ccb5f3af21df34
randymcmillan <randymcmillan@protonmail.com>
2025-08-25 13:36:15 -0400

    src/lib/sub_commands/convert_key.rs:to_hex:default true

--- src/lib/sub_commands/convert_key.rs
+++ src/lib/sub_commands/convert_key.rs
@@ -12,16 +12,12 @@ pub struct ConvertKeySubCommand {
     #[arg(short, long)]
     prefix: Option<Prefix>,
     /// Set to true if you're converting from bech32 to hex
-    #[arg(short, long, default_value = "false")]
+    #[arg(short, long, default_value = "true")]
     to_hex: bool,
 }
 
 pub async fn convert_key(sub_command_args: &ConvertKeySubCommand) -> Result<()> {
-    if sub_command_args.to_hex {
-        // Input is bech32 encoded so we find the hex value
-        let hex_key_or_id = parse_key_or_id_to_hex_string(sub_command_args.key.clone()).await?;
-        print!("{hex_key_or_id}");
-    } else {
+    if sub_command_args.prefix.is_some() {
         // Input is hex so we bech32 encode it based on the provided prefix value
         let encoded_key: String = match sub_command_args
             .prefix
@@ -33,6 +29,10 @@ pub async fn convert_key(sub_command_args: &ConvertKeySubCommand) -> Result<()> 
             Prefix::Note => EventId::from_str(sub_command_args.key.as_str())?.to_bech32()?,
         };
         print!("{encoded_key}");
+    } else {
+        // Input is bech32 encoded so we find the hex value
+        let hex_key_or_id = parse_key_or_id_to_hex_string(sub_command_args.key.clone()).await?;
+        print!("{hex_key_or_id}");
     }
 
     Ok(())
