398cfa1a605dfa244b427f054872ba1b1ebec83f
randymcmillan <randymcmillan@protonmail.com>
2025-08-26 13:39:23 -0400

    asyncgit/src/sync/reword.rs:fix lifetime

--- asyncgit/src/sync/reword.rs
+++ asyncgit/src/sync/reword.rs
@@ -59,7 +59,7 @@ pub fn reword(repo_path: &RepoPath, commit: CommitId, message: &str) -> Result<C
 /// Gets the current branch the user is on.
 /// Returns none if they are not on a branch
 /// and Err if there was a problem finding the branch
-fn get_current_branch(repo: &Repository) -> Result<Option<git2::Branch>> {
+fn get_current_branch(repo: &Repository) -> Result<Option<git2::Branch<'_>>> {
     for b in repo.branches(None)? {
         let branch = b?.0;
         if branch.is_head() {
