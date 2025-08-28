a286163d50b7c92707b790dd66f5e1b4b42143eb
randymcmillan <randymcmillan@protonmail.com>
2025-08-27 21:33:26 -0400

    src/lib/utils/temp_repo.rs:debug & apply cargo fmt

--- src/lib/utils/temp_repo.rs
+++ src/lib/utils/temp_repo.rs
@@ -2,13 +2,14 @@ use git2::{Repository, Signature};
 use std::fs::File;
 use std::io::prelude::*;
 use tempfile::{tempdir, TempDir};
+use tracing::debug;
 
 pub fn create_temp_repo() -> Result<(Repository, TempDir), git2::Error> {
     let dir = tempdir().expect("Failed to create temporary directory");
     let repo_path = dir.path();
 
     let repo = Repository::init(repo_path)?;
-    println!(
+    debug!(
         "Successfully initialized a new Git repository at: {}",
         repo_path.display()
     );
@@ -42,7 +43,7 @@ pub fn create_temp_repo() -> Result<(Repository, TempDir), git2::Error> {
         // releasing its borrow on `repo`.
     } // End of the scope block.
 
-    println!("Successfully made an initial commit.");
+    debug!("Successfully made an initial commit.");
 
     // Now it is safe to move `repo` out of the function.
     Ok((repo, dir))
