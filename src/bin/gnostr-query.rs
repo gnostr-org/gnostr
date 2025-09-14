8b77eca8dfafe03b0cf59364a903d591cecb9376
randymcmillan <randymcmillan@protonmail.com>
2025-09-14 12:47:35 -0400

    src/bin/gnostr-query.rs:quiet

--- src/bin/gnostr-query.rs
+++ src/bin/gnostr-query.rs
@@ -5,19 +5,13 @@ use log::debug;
 use serde_json::{json, to_string};
 use url::Url;
 
-use serde_json::{Map, Value};
-
-fn process_json_map_by_value(json_map: Map<String, Value>) {
-    for (key, value) in json_map {
-        println!("{{\"{}\": {}}}", key, value.to_string());
-    }
-}
-
 /// Usage
 /// nip-0034 kinds
 /// gnostr-query -k 1630,1632,1621,30618,1633,1631,1617,30617
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+	//debug!("debug! main!");
+	//println!("println! main!");
     let matches = cli().await?;
 
     let mut filt = serde_json::Map::new();
@@ -42,7 +36,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         // ["EOSE","gnostr-query"] counts as a message!      + 1
         filt.insert("limit".to_string(), json!(limit.clone() /*+ 1*/));
         limit_check = *limit;
-        //process_json_map_by_value(filt.clone());
     }
 
     if let Some(generic) = matches.get_many::<String>("generic") {
@@ -51,7 +44,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             let tag = format!("#{}", generic_vec[0]);
             let val = generic_vec[1].split(',').collect::<String>();
             filt.insert(tag, json!(val));
-            //process_json_map_by_value(filt.clone());
         }
     }
 
@@ -60,7 +52,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             "#t".to_string(),
             json!(hashtag.split(',').collect::<Vec<&str>>()),
         );
-        //process_json_map_by_value(filt.clone());
     }
 
     if let Some(mentions) = matches.get_one::<String>("mentions") {
@@ -68,7 +59,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             "#p".to_string(),
             json!(mentions.split(',').collect::<Vec<&str>>()),
         );
-        //process_json_map_by_value(filt.clone());
     }
 
     if let Some(references) = matches.get_one::<String>("references") {
@@ -76,7 +66,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             "#e".to_string(),
             json!(references.split(',').collect::<Vec<&str>>()),
         );
-        //process_json_map_by_value(filt.clone());
     }
 
     if let Some(kinds) = matches.get_one::<String>("kinds") {
@@ -90,7 +79,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             eprintln!("Error parsing kinds. Ensure they are integers.");
             std::process::exit(1);
         }
-        //process_json_map_by_value(filt.clone());
     }
     //["REQ", "", { "search": "orange" }, { "kinds": [1, 2], "search": "purple" }]
     if let Some(search) = matches.get_many::<String>("search") {
@@ -99,10 +87,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let search_string = "search".to_string();
         let val = search_vec[0].split(',').collect::<String>();
         filt.insert(search_string, json!(val));
-        //process_json_map_by_value(filt.clone());
         //}
     }
-    process_json_map_by_value(filt.clone());
+
     let config = ConfigBuilder::new()
         .host("localhost")
         .port(8080)
@@ -119,53 +106,46 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .search("", "")
         .build()?;
 
-    log::debug!("config=\n{config:?}");
+    //debug!("debug config=\n{config:?}");
+    //println!("println config=\n{config:?}");
     let q = json!(["REQ", "gnostr-query", filt]);
-    //println!("{}", q.clone());
     let query_string = to_string(&q)?;
-    println!("{{ \"query_string\" : {} }}", query_string.to_string());
+    //debug!("debug:query_string:\n{:?}", query_string);
+    //println!("println :query_string:\n{:?}", query_string);
 
-    let relay_url_str = matches.get_one::<String>("relay").unwrap();
-    let relay_url = Url::parse(relay_url_str)?;
+    let relays = if let Some(relay_str) = matches.get_one::<String>("relay") {
+        //log::debug!("117:log:using relay: {}", relay_str);
+        //println!("118:print:using relay: {}", relay_str);
+        vec![Url::parse(relay_str)?]
+    } else {
+        //log::debug!("log:using bootstrap relays");
+        //println!("print:using bootstrap relays");
+        BOOTSTRAP_RELAYS
+            .iter()
+            .filter_map(|s| Url::parse(s).ok())
+            .collect()
+    };
+
+    let vec_result = gnostr_query::send(query_string.clone(), relays, Some(limit_check)).await?;
+
+    //trace
+    //debug!("vec_result:\n{:?}", vec_result.clone());
+	//for s in vec_result {println!("s={}", s)};
+    //println!("vec_result:\n{:?}", vec_result);
+
+    let mut json_result: Vec<String> = vec![];
+    for element in vec_result {
+        //log::debug!("element=\n{}", element);
+        //println!("element=\n{}", element);
+        json_result.push(element);
+    }
 
     if matches.get_many::<String>("search").is_some() {
-        //println!("search is some {:?}", matches.get_many::<String>("search"));
-        let vec_result = gnostr_query::send(
-            query_string.clone(),
-            //vec![Url::parse(&BOOTSTRAP_RELAYS.to_vec()[0]).expect("")],
-            vec![Url::parse(&relay_url_str).expect("")],
-            Some(limit_check),
-        )
-        .await;
-
-        //trace
-        //println!("144:{{ \"vec_result\" : \" {:?} \"}}", vec_result);
-
-        let mut json_result: Vec<String> = vec![];
-        for element in vec_result.unwrap() {
-            println!("{{\"element\":{}}}", element);
-            json_result.push(element);
-        }
-
         for element in json_result {
-            //println!("{{\"element\": {} }}", element);
+            print!("{}", element);
         }
         std::process::exit(0);
     } else {
-        let vec_result = gnostr_query::send(
-            query_string.clone(),
-            //vec![Url::parse(&BOOTSTRAP_RELAYS.to_vec()[0]).expect("")],
-            vec![Url::parse(&relay_url_str).expect("")],
-            Some(limit_check),
-        )
-        .await;
-
-        let mut json_result: Vec<String> = vec![];
-        for element in vec_result.unwrap() {
-            //println!("{{ \"element\": \"{}\"}}", element);
-            json_result.push(element);
-        }
-
         for element in json_result {
             print!("{}", element);
         }
