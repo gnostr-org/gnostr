24d6b0dbe8cbb53bb00703366735ac41ac2c3eec
randymcmillan <randymcmillan@protonmail.com>
2025-10-21 18:52:53 -0400

    crawler/src/processor.rs:add LOCALHOST_8080

--- crawler/src/processor.rs
+++ crawler/src/processor.rs
@@ -5,15 +5,16 @@ use crate::stats::Stats;
 use nostr_sdk::prelude::{Event, Kind, Tag, Timestamp};
 use std::sync::LazyLock;
 
-pub const BOOTSTRAP_RELAY0: &str = "ws://127.0.0.1:8080";
-pub const BOOTSTRAP_RELAY1: &str = "wss://relay.nostr.band";
-pub const BOOTSTRAP_RELAY2: &str = "wss://nostr.wine";
-pub const BOOTSTRAP_RELAY3: &str = "wss://relay.damus.io";
-pub const BOOTSTRAP_RELAY4: &str = "wss://relay.noswhere.com";
-pub const BOOTSTRAP_RELAY5: &str = "wss://search.nos.today";
-pub const BOOTSTRAP_RELAY6: &str = "wss://feeds.nostr.band";
-pub const BOOTSTRAP_RELAY7: &str = "wss://relay.primal.net";
-pub const BOOTSTRAP_RELAY8: &str = "wss://gitnostr.com";
+pub const BOOTSTRAP_RELAY0: &str = "wss://relay.nostr.band";
+pub const BOOTSTRAP_RELAY1: &str = "wss://nostr.wine";
+pub const BOOTSTRAP_RELAY2: &str = "wss://relay.damus.io";
+pub const BOOTSTRAP_RELAY3: &str = "wss://relay.noswhere.com";
+pub const BOOTSTRAP_RELAY4: &str = "wss://search.nos.today";
+pub const BOOTSTRAP_RELAY5: &str = "wss://feeds.nostr.band";
+pub const BOOTSTRAP_RELAY6: &str = "wss://relay.primal.net";
+pub const BOOTSTRAP_RELAY7: &str = "wss://gitnostr.com";
+
+pub const LOCALHOST_8080: &str = "ws://127.0.0.1:8080";
 
 pub static BOOTSTRAP_RELAYS: LazyLock<Vec<String>> = LazyLock::new(|| {
     // The vec! macro and String::from calls are now inside a closure,
@@ -27,7 +28,6 @@ pub static BOOTSTRAP_RELAYS: LazyLock<Vec<String>> = LazyLock::new(|| {
         String::from(BOOTSTRAP_RELAY5),
         String::from(BOOTSTRAP_RELAY6),
         String::from(BOOTSTRAP_RELAY7),
-        String::from(BOOTSTRAP_RELAY8),
     ]
 });
 
