73d624b39df39225fbc2351b76b1394f4d36d426
randymcmillan <randymcmillan@protonmail.com>
2025-10-22 17:29:04 -0400

    tests/git_ssh.rs:refactor for "git-ssh" command

--- tests/git_ssh.rs
+++ tests/git_ssh.rs
@@ -1,36 +1,13 @@
 #[cfg(test)]
 mod tests {
     use std::env;
-    use std::fs::{self, File};
+    use std::fs::File;
     use std::io::Write;
     use std::net::TcpListener;
-    use std::path::Path;
-    use gag::BufferRedirect;
-    use std::io::Read;
+    use std::process::Command;
+    use gnostr::utils::find_available_port;
 
-	use gnostr::utils::find_available_port;
-
-    // This is the main function from src/bin/git-ssh.rs
-    async fn run_git_ssh_main() {
-        env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
-        if let Err(e) = gnostr::ssh::start().await {
-            println!("{}", e);
-            println!("EXAMPLE:server.toml\n{}", SERVER_TOML_TEMPLATE);
-            println!("check the port in your server.toml is available!");
-            println!("check the port in your server.toml is available!");
-            println!("check the port in your server.toml is available!\n");
-            println!("EXAMPLE:repo.toml\n{}", REPO_TOML_CONTENT);
-        }
-    }
-
-    const REPO_TOML_CONTENT: &str = r#" 
-name = "gnostr-gnit-server"
-public = true
-members = ["gnostr", "gnostr-user"]
-failed_push_message = "Issues and patches can be emailed to admin@gnostr.org"
-"#;
-
-    const SERVER_TOML_TEMPLATE: &str = r#" 
+    const SERVER_TOML_TEMPLATE: &str = r#"
 name = "gnostr.org"
 port = {}
 hostname = "gnostr.org"
@@ -42,9 +19,8 @@ can_create_repos = true
 public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDaBogLsfsOkKIpZEZYa3Ee+wFaaxeJuHps05sH2rZLf+KEE6pWX5MT2iWMgP7ihmm6OqbAPkWoBUGEO5m+m/K1S0MgQXUvaTsTI0II3MDqJT/RXA6Z9c+ZIDROEAkNIDrfeU2n8hQXfMHwG6aJjwv3Zky9jR/ey2rSgKLMcTOLrMeAyop6fYhjIHqp0dTagHo1j+XHAbVsrjw6oxC0ohTkp8rzH6cYJyjK4TOKApEgCALJUOA2rbHNxr68wAIe2RS36dRQobD3ops2+HoOGk7pkBQazBAlZp/H4monWRrq7tTEw8FkGMX5udZQX6BNEI0vJZqtdkSpG7jSS3aL7GXcuOYKpsTKxuGm5BWsrRPiphsc25U02oe/y3+qM0ceP/njJp3ZvXQ/a2QGPU4+P8WSD+J0oKS+TiRKrpiTR4ChJk8zWupg4PI5zflN3yyK7MrGXg1n0DsvHxPXcqpvVRz4i8ORt6IlKGkve1tC0Wd9pVy4044LDethMORRZFjWAdS/caN1EMgTrrGMxi0DLVw6ahedGUgZj2WYWfsrEg8Kzbfk3fn32sO/lMnNyz5hmavMBiNORGlIi2Qe2RjQEtcJHn89B7UtyEfnj87V+jZYcFf4nnNQigT2eQ3NlB1YzZS4Zk/OxQeYypclzYFaiYc7RZv2yxKVOy0KvEpldyUKeQ== randy.lee.mcmillan@gmail.com"
 "#;
 
-    #[tokio::test]
-    //#[should_panic]
-    async fn test_main_error_on_port_conflict() {
+    #[test]
+    fn test_main_error_on_port_conflict() {
         let temp_dir = tempfile::tempdir().unwrap();
         env::set_current_dir(&temp_dir).unwrap();
 
@@ -57,18 +33,21 @@ public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDaBogLsfsOkKIpZEZYa3Ee+wFaax
         writeln!(file, "{}", &SERVER_TOML_TEMPLATE.replace("{}", &port.to_string())).unwrap();
         drop(file);
 
-        let mut buf = BufferRedirect::stdout().unwrap();
+        let binary_path = env::current_exe()
+            .unwrap()
+            .parent()
+            .unwrap()
+            .parent()
+            .unwrap()
+            .join("git-ssh");
 
-        tokio::time::timeout(std::time::Duration::from_secs(5), run_git_ssh_main())
-            .await
-            .expect("Test timed out");
+        let output = Command::new(binary_path)
+            .output()
+            .expect("failed to execute process");
 
-        let mut output = String::new();
-        buf.read_to_string(&mut output).unwrap();
+        let stdout = String::from_utf8_lossy(&output.stdout);
 
-        assert!(output.contains("check the port in your server.toml is available!"));
-        //assert!(output.contains(&SERVER_TOML_TEMPLATE.replace("{}", &port.to_string())));
-        assert!(output.contains(REPO_TOML_CONTENT));
+        assert!(stdout.contains("check the port in your server.toml is available!"));
 
         drop(listener);
     }
