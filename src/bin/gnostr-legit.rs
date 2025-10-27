95e3e6634709f88015b6b95dd5796e8c49e33840
randymcmillan <randymcmillan@protonmail.com>
2025-10-27 14:59:27 -0400

    src/bin/gnostr-legit.rs:intermediate

--- src/bin/gnostr-legit.rs
+++ src/bin/gnostr-legit.rs
@@ -1,257 +1,312 @@
 #![allow(unused)]
 #![allow(dead_code)]
 extern crate chrono;
-extern crate time;
+use std::process::Command;
 use chrono::offset::Utc;
 use chrono::DateTime;
-use gnostr_asyncgit::sync::commit::{padded_commit_id, serialize_commit};
-
-use gnostr::global_rt::global_rt;
-use log::debug;
-use log::info;
-//
-use nostr_sdk_0_37_0::prelude::*;
-//
-
-use std::process::Command;
 //use std::time::SystemTime;
-use std::any::type_name;
-use std::convert::TryInto;
+use std::io::{Result};
 use std::env;
-use std::error::Error;
-use std::io::Result;
+use std::time::{Duration, SystemTime};
 use std::thread::sleep;
-use std::time::{Duration, SystemTime, UNIX_EPOCH};
-use std::time::Instant;
-use time::OffsetDateTime;
+use std::convert::TryInto;
+use std::any::type_name;
 //use std::mem::size_of;
-use argparse::{ArgumentParser, Store};
-use git2::*;
-use gnostr::get_pwd;
-use gnostr::legit::gitminer;
-use gnostr::legit::gitminer::Gitminer;
-use gnostr::legit::post_event;
-use gnostr::legit::repo;
-use gnostr::legit::worker;
-use gnostr_types::Event;
-use gnostr_types::EventV3;
-use pad::{Alignment, PadStr};
-use sha2::{Digest, Sha256};
 use std::{io, thread};
+use argparse::{ArgumentParser,Store};
+use gitminer::Gitminer;
+use git2::*;
+use sha2::{Sha256, Digest};
+use pad::{PadStr, Alignment};
 
-use std::path::PathBuf; //for get_current_dir
+use gnostr::legit::{worker, gitminer, repo};
 
-//pub mod gitminer;
-//pub mod repo;
-//pub mod worker;
+fn type_of<T>(_: T) -> &'static str {
+    type_name::<T>()
+}
 
-//fn type_of<T>(_: T) -> &'static str {
-//    type_name::<T>()
+//fn convert_to_u32(v: usize) -> Option<i8> {
+//    if v > (std::i8::MAX as i32).try_into().unwrap() {
+//        None
+//    } else {
+//        Some(v as i8)
+//    }
 //}
 
-fn get_epoch_ms() -> u128 {
-    SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-        .as_millis()
-}
+//fn get_current_working_dir() -> std::io::Result<PathBuf> {
+//    env::current_dir()
+//}
 
-fn convert_to_u32(v: usize) -> Option<i8> {
-    if v > std::i8::MAX as usize {
-        None
-    } else {
-        Some(v as i8)
-    }
-}
+fn main() -> io::Result<()> {
 
-fn get_current_working_dir() -> std::io::Result<PathBuf> {
-    env::current_dir()
-}
-
-#[cfg(debug_assertions)]
-fn example() {
-    debug!("Debugging enabled");
-    debug!("cwd={:?}", get_current_working_dir());
-}
-
-#[cfg(not(debug_assertions))]
-fn example() {
-    debug!("Debugging disabled");
-    debug!("cwd={:?}", get_current_working_dir());
-}
-
-#[tokio::main]
-async fn main() -> Result<()> {
-    #[allow(clippy::if_same_then_else)]
-    if cfg!(debug_assertions) {
-        debug!("Debugging enabled");
-    } else {
-        debug!("Debugging disabled");
-    }
-
-    #[cfg(debug_assertions)]
-    debug!("Debugging enabled");
-    #[cfg(not(debug_assertions))]
-    debug!("Debugging disabled");
-    example();
-
-	let start = Instant::now();
-    let epoch = get_epoch_ms();
-    println!("epoch:{}", epoch.clone());
+    let start = SystemTime::now();
     let system_time = SystemTime::now();
-    println!("system_time:{:?}", system_time.clone());
 
     let datetime: DateTime<Utc> = system_time.into();
-    println!("{}", datetime.format("%d/%m/%Y %T/%s"));
-    println!("{}", datetime.format("%d/%m/%Y %T/%f"));
-    println!("{}", datetime.format("%d/%m/%Y %T"));
+    //println!("{}", datetime.format("%d/%m/%Y %T/%s"));
+    //println!("{}", datetime.format("%d/%m/%Y %T"));
 
-    //let cwd = get_current_working_dir();
-    let cwd = get_pwd();
-    #[cfg(debug_assertions)]
-    println!("Debugging enabled");
-    println!("{:#?}", cwd);
     let state = repo::state();
-    println!("{:#?}", state);
+    //println!("{:#?}", state);
     //
     let repo_root = std::env::args().nth(1).unwrap_or(".".to_string());
-    println!("repo_root={:?}", repo_root.as_str());
-    let repo = Repository::discover(repo_root.as_str()).expect("Couldn't open repository");
-    println!("{} state={:?}", repo.path().display(), repo.state());
-    println!("state={:?}", repo.state());
+    //println!("repo_root={:?}", repo_root.as_str());
+    let repo = Repository::open(repo_root.as_str()).expect("Couldn't open repository");
+    //println!("{} state={:?}", repo.path().display(), repo.state());
+    //println!("state={:?}", repo.state());
+    if repo.state() != RepositoryState::Clean {
+
+    //println!("clean {:?}", repo.state());
+
+    let repo_state =
+        if cfg!(target_os = "windows") {
+        Command::new("cmd")
+                .args(["/C", "git status"])
+                .output()
+                .expect("failed to execute process")
+        } else
+        if cfg!(target_os = "macos"){
+        Command::new("sh")
+                .arg("-c")
+                .arg("gnostr-git diff")
+                .output()
+                .expect("failed to execute process")
+        } else
+        if cfg!(target_os = "linux"){
+        Command::new("sh")
+                .arg("-c")
+                .arg("gnostr-git diff")
+                .output()
+                .expect("failed to execute process")
+        } else {
+        Command::new("sh")
+                .arg("-c")
+                .arg("gnostr-git diff")
+                .output()
+                .expect("failed to execute process")
+        };
+
+    let state = String::from_utf8(repo_state.stdout)
+    .map_err(|non_utf8| String::from_utf8_lossy(non_utf8.as_bytes()).into_owned())
+    .unwrap();
+    //println!("state={:?}", state);
+    }
 
     let count = thread::available_parallelism()?.get();
     assert!(count >= 1_usize);
+    //println!("{}={}", type_of(count), (count as i32));
+    //println!("{}={}", type_of(count), (count as i64));
+    //let mut hasher = Sha256::new();
+    //let data = b"Hello world!";
+    //hasher.update(data);
+    //// `update` can be called repeatedly and is generic over `AsRef<[u8]>`
+    //hasher.update("String data");
+    //// Note that calling `finalize()` consumes hasher
+    //let hash = hasher.finalize();
+    ////println!("Binary hash: {:?}", hash);
+    //println!("hash: {:?}", hash);
+    //println!("sha256 before write: {:x}", hash);
+    //println!("sha256 before write: {:X}", hash);
 
-    let now = SystemTime::now();
 
-    let pwd = env::current_dir()?;
-    println!("pwd={}", pwd.clone().display());
-    let mut hasher = Sha256::new();
-    hasher.update(format!("{}", pwd.clone().display()));
-    //sha256sum <(echo gnostr-legit)
-    let pwd_hash: String = format!("{:x}", hasher.finalize());
-    println!("pwd_hash={:?}", pwd_hash);
+   let now = SystemTime::now();
 
-    let mut opts = gitminer::Options {
+   //// we sleep for 2 seconds
+   //sleep(Duration::new(2, 0));
+   // match now.elapsed() {
+   //    Ok(elapsed) => {
+   //        // it prints '2'
+   //        println!("{}", elapsed.as_secs());
+   //    }
+   //    Err(e) => {
+   //        // an error occurred!
+   //        println!("Error: {e:?}");
+   //    }
+   //}
+
+
+    let output =
+        if cfg!(target_os = "windows") {
+        Command::new("cmd")
+                .args(["/C", "git status"])
+                .output()
+                .expect("failed to execute process")
+        } else
+        if cfg!(target_os = "macos"){
+        Command::new("sh")
+                .arg("-c")
+                .arg("git diff")
+                .output()
+                .expect("failed to execute process")
+        } else
+        if cfg!(target_os = "linux"){
+        Command::new("sh")
+                .arg("-c")
+                .arg("git diff")
+                .output()
+                .expect("failed to execute process")
+        } else {
+        Command::new("sh")
+                .arg("-c")
+                .arg("git diff")
+                .output()
+                .expect("failed to execute process")
+        };
+
+    let message = String::from_utf8(output.stdout)
+    .map_err(|non_utf8| String::from_utf8_lossy(non_utf8.as_bytes()).into_owned())
+    .unwrap();
+
+    let path = env::current_dir()?;
+        //println!("The current directory is {}", path.display());
+        //Ok(());
+    let mut opts = gitminer::Options{
         threads: count.try_into().unwrap(),
-        target: "00000".to_string(), //default 00000
+        target:  "00000".to_string(),//default 00000
         //gnostr:##:nonce
         //part of the gnostr protocol
         //src/worker.rs adds the nonce
-        pwd_hash: pwd_hash.clone(),
-        message: cwd.unwrap(),
-        //message: message,
+        //message: "gnostr".to_string(),
+        message: message,
         //message: count.to_string(),
         //repo:    ".".to_string(),
-        repo: repo.path().display().to_string(),
-		timestamp: OffsetDateTime::now_utc(),
+        repo:    path.as_path().display().to_string(),
+        timestamp: SystemTime::now().into(),
+            //.duration_since(SystemTime::UNIX_EPOCH)
     };
 
     parse_args_or_exit(&mut opts);
 
     let mut miner = match Gitminer::new(opts) {
-        Ok(m) => m,
-        Err(e) => {
-            panic!("Failed to start git miner: {}", e);
-        }
+        Ok(m)  => m,
+        Err(e) => { panic!("Failed to start git miner: {}", e); }
     };
 
     let hash = match miner.mine() {
-        Ok(s) => s,
-        Err(e) => {
-            panic!("Failed to generate commit: {}", e);
-        }
+        Ok(s)  => s,
+        Err(e) => { panic!("Failed to generate commit: {}", e); }
     };
 
-    //initialize git repo
-    let repo = Repository::discover(".").expect("");
+    let mut hasher = Sha256::new();
+    hasher.update(&hash);
+    // `update` can be called repeatedly and is generic over `AsRef<[u8]>`
+    //hasher.update("String data");
+    // Note that calling `finalize()` consumes hasher
+    //let gnostr_sec = hasher.finalize();
+    let gnostr_sec: String = format!("{:X}", hasher.finalize());
+    //println!("Binary hash: {:?}", hash);
+    //println!("hash before: {:?}", hash);
+    //println!("hash after pad: {:?}", hash);
+    //println!("&hash before: {:?}", &hash);
+    //println!("&hash after pad: {:?}", &hash);
+    //println!("gnostr_sec before pad: {:?}", gnostr_sec);
+    //println!("gnostr_sec after pad: {:?}", gnostr_sec.pad(64, '0', Alignment::Right, true));
+    //println!("&gnostr_sec before pad: {:?}", &gnostr_sec);
+    //println!("&gnostr_sec after pad: {:?}", &gnostr_sec.pad(64, '0', Alignment::Right, true));
 
-    //gather some repo info
-    //find HEAD
-    let head = repo.head().expect("");
-    let obj = head
-        .resolve()
-        .expect("")
-        .peel(ObjectType::Commit)
-        .expect("");
 
-    //read top commit
-    let commit = obj.peel_to_commit().expect("");
-    let commit_id = commit.id().to_string();
 
-    let serialized_commit = serialize_commit(&commit).expect("gnostr-async:error!");
-    println!("Serialized commit:\n{}", serialized_commit.clone());
+    //let s = "12345".pad(64, '0', Alignment::Right, true);
+    //println!("s: {:?}", s);
+// echo "000000b64a065760e5441bf47f0571cb690b28fc" | openssl dgst -sha256 | sed 's/SHA2-256(stdin)= //g'
+//
+//
+//shell test
+    let touch =
+        Command::new("sh")
+                .args(["-c", "touch ", &hash])
+                .output()
+                .expect("failed to execute process");
+    let touch_event = String::from_utf8(touch.stdout)
+    .map_err(|non_utf8| String::from_utf8_lossy(non_utf8.as_bytes()).into_owned())
+    .unwrap();
+    let cat =
+        Command::new("sh")
+                .args(["-c", "touch ", &hash])
+                .output()
+                .expect("failed to execute process");
+    let cat_event = String::from_utf8(cat.stdout)
+    .map_err(|non_utf8| String::from_utf8_lossy(non_utf8.as_bytes()).into_owned())
+    .unwrap();
+//shell test
+    //git rev-parse --verify HEAD
+    let shell_test =
+        if cfg!(target_os = "windows") {
+        Command::new("cmd")
+                .args(["/C", "gnostr --hash 0"])
+                .output()
+                .expect("failed to execute process")
+        } else
+        if cfg!(target_os = "macos"){
+        Command::new("sh")
+                .args(["-c", "gnostr --hash 0"])
+                .output()
+                .expect("failed to execute process")
+        } else
+        if cfg!(target_os = "linux"){
+        Command::new("sh")
+                .args(["-c", "gnostr --hash 0"])
+                .output()
+                .expect("failed to execute process")
+        } else {
+        Command::new("sh")
+                .args(["-c", "gnostr --hash 0"])
+                .output()
+                .expect("failed to execute process")
+        };
 
-    //some info wrangling
-    println!("commit_id:\n{}", commit_id);
-    let padded_commitid = padded_commit_id(format!("{:0>64}", commit_id.clone()));
-    println!("padded_commitid:\n{}", padded_commitid.clone());
-    global_rt().spawn(async move {
-        //// commit based keys
-        //let keys = generate_nostr_keys_from_commit_hash(&commit_id)?;
-        //info!("keys.secret_key():\n{:?}", keys.secret_key());
-        //info!("keys.public_key():\n{}", keys.public_key());
+    let gnostr_test = String::from_utf8(shell_test.stdout)
+    .map_err(|non_utf8| String::from_utf8_lossy(non_utf8.as_bytes()).into_owned())
+    .unwrap();
 
-        //parse keys from sha256 hash
-        let padded_keys = Keys::parse(padded_commitid).unwrap();
-        //create nostr client with commit based keys
-        //let client = Client::new(keys);
-        let client = Client::new(padded_keys.clone());
-        client.add_relay("wss://relay.damus.io").await.expect("");
-        client.add_relay("wss://e.nos.lol").await.expect("");
-        client.connect().await;
+    //assert...
+    //echo gnostr|openssl dgst -sha256 | sed 's/SHA2-256(stdin)= //g'
 
-        //build git gnostr event
-        let builder = EventBuilder::text_note(serialized_commit.clone());
-
-        //send git gnostr event
-        let output = client.send_event_builder(builder).await.expect("");
-
-        //some reporting
-        info!("Event ID: {}", output.id());
-        info!("Event ID BECH32: {}", output.id().to_bech32().expect(""));
-        info!("Sent to: {:?}", output.success);
-        info!("Not sent to: {:?}", output.failed);
-    });
-
+//gnostr-legit must only return a sha256 generated by the
+//recent commit hash
+//to enable nested commands
+//REF:
+//gnostr --hash $(gnostr legit . -p 00000 -m "git rev-parse --verify HEAD")
+//gnostr --sec $(gnostr --hash $(gnostr legit . -p 00000 -m "git rev-parse --verify HEAD"))
+//Example:
+//gnostr --sec $(gnostr --hash $(gnostr legit . -p 00000 -m "#gnostr will exist!")) --envelope --content "$(gnostr-git log -n 1)" | gnostr-cat -u wss://relay.damus.io
+    //
+    //
+    //
+    let duration = SystemTime::now().duration_since(start).expect("Time went backwards");
+    //println!("Success! Generated commit {} in {} seconds", hash, duration.num_seconds());
+    println!("{}", gnostr_test);
     Ok(())
+
 }
 
 fn parse_args_or_exit(opts: &mut gitminer::Options) {
     let mut ap = ArgumentParser::new();
     ap.set_description("Generate git commit sha with a custom prefix");
-    ap.stop_on_first_argument(false);
+
+    //ap.stop_on_first_argument(false);
 
     //ap.refer(&mut opts.repo)
     //    //.add_argument("repository-path", Store, "Path to your git repository (required)");
     //    .add_argument("repository-path", Store, "Path to your git repository");
     //    //.required();
-    ap.refer(&mut opts.repo)
-        .add_argument("repository-path", Store, "Path to your git repository");
+    //ap.refer(&mut opts.repo).add_argument("repository-path", Store, "Path to your git repository");
 
-    ap.refer(&mut opts.target).add_option(
-        &["-p", "--prefix"],
-        Store,
-        "Desired commit prefix (required)",
-    );
-    //.required();
+    ap.refer(&mut opts.target)
+        .add_option(&["-p", "--prefix"], Store, "Desired commit prefix (required)");
+        //.required();
 
-    ap.refer(&mut opts.threads).add_option(
-        &["-t", "--threads"],
-        Store,
-        "Number of worker threads to use (default 8)",
-    );
+    ap.refer(&mut opts.threads)
+        .add_option(&["-t", "--threads"], Store, "Number of worker threads to use (default 8)");
 
-    ap.refer(&mut opts.message).add_option(
-        &["-m", "--message"],
-        Store,
-        "Commit message to use (required)",
-    );
-    //.required();
+    ap.refer(&mut opts.message)
+        .add_option(&["-m", "--message"], Store, "Commit message to use (required)");
+        //.required();
 
     //ap.refer(&mut opts.timestamp)
     //    .add_option(&["--timestamp"], Store, "Commit timestamp to use (default now)");
+    
+    ap.refer(&mut opts.repo).add_argument("repository-path", Store, "Path to your git repository");
 
     ap.parse_args_or_exit();
 }
