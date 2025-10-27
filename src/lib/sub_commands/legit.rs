896a22b2a52b4931e4a3d4d9c2f79265bf48e42a
randymcmillan <randymcmillan@protonmail.com>
2025-10-27 16:37:16 -0400

    src/lib/sub_commands/legit.rs:intermediate

--- src/lib/sub_commands/legit.rs
+++ src/lib/sub_commands/legit.rs
@@ -13,6 +13,8 @@ use nostr_sdk_0_34_0::prelude::*;
 
 use serde::ser::StdError;
 use gnostr_legit::command;
+use gnostr_legit::gitminer;
+use time::{get_time, now};
 
 #[derive(Args, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -20,6 +22,8 @@ use gnostr_legit::command;
 pub struct LegitSubCommand {
     #[command(subcommand)]
     command: Option<LegitCommands>,
+    /// Path to your git repository
+    repository_path: Option<String>,
     ///// nsec or hex private key
     #[arg(short, long, global = true)]
     nsec: Option<String>,
@@ -32,6 +36,15 @@ pub struct LegitSubCommand {
     ///// password to decrypt nsec
     #[arg(long, global = true)]
     pow: Option<String>,
+    /// Desired commit prefix
+    #[arg(short, long)]
+    prefix: String,
+    /// Number of worker threads to use
+    #[arg(short, long)]
+    threads: Option<usize>,
+    /// Commit message to use
+    #[arg(short, long)]
+    message: String,
     ///// disable spinner animations
     #[arg(long, action)]
     disable_cli_spinners: bool,
@@ -46,7 +59,21 @@ pub async fn legit(sub_command_args: &LegitSubCommand) -> Result<(), Box<dyn Std
         Some(LegitCommands::Pull) => pull::launch().await?,
         Some(LegitCommands::Push(args)) => push::launch(&args).await?,
         Some(LegitCommands::Fetch(args)) => fetch::launch(&args).await?,
-        Some(LegitCommands::Mine) | None => command::run_legit_command().map_err(|e| Box::new(e) as Box<dyn StdError>)?,
+        Some(LegitCommands::Mine) | None => {
+            let repo_path = sub_command_args.repository_path.clone().unwrap_or(".".to_string());
+            let prefix = sub_command_args.prefix.clone();
+            let threads = sub_command_args.threads.unwrap_or(8) as u32;
+            let message = sub_command_args.message.clone();
+
+            let opts = gitminer::Options {
+                threads,
+                target: prefix,
+                message,
+                repo: repo_path,
+                timestamp: now(),
+            };
+            command::run_legit(opts).map_err(|e| Box::new(e) as Box<dyn StdError>)?;
+        }
     }
     Ok(())
 }
