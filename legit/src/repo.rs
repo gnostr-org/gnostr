f54e1baa821d2ab0a7ef6f0530d2820779ad3e1f
randymcmillan <randymcmillan@protonmail.com>
2025-10-30 20:23:39 +0000

    gnostr-legit-test 05-0000030c

--- legit/src/repo.rs
+++ legit/src/repo.rs
@@ -2,10 +2,6 @@ extern crate git2;
 use git2::Repository;
 use git2::RepositoryState;
 
-pub trait ToString {
-    fn to_string(&self) -> String;
-}
-
 pub fn state() -> RepositoryState {
     let repo_root = std::env::args().nth(1).unwrap_or(".".to_string());
     let repo = Repository::discover(repo_root.as_str()).expect("Couldn't open repository");
