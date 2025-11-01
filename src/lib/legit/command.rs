0007894815205bed9f618845d888da976bb74f04
randymcmillan <randymcmillan@protonmail.com>
2025-11-01 11:52:11 -0400

    1911
    921770/509859
    00-00000245

--- src/lib/legit/command.rs
+++ src/lib/legit/command.rs
@@ -83,41 +83,41 @@ pub fn run_legit_command(mut opts: gitminer::Options) -> io::Result<()> {
         Err(e) => { panic!("Failed to generate commit: {}", e); }
     };
 
-    let mut hasher = Sha256::new();
-    hasher.update(&hash);
-    let gnostr_sec: String = format!("{:X}", hasher.finalize());
+    //let mut hasher = Sha256::new();
+    //hasher.update(&hash);
+    //let gnostr_sec: String = format!("{:X}", hasher.finalize());
 
-    let shell_test =
-        if cfg!(target_os = "windows") {
-        Command::new("cmd")
-                .args(["/C", "gnostr --hash 0"])
-                .output()
-                .expect("failed to execute process")
-        } else
-        if cfg!(target_os = "macos"){
-        Command::new("sh")
-                .args(["-c", "gnostr --hash 0"])
-                .output()
-                .expect("failed to execute process")
-        } else
-        if cfg!(target_os = "linux"){
-        Command::new("sh")
-                .args(["-c", "gnostr --hash 0"])
-                .output()
-                .expect("failed to execute process")
-        } else {
-        Command::new("sh")
-                .args(["-c", "gnostr --hash 0"])
-                .output()
-                .expect("failed to execute process")
-        };
+    //let shell_test =
+    //    if cfg!(target_os = "windows") {
+    //    Command::new("cmd")
+    //            .args(["/C", "gnostr --hash 0"])
+    //            .output()
+    //            .expect("failed to execute process")
+    //    } else
+    //    if cfg!(target_os = "macos"){
+    //    Command::new("sh")
+    //            .args(["-c", "gnostr --hash 0"])
+    //            .output()
+    //            .expect("failed to execute process")
+    //    } else
+    //    if cfg!(target_os = "linux"){
+    //    Command::new("sh")
+    //            .args(["-c", "gnostr --hash 0"])
+    //            .output()
+    //            .expect("failed to execute process")
+    //    } else {
+    //    Command::new("sh")
+    //            .args(["-c", "gnostr --hash 0"])
+    //            .output()
+    //            .expect("failed to execute process")
+    //    };
 
-    let gnostr_test = String::from_utf8(shell_test.stdout)
-    .map_err(|non_utf8| String::from_utf8_lossy(non_utf8.as_bytes()).into_owned())
-    .unwrap();
+    //let gnostr_test = String::from_utf8(shell_test.stdout)
+    //.map_err(|non_utf8| String::from_utf8_lossy(non_utf8.as_bytes()).into_owned())
+    //.unwrap();
 
-    let duration = SystemTime::now().duration_since(start).expect("Time went backwards");
-    println!("{}", gnostr_test);
+    //let duration = SystemTime::now().duration_since(start).expect("Time went backwards");
+    //println!("{}", gnostr_test);
     Ok(())
 
 }
