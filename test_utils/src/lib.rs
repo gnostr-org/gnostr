9c055d230355cb1b68c05bab0917136228ee5656
randymcmillan <randymcmillan@protonmail.com>
2025-11-01 23:42:02 -0400

    use expectrl:tests

--- test_utils/src/lib.rs
+++ test_utils/src/lib.rs
@@ -1074,19 +1074,20 @@ impl CliTester {
 	}
 
 	fn exp_string(&mut self, message: &str) -> Result<String> {
-		        match self
-		            .expectrl_session
-		            .expect(message)
-		            .context("expected immediate end but got timed out")
-		        {
-		            Ok(before) => Ok(std::str::from_utf8(before.get(0).unwrap_or(b"")).unwrap_or("").to_string()),
-		            Err(e) => {
-		                for p in [51, 52, 53, 55, 56, 57] {
-		                    let _ = relay::shutdown_relay(8000 + p);
-		                }
-		                Err(anyhow::Error::new(e.into_inner().unwrap_or_else(|| Box::new(e))))
-		            }
-		        }	}
+		match self
+			.expectrl_session
+			.expect(message)
+			.context("expected immediate end but got timed out")
+		{
+			Ok(before) => Ok(std::str::from_utf8(before.get(0).unwrap_or(b"")).unwrap_or("").to_string()),
+			Err(e) => {
+				for p in [51, 52, 53, 55, 56, 57] {
+					let _ = relay::shutdown_relay(8000 + p);
+				}
+				Err(anyhow::Error::new(e))
+			}
+		}
+	}
 
 	/// returns what came before expected message
 	pub fn expect_eventually<S>(
@@ -1163,11 +1164,12 @@ impl CliTester {
 			.expect(Eof)
 			.context("expected end but got timed out")
 		{
-			            Ok(before) => Ok(std::str::from_utf8(before.get(0).unwrap_or(b"")).unwrap_or("").to_string()),			Err(e) => {
+			Ok(before) => Ok(std::str::from_utf8(before.get(0).unwrap_or(b"")).unwrap_or("").to_string()),
+			Err(e) => {
 				for p in [51, 52, 53, 55, 56, 57] {
 					let _ = relay::shutdown_relay(8000 + p);
 				}
-				Err(anyhow::Error::new(e.into_inner().unwrap_or_else(|| Box::new(e))))
+				Err(anyhow::Error::new(e))
 			}
 		}
 	}
