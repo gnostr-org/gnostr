d75cfd5f6825816537424fbc61fa3d039dc9fb8d
randymcmillan <randymcmillan@protonmail.com>
2025-11-01 23:51:44 -0400

    use expectrl:tests

--- examples/exit_code.rs
+++ examples/exit_code.rs
@@ -7,18 +7,20 @@ use std::io::Read;
 /// cat exited with code 1
 /// Output (stdout and stderr): cat: /this/does/not/exist: No such file or directory
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut p = Session::spawn(Command::new("cat").arg("/etc/passwd"))?;
-    match p.child.wait() {
+    let mut cmd = Command::new("cat");
+    cmd.arg("/etc/passwd");
+    let mut p = Session::spawn(cmd)?;
+    match p.wait() {
         _ => println!("cat exited with code >0, or it was killed"),
     }
 
     let mut p = Session::spawn(Command::new("cat").arg("/this/does/not/exist"))?;
-    match p.child.wait() {
+    match p.wait() {
         Ok(Some(0)) => println!("cat succeeded"),
         Ok(Some(c)) => {
             println!("Cat failed with exit code {}", c);
             let mut output = String::new();
-            p.child.read_to_end(&mut output)?;
+            p.read_to_end(&mut output)?;
             println!("Output (stdout and stderr): {}", output);
         }
         // for other possible return types of wait()
