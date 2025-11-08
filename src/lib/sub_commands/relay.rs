09730bf8875daf3fb2f2a1191d71d2720501fa9f
randymcmillan <randymcmillan@protonmail.com>
2025-11-08 12:16:11 -0500

    src/lib/sub_commands/relay.rs
    src/main.rs
    
    diff --git a/src/lib/sub_commands/relay.rs b/src/lib/sub_commands/relay.rs
    index 5e256e8bb6..c543719998 100644
    --- a/src/lib/sub_commands/relay.rs
    +++ b/src/lib/sub_commands/relay.rs
    @@ -1,7 +1,7 @@
     use anyhow::Result;
     use gnostr_relay::App;
     use std::path::PathBuf;
    -use tokio::task::LocalSet;
    +//use tokio::task::LocalSet;
     use tracing::info;
    
     #[derive(clap::Parser, Debug, Clone)]
    @@ -19,15 +19,40 @@ pub struct RelaySubCommand {
         pub watch: bool,
     }
    
    +#[actix_web::main]
     pub async fn relay(args: RelaySubCommand) -> Result<()> {
         info!("Start relay server with args: {:?}", args);
    -    let app_data = App::create(args.config, args.watch, Some("NOSTR".to_owned()), args.data)?;
    -    let local_set = LocalSet::new();
    -    let server_future = app_data.web_server()?;
    -    local_set
    -        .run_until(async move { server_future.await })
    -        .await?;
    +
    +    //tracing_subscriber::fmt::init();
    +    info!("Start relay server");
    +
    +    let local_set = tokio::task::LocalSet::new();
    +
    +    local_set.run_until(async move {
    +        let app_data = gnostr_relay::App::create(
    +            Some(args.config).ok_or("").expect("REASON"),
    +            true,
    +            Some("NOSTR".to_owned()),
    +            None,
    +        ).map_err(anyhow::Error::from)?;
    +        app_data.web_server()?.await.map_err(anyhow::Error::from)
    +    }).await?;
    +
    +    info!("Relay server shutdown");
    +
    +
    +    //let local_set = tokio::task::LocalSet::new();
    +
    +    //local_set.run_until(async move {
    +    //    let app_data = App::create(
    +    //        args.config,
    +    //        args.watch,
    +    //        Some("NOSTR".to_owned()),
    +    //        args.data,
    +    //    ).map_err(anyhow::Error::from)?;
    +    //    app_data.web_server()?.await.map_err(anyhow::Error::from)
    +    //}).await?;
    
         info!("Relay server shutdown");
         Ok(())
    -}
    \ No newline at end of file
    +}
    diff --git a/src/main.rs b/src/main.rs
    index 7a57d58570..7f1b97a005 100644
    --- a/src/main.rs
    +++ b/src/main.rs
    @@ -332,7 +332,7 @@ async fn main() -> anyhow::Result<()> {
             Some(GnostrCommands::Relay(sub_command_args)) => {
                 debug!("sub_command_args:{:?}", sub_command_args);
                 sub_commands::relay::relay(sub_command_args.clone())
    -                .await
    +                //.await
                     .map_err(|e| anyhow!("Error in relay subcommand: {}", e))
             }
             Some(GnostrCommands::Sniper(sub_command_args)) => {

--- src/lib/sub_commands/relay.rs
+++ src/lib/sub_commands/relay.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use gnostr_relay::App;
 use std::path::PathBuf;
-use tokio::task::LocalSet;
+//use tokio::task::LocalSet;
 use tracing::info;
 
 #[derive(clap::Parser, Debug, Clone)]
@@ -19,14 +19,39 @@ pub struct RelaySubCommand {
     pub watch: bool,
 }
 
+#[actix_web::main]
 pub async fn relay(args: RelaySubCommand) -> Result<()> {
     info!("Start relay server with args: {:?}", args);
-    let app_data = App::create(args.config, args.watch, Some("NOSTR".to_owned()), args.data)?;
-    let local_set = LocalSet::new();
-    let server_future = app_data.web_server()?;
-    local_set
-        .run_until(async move { server_future.await })
-        .await?;
+
+    //tracing_subscriber::fmt::init();
+    info!("Start relay server");
+
+    let local_set = tokio::task::LocalSet::new();
+
+    local_set.run_until(async move {
+        let app_data = gnostr_relay::App::create(
+            Some(args.config).ok_or("").expect("REASON"),
+            true,
+            Some("NOSTR".to_owned()),
+            None,
+        ).map_err(anyhow::Error::from)?;
+        app_data.web_server()?.await.map_err(anyhow::Error::from)
+    }).await?;
+
+    info!("Relay server shutdown");
+
+
+    //let local_set = tokio::task::LocalSet::new();
+
+    //local_set.run_until(async move {
+    //    let app_data = App::create(
+    //        args.config,
+    //        args.watch,
+    //        Some("NOSTR".to_owned()),
+    //        args.data,
+    //    ).map_err(anyhow::Error::from)?;
+    //    app_data.web_server()?.await.map_err(anyhow::Error::from)
+    //}).await?;
 
     info!("Relay server shutdown");
     Ok(())
