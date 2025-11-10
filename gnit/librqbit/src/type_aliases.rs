44fbee6c635db4d437e68f76069e557536ee44e7
git <git@DeepSpaceM1.local>
2025-11-10 07:36:36 -0500

    intermediate

--- gnit/librqbit/src/type_aliases.rs
+++
@@ -1,8 +0,0 @@
-use std::net::SocketAddr;
-
-use futures::stream::BoxStream;
-
-pub type BF = bitvec::vec::BitVec<u8, bitvec::order::Msb0>;
-
-pub type PeerHandle = SocketAddr;
-pub type PeerStream = BoxStream<'static, SocketAddr>;
--- gnit/librqbit/webui/src/components/forms/Form.tsx
+++
@@ -1,5 +0,0 @@
-import { ReactNode } from "react";
-
-export const Form = ({ children }: { children: ReactNode }) => {
-  return <form>{children}</form>;
-};
