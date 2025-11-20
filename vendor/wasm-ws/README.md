75877946919f6546543d5770b11c174d621add43
randymcmillan <randymcmillan@protonmail.com>
2025-11-20 14:58:19 -0500

    wasm-ws

---
+++ vendor/wasm-ws/README.md
@@ -0,0 +1,13 @@
+# WebSocket for WASM
+
+Fork of https://github.com/najamelan/ws_stream_wasm
+
+## License
+
+This project is distributed under the MIT software license - see the [LICENSE](LICENSE) file for details
+
+## Donations
+
+⚡ Tips: <https://getalby.com/p/yuki>
+
+⚡ Lightning Address: yuki@getalby.com
---
+++ vendor/wasm-ws/src/lib.rs
@@ -0,0 +1,37 @@
+// Copyright (c) 2019-2022 Naja Melan
+// Copyright (c) 2023-2024 Yuki Kishimoto
+// Distributed under the MIT software license
+
+#![forbid(unsafe_code)]
+
+use pharos::SharedPharos;
+use wasm_bindgen_futures::spawn_local;
+
+mod error;
+mod event;
+mod message;
+mod meta;
+mod state;
+mod stream;
+mod stream_io;
+
+pub use self::error::WsErr;
+pub use self::event::{CloseEvent, WsEvent};
+pub use self::message::WsMessage;
+pub use self::meta::WsMeta as WebSocket;
+pub use self::state::WsState;
+pub use self::stream::WsStream;
+pub use self::stream_io::WsStreamIo;
+
+/// Helper function to reduce code bloat
+pub(crate) fn notify(pharos: SharedPharos<WsEvent>, evt: WsEvent) {
+    let notify = async move {
+        pharos
+            .notify(evt)
+            .await
+            .map_err(|e| unreachable!("{:?}", e))
+            .unwrap(); // only happens if we closed it.
+    };
+
+    spawn_local(notify);
+}
