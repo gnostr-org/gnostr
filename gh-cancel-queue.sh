9f82e37b2fc51b4fccc09ad235417234c61e4f41
randymcmillan <randymcmillan@protonmail.com>
2025-11-21 14:54:21 -0500

    fix(gh-actions): Correctly handle DEPTH variable in gh-cancel-queue.sh

--- gh-cancel-queue.sh
+++ gh-cancel-queue.sh
@@ -1,7 +1,9 @@
-#for id in $(gh run list --limit 50 --jq ".[] | select (.status == \"queued\" ) | .databaseId" --json databaseId,status); do echo $id; gh run cancel $id; done
-gh run list --status in_progress
-for id in $(gh run list --status in_progress --limit 500 --jq ".[] | .databaseId" --json databaseId,status); do echo $id; gh run cancel $id; done
-gh run list --status queued
-for id in $(gh run list --status queued --limit 500 --jq ".[] | .databaseId" --json databaseId,status); do echo $id; gh run cancel $id; done
-gh run list --status waiting
-for id in $(gh run list --status waiting --limit 500 --jq ".[] | .databaseId" --json databaseId,status); do echo $id; gh run cancel $id; done
+
+DEPTH=${1:-50}
+
+gh run list --status in_progress --limit $DEPTH
+for id in $(gh run list --status in_progress --limit $DEPTH --jq ".[] | .databaseId" --json databaseId,status); do echo $id; gh run cancel $id; done
+gh run list --status queued --limit $DEPTH
+for id in $(gh run list --status queued --limit $DEPTH --jq ".[] | .databaseId" --json databaseId,status); do echo $id; gh run cancel $id; done
+gh run list --status waiting --limit $DEPTH
+for id in $(gh run list --status waiting --limit $DEPTH --jq ".[] | .databaseId" --json databaseId,status); do echo $id; gh run cancel $id; done
