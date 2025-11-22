ba9f6179747f613a4276b26b71d559bea4f6b340
randymcmillan <randymcmillan@protonmail.com>
2025-11-22 09:56:18 -0500

    src/lib/sub_commands/bech32_to_any.rs:json output

--- src/lib/sub_commands/bech32_to_any.rs
+++ src/lib/sub_commands/bech32_to_any.rs
@@ -2,6 +2,7 @@ use anyhow::Result;
 use clap::Parser;
 use gnostr_types::{NostrBech32, PrivateKey};
 use nostr_0_34_1::bech32;
+use serde_json::json;
 
 #[derive(Parser, Debug, Clone)]
 pub struct Bech32ToAnySubCommand {
@@ -14,69 +15,42 @@ pub fn bech32_to_any(sub_command_args: &Bech32ToAnySubCommand) -> Result<()> {
     let bech32 = sub_command_args.bech32_string.trim();
 
     if let Some(nb32) = NostrBech32::try_from_string(bech32) {
-        match nb32 {
-            NostrBech32::NAddr(na) => {
-                println!("Event Address:");
-                println!("  d={}", na.d);
-                println!(
-                    "  relays={}",
-                    na.relays
-                        .iter()
-                        .map(|r| r.as_str().to_owned())
-                        .collect::<Vec<String>>()
-                        .join(", ")
-                );
-                println!("  kind={}", Into::<u32>::into(na.kind));
-                println!("  author={}", na.author.as_hex_string());
-            }
-            NostrBech32::NEvent(ne) => {
-                println!("Event Pointer:");
-                println!("  id={}", ne.id.as_hex_string());
-                println!(
-                    "  relays={}",
-                    ne.relays
-                        .iter()
-                        .map(|r| r.as_str().to_owned())
-                        .collect::<Vec<String>>()
-                        .join(", ")
-                );
-                if let Some(kind) = ne.kind {
-                    println!("  kind={}", Into::<u32>::into(kind));
-                }
-                if let Some(author) = ne.author {
-                    println!("  author={}", author.as_hex_string());
-                }
-            }
-            NostrBech32::Id(id) => {
-                println!("{}", id.as_hex_string());
-            }
-            NostrBech32::Profile(profile) => {
-                println!("Profile:");
-                println!("  pubkey: {}", profile.pubkey.as_hex_string());
-                println!(
-                    "  relays={}",
-                    profile
-                        .relays
-                        .iter()
-                        .map(|r| r.as_str().to_owned())
-                        .collect::<Vec<String>>()
-                        .join(", ")
-                );
-            }
-            NostrBech32::Pubkey(pubkey) => {
-                println!("Pubkey: {}", pubkey.as_hex_string());
-            }
-            NostrBech32::Relay(url) => {
-                println!("Relay URL: {}", url.0);
-            }
-            NostrBech32::CryptSec(_) => todo!(),
-        }
+        let json_output = match nb32 {
+            NostrBech32::NAddr(na) => json!({
+                "type": "Event Address",
+                "d": na.d,
+                "relays": na.relays.iter().map(|r| r.as_str().to_owned()).collect::<Vec<String>>(),
+                "kind": Into::<u32>::into(na.kind),
+                "author": na.author.as_hex_string(),
+            }),
+            NostrBech32::NEvent(ne) => json!({
+                "type": "Event Pointer",
+                "id": ne.id.as_hex_string(),
+                "relays": ne.relays.iter().map(|r| r.as_str().to_owned()).collect::<Vec<String>>(),
+                "kind": ne.kind.map(|k| Into::<u32>::into(k)),
+                "author": ne.author.map(|a| a.as_hex_string()),
+            }),
+            NostrBech32::Id(id) => json!({ "hex": id.as_hex_string() }),
+            NostrBech32::Profile(profile) => json!({
+                "type": "Profile",
+                "pubkey": profile.pubkey.as_hex_string(),
+                "relays": profile.relays.iter().map(|r| r.as_str().to_owned()).collect::<Vec<String>>(),
+            }),
+            NostrBech32::Pubkey(pubkey) => json!({ "pubkey": pubkey.as_hex_string() }),
+            NostrBech32::Relay(url) => json!({ "relay_url": url.0 }),
+            NostrBech32::CryptSec(_) => json!({ "error": "CryptSec not implemented" }),
+        };
+        println!("{}", serde_json::to_string_pretty(&json_output)?);
     } else if let Ok(mut key) = PrivateKey::try_from_bech32_string(bech32) {
-        println!("Private Key: {}", key.as_hex_string());
+        let json_output = json!({ "private_key": key.as_hex_string() });
+        println!("{}", serde_json::to_string_pretty(&json_output)?);
     } else {
         let (hrp, data) = bech32::decode(bech32).unwrap();
-        println!("HRP = {}", hrp);
-        println!("DATA = \"{:?}\"", String::from_utf8_lossy(&data));
+        let json_output = json!({
+            "hrp": hrp.to_string(),
+            "data": String::from_utf8_lossy(&data),
+        });
+        println!("{}", serde_json::to_string_pretty(&json_output)?);
     }
     Ok(())
 }
