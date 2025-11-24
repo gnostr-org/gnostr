059b66351ddb9d863a9a089bedcee1c80f6e8428
randymcmillan <randymcmillan@protonmail.com>
2025-11-24 12:33:31 -0500

    src/lib/sub_commands/rad.rs:begin radicle support

---
+++ src/lib/sub_commands/rad.rs
@@ -0,0 +1,47 @@
+use clap::Parser;
+use anyhow::Result;
+
+#[derive(Parser, Debug, Clone)]
+#[clap(
+    about = "A tool for interacting with the Radicle network.",
+    help_template = "\
+{about-with-newline}
+{usage-heading} {usage}
+
+{all-args}
+"
+)]
+pub struct RadSubCommand {
+    /// Placeholder for a rad command argument.
+    #[arg(long)]
+    pub example_arg: bool,
+}
+
+pub async fn rad(sub_command_args: &RadSubCommand) -> Result<(), Box<dyn std::error::Error>> {
+    if sub_command_args.example_arg {
+        println!("Rad subcommand executed with --example-arg");
+    } else {
+        println!("Rad subcommand executed.");
+    }
+    // Placeholder for future rad logic
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_rad_subcommand() {
+        let args = RadSubCommand { example_arg: false };
+        let result = rad(&args).await;
+        assert!(result.is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_rad_subcommand_with_arg() {
+        let args = RadSubCommand { example_arg: true };
+        let result = rad(&args).await;
+        assert!(result.is_ok());
+    }
+}
