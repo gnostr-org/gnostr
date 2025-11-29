c7cb25f08d761bb5f52542f051363e433f434bd9
randymcmillan <randymcmillan@protonmail.com>
2025-11-29 15:01:08 -0500

    fix(legit): fix unused variable warning
    00-000003da

--- src/lib/legit/command.rs
+++ src/lib/legit/command.rs
@@ -28,7 +28,7 @@ use gnostr_legit::{gitminer, gitminer::Gitminer};
 pub async fn run_legit_command(mut opts: gitminer::Options) -> io::Result<()> {
 
     let _start = SystemTime::now();
-    let system_time = SystemTime::now();
+    let _system_time = SystemTime::now();
 
     let repo = Repository::open(&opts.repo).expect("Couldn't open repository");
 
