00093a72dc46f7b4c67b421dd1d6ac31ed02136b
randymcmillan <randymcmillan@protonmail.com>
2025-11-29 14:46:07 -0500

    refactor(p2p): remove unused time import
    00-00000574

--- src/lib/p2p/lookup.rs
+++ src/lib/p2p/lookup.rs
@@ -25,7 +25,6 @@ use libp2p::{
 };
 use libp2p::swarm::SwarmEvent;
 use log::debug;
-use std::time::Duration;
 use clap::{Parser, ValueEnum};
 use crate::p2p::network_config::Network;
 use thiserror::Error;
