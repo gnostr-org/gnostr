6589b726e9b954f8bb84c4efa8b1afda8258e8c1
randymcmillan <randymcmillan@protonmail.com>
2025-12-01 17:32:44 -0500

    src/bin/gnostr-client.rs:clap

--- src/bin/gnostr-client.rs
+++ src/bin/gnostr-client.rs
@@ -1,19 +1,30 @@
+use clap::Parser;
 use gnostr::types::{NostrClient, UncheckedUrl};
 use tokio::sync::mpsc;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value = "wss://relay.damus.io")]
+    relay_url: String,
+    #[arg(short, long, default_value = "test")]
+    channel_id: String,
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
 
+    let args = Args::parse();
+
     let (tx, mut rx) = mpsc::channel(100);
 
     let mut client = NostrClient::new(tx);
 
-    let relay_url = UncheckedUrl("wss://relay.damus.io".to_string());
+    let relay_url = UncheckedUrl(args.relay_url);
     client.connect_relay(relay_url).await?;
 
-    let channel_id = "test".to_string();
-    client.subscribe_to_channel(channel_id).await;
+    client.subscribe_to_channel(args.channel_id).await;
 
     println!("Listening for events...");
 
