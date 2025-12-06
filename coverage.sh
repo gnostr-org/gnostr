8fc3f3946842cec72a03cad61cf6cdf166c8090f
randymcmillan <randymcmillan@protonmail.com>
2025-12-05 22:56:28 -0500

    coverage.sh

--- coverage.sh
+++ coverage.sh
@@ -71,9 +71,9 @@ find "$PROFRAW_DIR" -name "*.profraw" -print0 | xargs -0 llvm-profdata merge -sp
 # Use llvm-cov to generate the report
 BINARY_PATH=target/debug/$BINARY_NAME
 echo "OBJECTS: $OBJECTS"
-echo "Executing: llvm-cov show $OBJECTS --ignore-filename-regex \".*cargo/registry.*\" --instr-profile=$PROFRAW_DIR/default.profdata --format=html --output-dir=./coverage --show-line-counts-or-regions --show-instantiations --show-regions --Xdemangler=rustfilt"
+echo "Executing: llvm-cov show $OBJECTS --ignore-filename-regex \".*cargo/registry.*|/rustc/.*\" --instr-profile=$PROFRAW_DIR/default.profdata --format=html --output-dir=./coverage --show-line-counts-or-regions --show-instantiations --show-regions --Xdemangler=rustfilt"
 llvm-cov show $OBJECTS \
-    --ignore-filename-regex ".*cargo/registry.*" \
+    --ignore-filename-regex ".*cargo/registry.*|/rustc/.*" \
     --instr-profile=$PROFRAW_DIR/default.profdata \
     --format=html \
     --output-dir=./coverage \
