544ecda970c29d164a499ccdff1231629deebee3
randymcmillan <randymcmillan@protonmail.com>
2025-12-07 17:02:15 -0500

    src/lib/test_utils/mod.rs

--- src/lib/test_utils/mod.rs
+++ src/lib/test_utils/mod.rs
@@ -292,7 +292,7 @@ impl Drop for CliTester {
 impl Drop for CliTester {
     fn drop(&mut self) {
         // Ensure the child process is killed when the CliTester is dropped.
-        let _ = self.expectrl_session.get_process_mut().kill();
+        let _ = self.expectrl_session.get_process_mut().terminate();
     }
 }
 
