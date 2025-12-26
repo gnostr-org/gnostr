e02afaf2d03b10cb7393d807f5788fee428a4bdc
randymcmillan <randymcmillan@protonmail.com>
2025-12-26 10:30:09 -0500

    src/bin/gnostr-legit.rs

--- src/bin/gnostr-legit.rs
+++ src/bin/gnostr-legit.rs
@@ -1,17 +1,16 @@
 /// #![allow(unused)]
 /// #![allow(dead_code)]
-/// 
+///
 /// #![allow(unused)]
 /// #![allow(dead_code)]
-
 use clap::Parser;
 use gnostr::legit::command;
 use gnostr_legit::gitminer;
-use std::io::{self};
+use std::convert::TryInto;
 use std::env;
+use std::io::{self};
 use std::process::Command;
 use std::thread;
-use std::convert::TryInto;
 use time::OffsetDateTime;
 
 #[derive(Parser, Debug)]
@@ -53,7 +52,8 @@ async fn main() -> io::Result<()> {
         };
         [String::from_utf8(output.stdout)
             .map_err(|non_utf8| String::from_utf8_lossy(non_utf8.as_bytes()).into_owned())
-            .unwrap()].to_vec()
+            .unwrap()]
+        .to_vec()
     };
 
     let count = thread::available_parallelism()?.get();
