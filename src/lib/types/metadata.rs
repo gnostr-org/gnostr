13be0b6c902f03b33af74d60ea8dd544a73da519
randymcmillan <randymcmillan@protonmail.com>
2025-12-27 11:11:00 -0500

    src/lib/types/metadata.rs

--- src/lib/types/metadata.rs
+++ src/lib/types/metadata.rs
@@ -7,7 +7,7 @@ use super::versioned::metadata::MetadataV1;
 /// events to fail. We treat these in our get() function the same as if the key
 /// did not exist.
 
-pub const DEFAULT_AVATAR: &str = "https://avatars.githubusercontent.com/u/135379339?s=400&u=11cb72cccbc2b13252867099546074c50caef1ae&v=4";
-pub const DEFAULT_BANNER: &str = "https://raw.githubusercontent.com/gnostr-org/gnostr-icons/refs/heads/master/banner/1024x341.png";
+pub(super) const DEFAULT_AVATAR: &str = "https://avatars.githubusercontent.com/u/135379339?s=400&u=11cb72cccbc2b13252867099546074c50caef1ae&v=4";
+pub(super) const DEFAULT_BANNER: &str = "https://raw.githubusercontent.com/gnostr-org/gnostr-icons/refs/heads/master/banner/1024x341.png";
 
 pub type Metadata = MetadataV1;
