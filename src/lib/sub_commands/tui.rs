2112ef87eeed9bbec303087261949e2f3ae1afa2
randymcmillan <randymcmillan@protonmail.com>
2026-01-10 18:09:27 -0500

    types:migration:apply cargo fmt

--- src/lib/sub_commands/tui.rs
+++ src/lib/sub_commands/tui.rs
@@ -28,7 +28,6 @@ use gnostr_asyncgit::{
     AsyncGitNotification,
     sync::{RepoPath, utils::repo_work_dir},
 };
-use nostr_sdk_0_37_0::Keys;
 use ratatui::backend::CrosstermBackend;
 use scopeguard::defer;
 use scopetime::{self, scope_time};
@@ -43,6 +42,7 @@ use crate::{
     input::{Input, InputEvent, InputState},
     keys::KeyConfig,
     spinner::Spinner,
+    types::Keys,
     ui::style::Theme,
     watcher::RepoWatcher,
     weeble, wobble,
