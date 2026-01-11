423417a4f3e8140194693a2550aa6e4089172c47
randymcmillan <randymcmillan@protonmail.com>
2026-01-11 12:58:28 -0500

    examples/bech32_test.rs:use gnostr::types::PublicKey

--- examples/bech32_test.rs
+++ examples/bech32_test.rs
@@ -1,9 +1,9 @@
-use nostr_0_34_1::{PublicKey, nips::nip19::FromBech32};
+use gnostr::types::PublicKey;
 
 fn main() {
     let bech32_str = "npub10elfcs4fr0l0r8af98jlmgdh9c8efajjp7d99q03f6tbpkct3uhqsfq0z6";
     println!("Decoding: {}", bech32_str);
-    match PublicKey::from_bech32(bech32_str) {
+    match PublicKey::try_from_bech32_string(bech32_str, false) {
         Ok(pubkey) => {
             println!("Successfully decoded pubkey: {}", pubkey);
         }
