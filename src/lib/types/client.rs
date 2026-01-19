39512ce4d3226669ca530ea0e424a8530dc5eddb
randymcmillan <randymcmillan@protonmail.com>
2026-01-19 09:17:47 -0500

    src::nip44_direct_message:initial impl

--- src/lib/types/client.rs
+++ src/lib/types/client.rs
@@ -269,7 +269,10 @@ impl Client {
             .map_err(|e| Error::Custom(e.into()))?;
 
         // Convert k256::ecdsa::SigningKey to k256::elliptic_curve::SecretKey
-        let secret_key = SecretKey::from_bytes(sender_secret_key.0.secret_bytes().as_slice())
+        let secret_key = SecretKey::from_bytes(
+            FieldBytes::from_slice(sender_secret_key.0.secret_bytes().as_slice())
+                .map_err(|e| Error::Custom(format!("FieldBytes conversion error: {:?}", e).into()))?,
+        )
             .map_err(|e| Error::Custom(e.into()))?;
 
         // Convert PublicKey to k256::elliptic_curve::PublicKey
