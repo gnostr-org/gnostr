8211bca1e79be50ed7d050b4190b05591311fc79
randymcmillan <randymcmillan@protonmail.com>
2026-01-20 14:21:17 -0500

    remove:statics:templates:test_screenshotes

--- statics/js/repo.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("branchSelect").addEventListener("change", (selected) => {
-    console.log(selected)
-    window.location.href = selected.target.value
-  })
-})
