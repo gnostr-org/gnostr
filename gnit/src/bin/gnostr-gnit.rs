a1bbbb96e32874fdbcaad998dac681f12c1ea912
randymcmillan <randymcmillan@protonmail.com>
2026-01-23 16:44:57 -0500

    src/bin/gnostr-gnit.rs:route:dm

--- gnit/src/bin/gnostr-gnit.rs
+++ gnit/src/bin/gnostr-gnit.rs
@@ -260,8 +260,8 @@ async fn main() -> Result<(), anyhow::Error> {
         .route("/gnostr/", get(methods::gnostr::handle)) // Handle trailing slash
         .route("/thread/:thread_id", get(methods::index::handle_spa)) // Thread view
         .route("/messages", get(methods::index::handle_spa)) // Messages view
+        .route("/dm", get(methods::index::handle_spa)) // Messages view
         .route("/notifications", get(methods::index::handle_spa)) // Notifications view
-        .route("/messages", get(methods::index::handle_spa)) // Messages view
         .route("/settings", get(methods::index::handle_spa)) // Settings view
         .route("/profile/:pubkey", get(methods::index::handle_spa)) // Profile view
         .route("/nip34", get(methods::index::handle_spa)) // NIP-34 view
