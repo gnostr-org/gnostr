c83e8d8e392c4194951189a1654d9afedc159a4c
randymcmillan <randymcmillan@protonmail.com>
2026-01-23 15:00:17 -0500

    intermediate:working

--- gnit/src/bin/gnostr-gnit.rs
+++ gnit/src/bin/gnostr-gnit.rs
@@ -262,6 +262,7 @@ async fn main() -> Result<(), anyhow::Error> {
         .route("/messages", get(methods::index::handle_spa)) // Messages view
         .route("/notifications", get(methods::index::handle_spa)) // Notifications view
         .route("/settings", get(methods::index::handle_spa)) // Settings view
+        .route("/profile/:pubkey", get(methods::index::handle_spa)) // Profile view
         .route("/nip34", get(methods::index::handle_spa)) // NIP-34 view
         .route("/nip34-global", get(methods::index::handle_spa)) // NIP-34 global view
         .route(
