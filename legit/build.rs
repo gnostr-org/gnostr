b9260e7981715db4cced0bd3805d8ae8ffb43e09
randymcmillan <randymcmillan@protonmail.com>
2026-01-26 13:47:22 -0500

    fix(deps): Update Homebrew path in build scripts

--- legit/build.rs
+++ legit/build.rs
@@ -587,7 +587,7 @@ fn main() {
                 // The `openssl` crate generally handles finding these libraries.
                 // If you need explicit linking (less recommended):
                 if target_os == "aarch64-apple-darwin" {
-                    println!("cargo:rustc-link-search=native=/opt/homebrew/opt/openssl@3/lib");
+                    println!("cargo:rustc-link-search=native=/usr/local/opt/openssl@3/lib");
                     println!("cargo:rustc-link-lib=dylib=ssl@3");
                     println!("cargo:rustc-link-lib=dylib=crypto@3");
                 } else if target_os == "x86_64-apple-darwin" {
