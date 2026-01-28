0fbdd40e73cb8ac3551f9b33bd337000f4916af2
randymcmillan <randymcmillan@protonmail.com>
2026-01-28 17:30:06 -0500

    asyncgit:update:intermediate

--- asyncgit/src/gitui/syntax_parser/mod.rs
+++ asyncgit/src/gitui/syntax_parser/mod.rs
@@ -88,7 +88,7 @@ fn determine_lang(path: &Path) -> Option<(&'static Grammar, Language)> {
     let file_language = tree_sitter_grammar_repository::Language::from_file_name(path)?;
     let grammar_variant = file_language.grammar();
     let params = Grammar::highlight_configuration_params(grammar_variant);
-    Some((&grammar_variant, Language((params.language).into_raw() as *const _)))}
+    Some((&grammar_variant, tree_sitter::Language((params.language).into_raw() as *const _)))}
 
 fn create_highlight_config(grammar_variant: &'static Grammar, language: &Language) -> HighlightConfiguration {
     let params = Grammar::highlight_configuration_params(*grammar_variant);
