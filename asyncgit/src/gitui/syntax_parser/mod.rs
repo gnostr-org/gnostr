06aa6d773cf308b04f0cbf9e9b0676763ade727b
randymcmillan <randymcmillan@protonmail.com>
2026-01-28 17:11:34 -0500

    asyncgit:update:intermediate

--- asyncgit/src/gitui/syntax_parser/mod.rs
+++ asyncgit/src/gitui/syntax_parser/mod.rs
@@ -1,7 +1,7 @@
+use tree_sitter_grammar_repository::{grammar::Grammar, HighlightConfigurationParams};
 use std::{cell::RefCell, collections::HashMap, ops::Range, path::Path};
 use tree_sitter::Language;
 use tree_sitter_highlight::{Highlight, HighlightConfiguration, HighlightEvent, Highlighter};
-
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum SyntaxTag {
     Attribute,
@@ -84,123 +84,16 @@ fn tags_by_highlight_index() -> [SyntaxTag; 22] {
     ]
 }
 
-/// The defaults for these seem to exist in the `package.json` of each repo:
-/// `curl https://raw.githubusercontent.com/tree-sitter/tree-sitter-html/master/package.json | jq -r '."tree-sitter"'`
 fn determine_lang(path: &Path) -> Option<Language> {
     let extension = path.extension().and_then(|s| s.to_str())?;
-
-    match extension {
-        "rs" => Some(tree_sitter_rust::language()),
-        "toml" => Some(tree_sitter_toml::language()),
-        "js" => Some(tree_sitter_javascript::language()),
-        "c" | "h" => Some(tree_sitter_c::language()),
-        "json" => Some(tree_sitter_json::LANGUAGE),
-        "cc" => Some(tree_sitter_cpp::language()),
-        "rb" => Some(tree_sitter_ruby::language()),
-        "hs" => Some(tree_sitter_haskell::language()),
-        "go" => Some(tree_sitter_go::language()),
-        "cs" => Some(tree_sitter_c_sharp::language()),
-        "py" => Some(tree_sitter_python::language()),
-        "ts" => Some(tree_sitter_typescript::language_typescript()),
-        "tsx" => Some(tree_sitter_typescript::language_tsx()),
-        "sh" | "bash" | ".bashrc" | ".bash_profile" | "ebuild" | "eclass" => {
-            Some(tree_sitter_bash::LANGUAGE)
-        }
-        "php" => Some(tree_sitter_php::language()),
-        "java" => Some(tree_sitter_java::language()),
-        "scala" | "sbt" => Some(tree_sitter_scala::language()),
-        "ml" => Some(tree_sitter_ocaml::language_ocaml()),
-        "mli" => Some(tree_sitter_ocaml::language_ocaml_interface()),
-        "html" => Some(tree_sitter_html::language()),
-        "ex" | "exs" => Some(tree_sitter_elixir::language()),
-        _ => None,
-    }
+    Grammar::from_file_name(extension).map(Into::into)
 }
 
 fn create_highlight_config(lang: &Language) -> HighlightConfiguration {
-    let (highlights_query, injections_query, locals_query) =
-        if lang == &tree_sitter_rust::language() {
-            (
-                tree_sitter_rust::HIGHLIGHT_QUERY,
-                tree_sitter_rust::HIGHLIGHT_QUERY,
-                "",
-            )
-        } else if lang == &tree_sitter_toml::language() {
-            (tree_sitter_toml::HIGHLIGHT_QUERY, "", "")
-        } else if lang == &tree_sitter_javascript::language() {
-            (
-                tree_sitter_javascript::HIGHLIGHT_QUERY,
-                tree_sitter_javascript::INJECTION_QUERY,
-                tree_sitter_javascript::LOCALS_QUERY,
-            )
-        } else if lang == &tree_sitter_c::language() {
-            (tree_sitter_c::HIGHLIGHT_QUERY, "", "")
-        } else if lang == &tree_sitter_json::LANGUAGE {
-            (tree_sitter_json::HIGHLIGHTS_QUERY, "", "")
-        } else if lang == &tree_sitter_cpp::language() {
-            (tree_sitter_cpp::HIGHLIGHT_QUERY, "", "")
-        } else if lang == &tree_sitter_ruby::language() {
-            (
-                tree_sitter_ruby::HIGHLIGHT_QUERY,
-                "",
-                tree_sitter_ruby::LOCALS_QUERY,
-            )
-        } else if lang == &tree_sitter_haskell::language() {
-            (
-                tree_sitter_haskell::HIGHLIGHTS_QUERY,
-                "",
-                tree_sitter_haskell::LOCALS_QUERY,
-            )
-        } else if lang == &tree_sitter_go::language() {
-            (tree_sitter_go::HIGHLIGHT_QUERY, "", "")
-        } else if lang == &tree_sitter_c_sharp::language() {
-            (tree_sitter_c_sharp::HIGHLIGHT_QUERY, "", "")
-        } else if lang == &tree_sitter_python::language() {
-            (tree_sitter_python::HIGHLIGHT_QUERY, "", "")
-        } else if lang == &tree_sitter_typescript::language_typescript()
-            || lang == &tree_sitter_typescript::language_tsx()
-        {
-            (
-                tree_sitter_typescript::HIGHLIGHT_QUERY,
-                "",
-                tree_sitter_typescript::LOCALS_QUERY,
-            )
-        } else if lang == &tree_sitter_bash::LANGUAGE {
-            (tree_sitter_bash::HIGHLIGHT_QUERY, "", "")
-        } else if lang == &tree_sitter_php::language() {
-            (
-                tree_sitter_php::HIGHLIGHTS_QUERY,
-                tree_sitter_php::INJECTIONS_QUERY,
-                "",
-            )
-        } else if lang == &tree_sitter_java::language() {
-            (tree_sitter_java::HIGHLIGHT_QUERY, "", "")
-        } else if lang == &tree_sitter_scala::language() {
-            (
-                tree_sitter_scala::HIGHLIGHTS_QUERY,
-                "",
-                tree_sitter_scala::LOCALS_QUERY,
-            )
-        } else if lang == &tree_sitter_ocaml::language_ocaml() {
-            (
-                tree_sitter_ocaml::HIGHLIGHTS_QUERY,
-                "",
-                tree_sitter_ocaml::LOCALS_QUERY,
-            )
-        } else if lang == &tree_sitter_html::language() {
-            (
-                tree_sitter_html::HIGHLIGHTS_QUERY,
-                tree_sitter_html::INJECTIONS_QUERY,
-                "",
-            )
-        } else if lang == &tree_sitter_elixir::language() {
-            (tree_sitter_elixir::HIGHLIGHTS_QUERY, "", "")
-        } else {
-            panic!("Undefined language");
-        };
+    let params = Grammar::highlight_configuration_params(lang.into());
 
     let mut highlight_config =
-        HighlightConfiguration::new(*lang, highlights_query, injections_query, locals_query)
+        HighlightConfiguration::new(*lang, params.highlights_query, params.injection_query, params.locals_query)
             .unwrap();
 
     highlight_config.configure(&tags_by_highlight_index());
