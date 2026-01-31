b88f2b48595f83632af0ca54df251df54009020f
randymcmillan <randymcmillan@protonmail.com>
2026-01-30 20:36:35 -0500

    intermediate:javascript module type

--- js/src/bin/w2ui.rs
+++ js/src/bin/w2ui.rs
@@ -29,14 +29,33 @@ async fn main() {
         let mut tags = String::new();
         // Explicitly load db.js, model.js, and ui/state.js first due to dependencies
         tags.push_str("<script src=\"/js/jquery-4.0.0.js\"></script>\n");
-        tags.push_str("<script src=\"/js/ui/w2ui-1.5.min.js\"></script>\n");
+        tags.push_str("<script src=\"/js/ui/w2ui-1.5.js\"></script>\n");
         tags.push_str("<script src=\"/js/core.js\"></script>\n");
         tags.push_str("<script src=\"/js/db.js\"></script>\n");
         tags.push_str("<script src=\"/js/model.js\"></script>\n");
         tags.push_str("<script src=\"/js/ui/state.js\"></script>\n");
 
+        let module_filenames: std::collections::HashSet<&str> = [
+            "query.js",
+            "ui/w2base.js",
+            "ui/w2compat.js",
+            "ui/w2field.js",
+            "ui/w2form.js",
+            "ui/w2grid.js",
+            "ui/w2layout.js",
+            "ui/w2locale.js",
+            "ui/w2popup.js",
+            "ui/w2sidebar.js",
+            "ui/w2tabs.js",
+            "ui/w2toolbar.js",
+            "ui/w2tooltip.js",
+            "ui/w2utils.js",
+        ].iter().cloned().collect();
+
         let mut filenames: Vec<_> = js_assets_map.keys().cloned().collect();
         filenames.sort();
+
+
         for filename in filenames {
             // Skip db.js, model.js and ui/state.js as they're already added
             if filename == "core.js" ||
@@ -44,10 +63,15 @@ async fn main() {
                 filename == "model.js" ||
                 filename == "ui/state.js" ||
                 filename == "ui/jquery-4.0.0.js" ||
-                filename == "ui/w2ui-1.5.min.js" {
+                filename == "ui/w2ui-1.5.js" {
                 continue;
             }
-            tags.push_str(&format!("<script src=\"/js/{}\"></script>\n", filename));
+            let script_tag = if module_filenames.contains(filename.as_str()) {
+                format!("<script type=\"module\" src=\"/js/{}\"></script>\n", filename)
+            } else {
+                format!("<script src=\"/js/{}\"></script>\n", filename)
+            };
+            tags.push_str(&script_tag);
         }
         tags
     };
@@ -78,23 +102,129 @@ async fn main() {
 
     let index_html_content = format!(
         r#"<!DOCTYPE html>
-        <html lang="en">
-        <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Embedded JS and CSS</title>
-        <link rel="icon" href="/images/favicon.ico" type="image/x-icon">
+<html>
+<head>
+-        <meta charset="utf-8">
+-        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>W2UI Demo: combo/1</title>
             {}
+</head>
+<body>
+
+<div id="main" style="width: 100%; height: 400px;"></div>
+
+        <link rel="icon" href="/images/favicon.ico" type="image/x-icon">
         </head>
         <body>
             <h1>Hello from Rust with embedded w2ui JS and CSS!</h1>
             {}
+    <div id="container-busy">
+      <div class="loader" title="Loading...">
+        <img class="dark-invert" src="/images/loader-fragment.svg" />
+        HELLO! from div
+      </div>
+    </div>
+    <div id="container-welcome" class="hide">
+      <div class="hero-box">
+        <div class="padded">
+          <h1>
+            gnostr
+            <img class="icon svg" src="/images/logo-inverted.svg" />
+          </h1>
+          <p>Please access with a nos2x compatible browser.</p>
+        </div>
+      </div>
+    </div>
+<script>
+let config = {{
+    layout: {{
+        name: 'layout',
+        padding: 0,
+        panels: [
+            {{ type: 'left', size: 200, resizable: true, minSize: 120 }},
+            {{ type: 'main', minSize: 550, overflow: 'hidden' }}
+        ]
+    }},
+    sidebar: {{
+        name: 'sidebar',
+        nodes: [
+            {{ id: 'general', text: 'General', group: true, expanded: true, nodes: [
+                {{ id: 'grid1', text: 'Grid 1', icon: 'fa fa-pencil-square-o', selected: true }},
+                {{ id: 'grid2', text: 'Grid 2', icon: 'fa fa-pencil-square-o' }},
+                {{ id: 'html', text: 'Some HTML', icon: 'fa fa-list-alt' }}
+            ]}}
+        ],
+        onClick(event) {{
+            switch (event.target) {{
+                case 'grid1':
+                    layout.html('main', grid1)
+                    break
+                case 'grid2':
+                    layout.html('main', grid2)
+                    break
+                case 'html':
+                    layout.html('main', '<div style="padding: 10px">Some HTML</div>')
+                    query(layout.el('main'))
+                        .removeClass('w2ui-grid')
+                        .css({{
+                            'border-left': '1px solid #efefef'
+                        }})
+                    break
+            }}
+        }}
+    }},
+    grid1: {{
+        name: 'grid1',
+        columns: [
+            {{ field: 'fname', text: 'First Name', size: '180px' }},
+            {{ field: 'lname', text: 'Last Name', size: '180px' }},
+            {{ field: 'email', text: 'Email', size: '40%' }},
+            {{ field: 'sdate', text: 'Start Date', size: '120px' }}
+        ],
+        records: [
+            {{ recid: 1, fname: 'John', lname: 'Doe', email: 'jdoe@gmail.com', sdate: '4/3/2012' }},
+            {{ recid: 2, fname: 'Stuart', lname: 'Motzart', email: 'jdoe@gmail.com', sdate: '4/3/2012' }},
+            {{ recid: 3, fname: 'Jin', lname: 'Franson', email: 'jdoe@gmail.com', sdate: '4/3/2012' }},
+            {{ recid: 4, fname: 'Susan', lname: 'Ottie', email: 'jdoe@gmail.com', sdate: '4/3/2012' }},
+            {{ recid: 5, fname: 'Kelly', lname: 'Silver', email: 'jdoe@gmail.com', sdate: '4/3/2012' }},
+            {{ recid: 6, fname: 'Francis', lname: 'Gatos', email: 'jdoe@gmail.com', sdate: '4/3/2012' }},
+            {{ recid: 7, fname: 'Mark', lname: 'Welldo', email: 'jdoe@gmail.com', sdate: '4/3/2012' }},
+            {{ recid: 8, fname: 'Thomas', lname: 'Bahh', email: 'jdoe@gmail.com', sdate: '4/3/2012' }},
+            {{ recid: 9, fname: 'Sergei', lname: 'Rachmaninov', email: 'jdoe@gmail.com', sdate: '4/3/2012' }}
+        ]
+    }},
+    grid2: {{
+        name: 'grid2',
+        columns: [
+            {{ field: 'state', text: 'State', size: '80px' }},
+            {{ field: 'title', text: 'Title', size: '100%' }},
+            {{ field: 'priority', text: 'Priority', size: '80px', attr: 'align="center"' }}
+        ],
+        records: [
+            {{ recid: 1, state: 'Open', title: 'Short title for the record', priority: 2 }},
+            {{ recid: 2, state: 'Open', title: 'Short title for the record', priority: 3 }},
+            {{ recid: 3, state: 'Closed', title: 'Short title for the record', priority: 1 }}
+        ]
+    }}
+}}
+
             {}
-        </body>
+let layout = new w2layout(config.layout)
+let sidebar = new w2sidebar(config.sidebar)
+let grid1 = new w2grid(config.grid1)
+let grid2 = new w2grid(config.grid2)
+// initialization
+layout.render('#main')
+layout.html('left', sidebar)
+layout.html('main', grid1)
+</script>
+
+</body>
+
         </html>"#,
         link_tags, // Insert CSS link tags here
-        script_tags,
-        images_tags // Insert image tags here
+        images_tags, // Insert image tags here
+        script_tags
     );
 
     let index_html_route = warp::path::end()
