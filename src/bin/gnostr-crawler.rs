f17cb9a9bc803b20e39615d9efff4e772e2138f8
randymcmillan <randymcmillan@protonmail.com>
2026-02-15 22:49:39 -0500

    src/bin/gnostr-crawler.rs:update serve async watch service

--- src/bin/gnostr-crawler.rs
+++ src/bin/gnostr-crawler.rs
@@ -1,35 +1,14 @@
 use clap::Parser;
-use gnostr_crawler::{Cli, Commands, run_sniper, run_watch, run_nip34, run_api_server};
+use gnostr_crawler::{Cli, init_tracing, dispatch_cli_command};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tracing_subscriber::fmt()
-        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env()
-        // .add_directive("nostr_sdk::relay=off".parse()?)
-        // .add_directive("hyper=off".parse()?)
-
-        /**/)/**/
-        .init();
+    init_tracing()?;
 
     let cli = Cli::parse();
+    let client = reqwest::Client::new(); // Centralized client creation
 
-    match &cli.command {
-        Commands::Sniper { nip, shitlist } => {
-            run_sniper(*nip, shitlist.clone()).await?;
-        }
-        Commands::Watch { shitlist } => {
-            run_watch(shitlist.clone()).await?;
-        }
-        Commands::Nip34 { shitlist } => {
-            run_nip34(shitlist.clone()).await?;
-        }
-        Commands::Crawl(args) => {
-            gnostr_crawler::run(args).await?;
-        }
-        Commands::Serve { port } => {
-            run_api_server(*port).await?;
-        }
-    }
+    dispatch_cli_command(cli, &client).await?;
 
     Ok(())
 }
