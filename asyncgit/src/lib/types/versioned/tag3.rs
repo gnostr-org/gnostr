7da03d68d8c68c731c77b0fc2d708a6f1060dc66
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2026-02-20 14:43:44 -0500

    asyncgit/src/lib/types/versioned/tag3.rs:impl From:TagV2 for TagV3

--- asyncgit/src/lib/types/versioned/tag3.rs
+++ asyncgit/src/lib/types/versioned/tag3.rs
@@ -520,6 +520,189 @@ impl fmt::Display for TagV3 {
     }
 }
 
+impl From<crate::types::versioned::tag2::TagV2> for TagV3 {
+    fn from(tag_v2: crate::types::versioned::tag2::TagV2) -> Self {
+        match tag_v2 {
+            crate::types::versioned::tag2::TagV2::Address {
+                kind,
+                pubkey,
+                d,
+                relay_url,
+                marker,
+                trailing,
+            } => {
+                let mut vec = vec![
+                    "a".to_owned(),
+                    format!("{}:{}:{}", Into::<u32>::into(kind), pubkey, d),
+                ];
+                if let Some(ru) = relay_url {
+                    vec.push(ru.0);
+                } else if marker.is_some() || !trailing.is_empty() {
+                    vec.push("".to_owned());
+                }
+                if let Some(m) = marker {
+                    vec.push(m);
+                } else if !trailing.is_empty() {
+                    vec.push("".to_owned());
+                }
+                vec.extend(trailing);
+                TagV3(vec)
+            }
+            crate::types::versioned::tag2::TagV2::ContentWarning {
+                warning,
+                trailing,
+            } => {
+                let mut vec = vec!["content-warning".to_owned(), warning];
+                vec.extend(trailing);
+                TagV3(vec)
+            }
+            crate::types::versioned::tag2::TagV2::Delegation {
+                pubkey,
+                conditions,
+                sig,
+                trailing,
+            } => {
+                let mut vec = vec![
+                    "delegation".to_owned(),
+                    pubkey.to_string(),
+                    conditions.as_string(),
+                    sig.to_string(),
+                ];
+                vec.extend(trailing);
+                TagV3(vec)
+            }
+            crate::types::versioned::tag2::TagV2::Event {
+                id,
+                recommended_relay_url,
+                marker,
+                trailing,
+            } => {
+                let mut vec = vec!["e".to_owned(), id.as_hex_string()];
+                if let Some(rru) = recommended_relay_url {
+                    vec.push(rru.0);
+                } else if marker.is_some() || !trailing.is_empty() {
+                    vec.push("".to_owned());
+                }
+                if let Some(m) = marker {
+                    vec.push(m);
+                } else if !trailing.is_empty() {
+                    vec.push("".to_owned());
+                }
+                vec.extend(trailing);
+                TagV3(vec)
+            }
+            crate::types::versioned::tag2::TagV2::Expiration { time, trailing } => {
+                let mut vec = vec!["expiration".to_owned(), time.to_string()];
+                vec.extend(trailing);
+                TagV3(vec)
+            }
+            crate::types::versioned::tag2::TagV2::Pubkey {
+                pubkey,
+                recommended_relay_url,
+                petname,
+                trailing,
+            } => {
+                let mut vec = vec!["p".to_owned(), pubkey.to_string()];
+                if let Some(rru) = recommended_relay_url {
+                    vec.push(rru.0);
+                } else if petname.is_some() || !trailing.is_empty() {
+                    vec.push("".to_owned());
+                }
+                if let Some(pn) = petname {
+                    vec.push(pn);
+                } else if !trailing.is_empty() {
+                    vec.push("".to_owned());
+                }
+                vec.extend(trailing);
+                TagV3(vec)
+            }
+            crate::types::versioned::tag2::TagV2::Hashtag { hashtag, trailing } => {
+                let mut vec = vec!["t".to_owned(), hashtag];
+                vec.extend(trailing);
+                TagV3(vec)
+            }
+            crate::types::versioned::tag2::TagV2::Reference {
+                url,
+                marker,
+                trailing,
+            } => {
+                let mut vec = vec!["r".to_owned(), url.0];
+                if let Some(m) = marker {
+                    vec.push(m);
+                } else if !trailing.is_empty() {
+                    vec.push("".to_owned());
+                }
+                vec.extend(trailing);
+                TagV3(vec)
+            }
+            crate::types::versioned::tag2::TagV2::Geohash { geohash, trailing } => {
+                let mut vec = vec!["g".to_owned(), geohash];
+                vec.extend(trailing);
+                TagV3(vec)
+            }
+            crate::types::versioned::tag2::TagV2::Identifier { d, trailing } => {
+                let mut vec = vec!["d".to_owned(), d];
+                vec.extend(trailing);
+                TagV3(vec)
+            }
+            crate::types::versioned::tag2::TagV2::Subject { subject, trailing } => {
+                let mut vec = vec!["subject".to_owned(), subject];
+                vec.extend(trailing);
+                TagV3(vec)
+            }
+            crate::types::versioned::tag2::TagV2::Nonce {
+                nonce,
+                target,
+                trailing,
+            } => {
+                let mut vec = vec!["nonce".to_owned(), nonce];
+                if let Some(t) = target {
+                    vec.push(t);
+                } else if !trailing.is_empty() {
+                    vec.push("".to_owned());
+                }
+                vec.extend(trailing);
+                TagV3(vec)
+            }
+            crate::types::versioned::tag2::TagV2::Parameter { param, trailing } => {
+                let mut vec = vec!["parameter".to_owned(), param];
+                vec.extend(trailing);
+                TagV3(vec)
+            }
+            crate::types::versioned::tag2::TagV2::Title { title, trailing } => {
+                let mut vec = vec!["title".to_owned(), title];
+                vec.extend(trailing);
+                TagV3(vec)
+            }
+            crate::types::versioned::tag2::TagV2::Other { tag, data } => {
+                let mut vec = vec![tag];
+                vec.extend(data);
+                TagV3(vec)
+            }
+            crate::types::versioned::tag2::TagV2::Empty => TagV3(vec![]),
+            crate::types::versioned::tag2::TagV2::EventParent {
+                id,
+                recommended_relay_url,
+                trailing,
+            } => {
+                let mut vec = vec!["E".to_owned(), id.as_hex_string()];
+                if let Some(rru) = recommended_relay_url {
+                    vec.push(rru.0);
+                } else if !trailing.is_empty() {
+                    vec.push("".to_owned());
+                }
+                vec.extend(trailing);
+                TagV3(vec)
+            }
+            crate::types::versioned::tag2::TagV2::Kind { kind, trailing } => {
+                let mut vec = vec!["k".to_owned(), format!("{}", Into::<u32>::into(kind))];
+                vec.extend(trailing);
+                TagV3(vec)
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
