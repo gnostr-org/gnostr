c54a3687bcb18223dbd8df701d41218f36d9ed1b
randymcmillan <randymcmillan@protonmail.com>
2026-02-23 12:54:08 -0500

    src/lib/node/remote_helper.rs:_remote_url

--- src/lib/node/remote_helper.rs
+++ src/lib/node/remote_helper.rs
@@ -1,11 +1,11 @@
 use nostr::prelude::*;
 use std::io::{self, BufRead};
-use base64::Engine;
+//use base64::Engine;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = std::env::args().collect();
     if args.len() < 3 { return Ok(()); }
-    let remote_url = args[2].replace("gnostr://", "http://");
+    let _remote_url = args[2].replace("gnostr://", "http://");
       
     let stdin = io::stdin();
     let mut handle = stdin.lock();
