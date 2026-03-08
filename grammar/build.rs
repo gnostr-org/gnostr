a25d6cf034ba521de64648c2e0a71387fd445270
randymcmillan <randymcmillan@protonmail.com>
2026-03-08 11:56:10 -0400

    refactor(build): Use local helix directory for grammars

--- grammar/build.rs
+++ grammar/build.rs
@@ -14,9 +14,6 @@ use quote::{format_ident, quote};
 use serde::Deserialize;
 use threadpool::ThreadPool;
 
-const GRAMMAR_REPOSITORY_URL: &str = "https://github.com/helix-editor/helix";
-const GRAMMAR_REPOSITORY_REF: &str = "82dd96369302f60a9c83a2d54d021458f82bcd36";
-const GRAMMAR_REPOSITORY_CONFIG_PATH: &str = "languages.toml";
 
 static BLACKLISTED_MODULES: &[&str] = &[
     // these languages all don't have corresponding grammars
@@ -67,15 +64,10 @@ fn main() -> anyhow::Result<()> {
 
         (config, root.join("queries"))
     } else {
-        let sources = out_dir.join("sources");
-        fs::create_dir_all(&sources)?;
+        let helix_root = PathBuf::from("./helix");
 
-        let helix_root = sources.join("helix");
-
-        //TODO detect if available/internet connectivity else use existing
         if !helix_root.exists() {
-            fetch_git_repository(GRAMMAR_REPOSITORY_URL, GRAMMAR_REPOSITORY_REF, &helix_root)
-                .context(GRAMMAR_REPOSITORY_URL)?;
+            anyhow::bail!("helix directory not found at {}", helix_root.display());
         }
 
         let config: HelixLanguages = toml::from_str(
@@ -83,8 +75,6 @@ fn main() -> anyhow::Result<()> {
                 .context("failed to read helix languages.toml")?,
         )
         .context("failed to parse helix languages.toml")?;
-
-        fetch_and_build_grammar(config.grammar.clone(), &sources)?;
 
         (config, helix_root.join("runtime/queries"))
     };
@@ -372,183 +362,7 @@ fn read_local_query(query_path: &Path, language: &str, filename: &str) -> String
     }
 }
 
-fn fetch_and_build_grammar(
-    grammars: Vec<GrammarDefinition>,
-    source_dir: &Path,
-) -> anyhow::Result<()> {
-    let pool = ThreadPool::new(std::thread::available_parallelism()?.get());
 
-    for grammar in grammars {
-        if BLACKLISTED_MODULES.contains(&grammar.name.as_str()) {
-            continue;
-        }
-
-        let mut grammar_root = source_dir.join(&grammar.name);
-
-        pool.execute(move || {
-            let grammar_root = match grammar.source {
-                GrammarSource::Git {
-                    remote,
-                    revision,
-                    subpath,
-                } => {
-                    fetch_git_repository(&remote, &revision, &grammar_root)
-                        .context(GRAMMAR_REPOSITORY_URL)
-                        .expect("failed to fetch git repository");
-
-                    if let Some(subpath) = subpath {
-                        grammar_root.push(subpath);
-                    }
-
-                    grammar_root
-                }
-                GrammarSource::Local { path } => path,
-            };
-
-            let grammar_src = grammar_root.join("src");
-
-            let parser_file = Some(grammar_src.join("parser.c"))
-                .filter(|s| s.exists())
-                .or_else(|| Some(grammar_src.join("parser.cc")))
-                .filter(|s| s.exists());
-            let _scanner_file = Some(grammar_src.join("scanner.c"))
-                .filter(|s| s.exists())
-                .or_else(|| Some(grammar_src.join("scanner.cc")))
-                .filter(|s| s.exists());
-
-            // Handle scanner file:
-            let mut actual_scanner_file = None;
-            let scanner_c_path = grammar_src.join("scanner.c");
-            let scanner_cc_path = grammar_src.join("scanner.cc");
-
-            if scanner_c_path.exists() {
-                actual_scanner_file = Some(scanner_c_path);
-            } else if scanner_cc_path.exists() {
-                actual_scanner_file = Some(scanner_cc_path);
-            }
-
-            // If a parser exists but no scanner file was found, create a dummy scanner file.
-            if grammar.name == "groovy" || (parser_file.is_some() && actual_scanner_file.is_none()) {
-                let dummy_scanner_path = grammar_src.join("scanner.c"); // Always create a .c scanner
-                let snake_case_name = grammar.name.to_snake_case();
-                let create_fn = format!("tree_sitter_{}_external_scanner_create", snake_case_name);
-                let scan_fn = format!("tree_sitter_{}_external_scanner_scan", snake_case_name);
-                let serialize_fn = format!("tree_sitter_{}_external_scanner_serialize", snake_case_name);
-                let deserialize_fn = format!("tree_sitter_{}_external_scanner_deserialize", snake_case_name);
-                let destroy_fn = format!("tree_sitter_{}_external_scanner_destroy", snake_case_name);
-
-                let dummy_c_scanner = format!(
-r#"
-#include "tree_sitter/parser.h"
-
-// Dummy external scanner functions to provide global symbols for grammar: {}
-// This is a placeholder to prevent 'ranlib: archive library: ... the table of contents is empty' warning
-// if the actual grammar does not provide an external scanner.
-
-void *{create_fn}() {{
-    return NULL;
-}}
-
-bool {scan_fn}(void *payload, TSLexer *lexer, const bool *valid_symbols) {{
-    return false;
-}}
-
-unsigned {serialize_fn}(void *payload, char *buffer) {{
-    return 0;
-}}
-
-void {deserialize_fn}(void *payload, const char *buffer, unsigned length) {{
-}}
-
-void {destroy_fn}(void *payload) {{
-}}
-"#,
-                    grammar.name,
-                    create_fn = create_fn,
-                    scan_fn = scan_fn,
-                    serialize_fn = serialize_fn,
-                    deserialize_fn = deserialize_fn,
-                    destroy_fn = destroy_fn
-                );
-
-                fs::write(&dummy_scanner_path, dummy_c_scanner)
-                    .with_context(|| format!("failed to write dummy scanner to {dummy_scanner_path:?}"))
-                    .unwrap();
-                actual_scanner_file = Some(dummy_scanner_path); // Update scanner_file to point to our new dummy
-                //println!("cargo:warning=DEBUG: Injected dummy scanner for {}.", grammar.name);
-            }
-
-            if let Some(parser_file) = parser_file {
-                cc::Build::new()
-                    .cpp(parser_file.extension() == Some(OsStr::new("cc")))
-                    .file(parser_file)
-                    .flag_if_supported("-w")
-                    .flag_if_supported("-s")
-                    .include(&grammar_src)
-                    .compile(&format!("{}-parser", grammar.name));
-            }
-
-            if let Some(scanner_file_path) = actual_scanner_file {
-                //println!("cargo:warning=DEBUG: Compiling scanner for {}: path={:?}", grammar.name, &scanner_file_path);
-                cc::Build::new()
-                    .cpp(scanner_file_path.extension() == Some(OsStr::new("cc")))
-                    .file(&scanner_file_path)
-                    .flag_if_supported("-w")
-                    .flag_if_supported("-s")
-                    .include(&grammar_src)
-                    .compile(&format!("{}-scanner", grammar.name));
-            }
-        });
-    }
-
-    pool.join();
-
-    Ok(())
-}
-
-fn fetch_git_repository(url: &str, ref_: &str, destination: &Path) -> anyhow::Result<()> {
-    if !destination.exists() {
-        let res = Command::new("git").arg("init").arg(destination).status()?;
-        if !res.success() {
-            bail!("git init failed with exit code {res}");
-        }
-
-        let res = Command::new("git")
-            .args(["remote", "add", "origin", url])
-            .current_dir(destination)
-            .status()?;
-        if !res.success() {
-            bail!("git remote failed with exit code {res}");
-        }
-    }
-
-    let res = Command::new("git")
-        .args(["rev-parse", "HEAD"])
-        .current_dir(destination)
-        .output()?
-        .stdout;
-    if res == ref_.as_bytes() {
-        return Ok(());
-    }
-
-    let res = Command::new("git")
-        .args(["fetch", "--depth", "1", "origin", ref_])
-        .current_dir(destination)
-        .status()?;
-    if !res.success() {
-        bail!("git fetch failed with exit code {res}");
-    }
-
-    let res = Command::new("git")
-        .args(["reset", "--hard", ref_])
-        .current_dir(destination)
-        .status()?;
-    if !res.success() {
-        bail!("git fetch failed with exit code {res}");
-    }
-
-    Ok(())
-}
 
 #[derive(Deserialize)]
 #[serde(rename_all = "kebab-case")]
