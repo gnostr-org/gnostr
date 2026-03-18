512e0006e8cad5337c7aa6e8480ca203bb63d7f8
randymcmillan <randymcmillan@protonmail.com>
2026-03-18 14:16:26 -0400

    src/lib/mod.rs

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -28,8 +28,8 @@ pub mod git;
 pub mod git_events;
 ///  <https://docs.rs/gnostr/latest/gnostr/global_events/index.html>
 pub mod global_rt;
-///  <https://docs.rs/gnostr/latest/gnostr/grammar/index.html>
-pub mod grammar;
+// ///  <https://docs.rs/gnostr/latest/gnostr/grammar/index.html>
+// pub mod grammar;
 ///  <https://docs.rs/gnostr/latest/gnostr/input/index.html>
 pub mod input;
 ///  <https://docs.rs/gnostr/latest/gnostr/keys/index.html>
