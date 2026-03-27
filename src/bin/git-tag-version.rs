00063805f4ddafbfbcaf953956595d69dd9955cb
randymcmillan <randymcmillan@protonmail.com>
2026-03-27 12:15:36 -0400

    fix(git-tag-version): Update weeble, blockheight, wobble usages
    00-00000853

--- src/bin/git-tag-version.rs
+++ src/bin/git-tag-version.rs
@@ -136,11 +136,11 @@ mod tests {
         let repo_path = dir.path();
         std::env::set_current_dir(repo_path)?;
 
-        let weeble = gnostr::weeble::weeble().unwrap_or(0.0).to_string();
-        let blockheight = gnostr::blockheight::blockheight()
+        let weeble = gnostr_types::weeble::weeble().unwrap_or(0.0).to_string();
+        let blockheight = gnostr_types::blockheight::blockheight()
             .unwrap_or(0.0)
             .to_string();
-        let wobble = gnostr::wobble::wobble().unwrap_or(0.0).to_string();
+        let wobble = gnostr_types::wobble::wobble().unwrap_or(0.0).to_string();
         let expected_tag_name = format!(
             "v{}.{}.{}",
             weeble.clone(),
@@ -182,11 +182,11 @@ mod tests {
         let repo_path = dir.path();
         std::env::set_current_dir(repo_path)?;
 
-        let weeble = gnostr::weeble::weeble().unwrap_or(0.0).to_string();
-        let blockheight = gnostr::blockheight::blockheight()
+        let weeble = gnostr_types::weeble::weeble().unwrap_or(0.0).to_string();
+        let blockheight = gnostr_types::blockheight::blockheight()
             .unwrap_or(0.0)
             .to_string();
-        let wobble = gnostr::wobble::wobble().unwrap_or(0.0).to_string();
+        let wobble = gnostr_types::wobble::wobble().unwrap_or(0.0).to_string();
 
         let suffix = "test_suffix";
         let expected_tag_name = format!("v{}.{}.{}-{}", weeble, blockheight, wobble, suffix);
