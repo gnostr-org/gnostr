5a9a9d76f0378de3e0ec037cb0b982a321cb3ccd
randymcmillan <randymcmillan@protonmail.com>
2026-04-19 09:49:41 -0400

    src/lib/fetch_by_kind_and_commit.rs

--- src/lib/fetch_by_kind_and_commit.rs
+++ src/lib/fetch_by_kind_and_commit.rs
@@ -1,4 +1,4 @@
-use gnostr_crawler::processor::BOOTSTRAP_RELAYS;
+use gnostr::crawler::processor::BOOTSTRAP_RELAYS;
 use gnostr::types::{EventKind, Filter, PublicKey, PublicKeyHex};
 use log::debug;
 fn fetch_by_kind_and_commit(author_pubkey: &str, kind: &str, relay: &str) {
