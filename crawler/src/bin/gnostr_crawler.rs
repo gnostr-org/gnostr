a240c18c364607fc8e137ecafdfcc9f03ffa1ca5
randymcmillan <randymcmillan@protonmail.com>
2026-04-20 18:20:33 -0400

    src/bin/gnostr_crawler.rs:--logging levels

--- crawler/src/bin/gnostr_crawler.rs
+++ crawler/src/bin/gnostr_crawler.rs
@@ -1,14 +1,33 @@
 use clap::Parser;
-use gnostr_crawler::{Cli, init_tracing, dispatch_cli_command};
+use gnostr_crawler::{dispatch_cli_command, Cli};
+
+#[derive(Parser, Debug)]
+struct GnostrCrawlerArgs {
+    /// Logging level (error, warn, info, debug, trace, off)
+    #[clap(long, default_value = "info")]
+    logging: String,
+
+    #[clap(flatten)]
+    cli: Cli,
+}
+
+fn init_tracing(logging: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let filter = tracing_subscriber::EnvFilter::try_new(format!(
+        "{logging},nostr_sdk::relay=off,nostr_relay_pool=off,nostr_relay_pool::relay::inner=off"
+    ))?;
+
+    tracing_subscriber::fmt().with_env_filter(filter).init();
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    init_tracing()?;
+    let args = GnostrCrawlerArgs::parse();
+    init_tracing(&args.logging)?;
 
-    let cli = Cli::parse();
     let client = reqwest::Client::new(); // Centralized client creation
 
-    dispatch_cli_command(cli, &client).await?;
+    dispatch_cli_command(args.cli, &client).await?;
 
     Ok(())
 }
