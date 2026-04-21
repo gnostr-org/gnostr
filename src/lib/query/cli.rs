df41ede2581ab42c7c4c7777916f4dd850a5bf0a
randymcmillan <randymcmillan@protonmail.com>
2026-04-21 12:06:42 -0400

    src/lib/sub_commands/query.rs:intermediate:use gnostr::query:re-edxport

--- src/lib/query/cli.rs
+++ src/lib/query/cli.rs
@@ -1,1 +1,83 @@
-pub use gnostr_crawler::query::cli::*;
+use clap::{Args, Parser};
+
+use super::ConfigBuilder;
+
+#[derive(Args, Clone, Debug, Default)]
+pub struct QuerySubCommand {
+    #[arg(short, long)]
+    pub relay: Option<String>,
+
+    #[arg(short, long)]
+    pub authors: Option<String>,
+
+    #[arg(short, long)]
+    pub ids: Option<String>,
+
+    #[arg(short, long)]
+    pub limit: Option<i32>,
+
+    #[arg(long)]
+    pub generic: Option<Vec<String>>,
+
+    #[arg(short, long)]
+    pub hashtag: Option<String>,
+
+    #[arg(short, long)]
+    pub mentions: Option<String>,
+
+    #[arg(short, long)]
+    pub references: Option<String>,
+
+    #[arg(short, long)]
+    pub kinds: Option<String>,
+
+    #[arg(short, long)]
+    pub search: Option<Vec<String>>,
+}
+
+pub fn cli() -> QuerySubCommand {
+    QuerySubCommand::parse()
+}
+
+impl QuerySubCommand {
+    pub fn into_config_builder(self) -> ConfigBuilder {
+        let mut builder = ConfigBuilder::default();
+
+        if let Some(relay) = self.relay {
+            builder = builder.host(&relay);
+        }
+        if let Some(authors) = self.authors {
+            builder = builder.authors(&authors);
+        }
+        if let Some(ids) = self.ids {
+            builder = builder.ids(&ids);
+        }
+        if let Some(limit) = self.limit {
+            builder = builder.limit(limit);
+        }
+        if let Some(generic) = self.generic {
+            if generic.len() == 2 {
+                builder = builder.generic(&generic[0], &generic[1]);
+            }
+        }
+        if let Some(hashtag) = self.hashtag {
+            builder = builder.hashtag(&hashtag);
+        }
+        if let Some(mentions) = self.mentions {
+            builder = builder.mentions(&mentions);
+        }
+        if let Some(references) = self.references {
+            builder = builder.references(&references);
+        }
+        if let Some(kinds) = self.kinds {
+            builder = builder.kinds(&kinds);
+        }
+        if let Some(search) = self.search {
+            if search.len() == 2 {
+                builder = builder.search(&search[0], &search[1]);
+            }
+        }
+
+        builder
+    }
+}
