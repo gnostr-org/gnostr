9381e17e4b9936efff8dd65e79505a1aca63c890
randymcmillan <randymcmillan@protonmail.com>
2026-04-22 11:26:10 -0400

    src/lib/utils/detach.rs:spawn_detached_named_with_env

--- src/lib/utils/detach.rs
+++ src/lib/utils/detach.rs
@@ -7,7 +7,10 @@ use std::{
 #[cfg(unix)]
 use std::os::unix::process::CommandExt;
 
-pub fn spawn_detached<P, I, S>(program: P, args: I) -> Result<u32, Box<dyn std::error::Error>>
+pub fn spawn_detached<P, I, S>(
+    program: P,
+    args: I,
+) -> anyhow::Result<u32>
 where
     P: AsRef<Path>,
     I: IntoIterator<Item = S>,
@@ -18,9 +21,9 @@ where
 
 pub fn spawn_detached_named<P, N, I, S>(
     program: P,
-    _process_name: Option<N>,
+    process_name: Option<N>,
     args: I,
-) -> Result<u32, Box<dyn std::error::Error>>
+) -> anyhow::Result<u32>
 where
     P: AsRef<Path>,
     N: AsRef<OsStr>,
@@ -31,7 +34,7 @@ where
     command.args(args);
 
     #[cfg(unix)]
-    if let Some(process_name) = _process_name.as_ref() {
+    if let Some(process_name) = process_name.as_ref() {
         command.arg0(process_name.as_ref());
     }
 
@@ -55,7 +58,52 @@ where
     Ok(child.id())
 }
 
-pub fn spawn_detached_current_exe<I, S>(args: I) -> Result<u32, Box<dyn std::error::Error>>
+pub fn spawn_detached_named_with_env<P, N, I, S, K, V, E>(
+    program: P,
+    process_name: Option<N>,
+    args: I,
+    envs: E,
+) -> anyhow::Result<u32>
+where
+    P: AsRef<Path>,
+    N: AsRef<OsStr>,
+    I: IntoIterator<Item = S>,
+    S: AsRef<OsStr>,
+    E: IntoIterator<Item = (K, V)>,
+    K: AsRef<OsStr>,
+    V: AsRef<OsStr>,
+{
+    let mut command = Command::new(program.as_ref());
+    command.args(args).envs(envs);
+
+    #[cfg(unix)]
+    if let Some(process_name) = process_name.as_ref() {
+        command.arg0(process_name.as_ref());
+    }
+
+    command.stdin(Stdio::null());
+    command.stdout(Stdio::null());
+    command.stderr(Stdio::null());
+
+    #[cfg(unix)]
+    unsafe {
+        command.pre_exec(|| {
+            let pid = libc::setsid();
+            if pid == -1 {
+                Err(std::io::Error::last_os_error())
+            } else {
+                Ok(())
+            }
+        });
+    }
+
+    let child = command.spawn()?;
+    Ok(child.id())
+}
+
+pub fn spawn_detached_current_exe<I, S>(
+    args: I,
+) -> anyhow::Result<u32>
 where
     I: IntoIterator<Item = S>,
     S: AsRef<OsStr>,
@@ -66,11 +114,27 @@ where
 pub fn spawn_detached_current_exe_named<N, I, S>(
     process_name: Option<N>,
     args: I,
-) -> Result<u32, Box<dyn std::error::Error>>
+) -> anyhow::Result<u32>
 where
     N: AsRef<OsStr>,
     I: IntoIterator<Item = S>,
     S: AsRef<OsStr>,
 {
     spawn_detached_named(std::env::current_exe()?, process_name, args)
+}
+
+pub fn spawn_detached_current_exe_named_with_env<N, I, S, K, V, E>(
+    process_name: Option<N>,
+    args: I,
+    envs: E,
+) -> anyhow::Result<u32>
+where
+    N: AsRef<OsStr>,
+    I: IntoIterator<Item = S>,
+    S: AsRef<OsStr>,
+    E: IntoIterator<Item = (K, V)>,
+    K: AsRef<OsStr>,
+    V: AsRef<OsStr>,
+{
+    spawn_detached_named_with_env(std::env::current_exe()?, process_name, args, envs)
 }
