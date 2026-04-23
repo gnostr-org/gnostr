cb00d5fbc92246da03f7dc818b2dddccf9b3baa0
randymcmillan <randymcmillan@protonmail.com>
2026-04-22 22:21:50 -0400

    asyncgit/src/lib/sync/mod.rs:notes:initial commit

--- asyncgit/src/lib/sync/mod.rs
+++ asyncgit/src/lib/sync/mod.rs
@@ -19,6 +19,7 @@ mod hunks;
 mod ignore;
 mod logwalker;
 mod merge;
+mod notes;
 mod patches;
 mod rebase;
 pub mod remotes;
@@ -66,6 +67,7 @@ pub use merge::{
     abort_pending_rebase, abort_pending_state, continue_pending_rebase, merge_branch, merge_commit,
     merge_msg, mergehead_ids, rebase_progress,
 };
+pub use notes::{add_note, default_notes_ref, list_notes, remove_note, show_note, NoteInfo};
 pub use rebase::rebase_branch;
 pub use remotes::{
     get_default_remote, get_default_remote_for_fetch, get_default_remote_for_push, get_remotes,
