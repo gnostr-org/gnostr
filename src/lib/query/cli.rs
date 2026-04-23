0d54bdccb706fa0d634aee2aa33ca88dfeec3dbb
randymcmillan <randymcmillan@protonmail.com>
2026-04-22 20:50:28 -0400

    src/lib/query

--- src/lib/query/cli.rs
+++ src/lib/query/cli.rs
@@ -1,4 +1,7 @@
-use clap::{Arg, ArgMatches, Command};
+use clap::{Arg, ArgMatches, Command, Args};
+
+use super::ConfigBuilder;
+
 pub async fn cli() -> Result<ArgMatches, Box<dyn std::error::Error>> {
     let matches = Command::new("gnostr-query")
         .about("Construct nostr queries and send them over a websocket")
@@ -36,7 +39,7 @@ pub async fn cli() -> Result<ArgMatches, Box<dyn std::error::Error>> {
             Arg::new("kinds")
                 .short('k')
                 .long("kinds")
-                .default_value("30617,30618,1617,1621,1630,1631,1632,1633")
+                .default_value("1630,1632,1621,30618,1633,1631,1617,30617")
                 .help("Comma-separated list of kinds (integers)"),
         )
         .arg(
@@ -60,14 +63,77 @@ pub async fn cli() -> Result<ArgMatches, Box<dyn std::error::Error>> {
                 .short('r')
                 .long("relay")
                 .required(false)
-                //.help("-r wss://relay.damus.io")
                 .default_value("wss://relay.damus.io"),
         )
-        .arg(
-            Arg::new("search").short('s').long("search").required(false), //.help("-r wss://relay.damus.io")
-                                                                          //.default_value("gnostr"),
-        )
+        .arg(Arg::new("search").short('s').long("search").required(false))
         .get_matches();
 
     Ok(matches)
+}
+
+#[derive(Args, Clone, Debug, Default)]
+pub struct QuerySubCommand {
+    #[arg(short, long)]
+    pub relay: Option<String>,
+    #[arg(short, long)]
+    pub authors: Option<String>,
+    #[arg(short, long)]
+    pub ids: Option<String>,
+    #[arg(short, long)]
+    pub limit: Option<i32>,
+    #[arg(long, num_args = 2, value_names = ["tag", "value"])]
+    pub generic: Option<Vec<String>>,
+    #[arg(short = 't', long)]
+    pub hashtag: Option<String>,
+    #[arg(short, long)]
+    pub mentions: Option<String>,
+    #[arg(short = 'e', long)]
+    pub references: Option<String>,
+    #[arg(short = 'k', long, default_value = "1630,1632,1621,30618,1633,1631,1617,30617")]
+    pub kinds: Option<String>,
+    #[arg(short, long)]
+    pub search: Option<Vec<String>>,
+}
+
+impl QuerySubCommand {
+    pub fn into_config_builder(self) -> ConfigBuilder {
+        let mut builder = ConfigBuilder::default();
+
+        if let Some(relay) = self.relay {
+            builder = builder.host(&relay);
+        }
+        if let Some(authors) = self.authors {
+            builder = builder.authors(&authors);
+        }
+        if let Some(ids) = self.ids {
+            builder = builder.ids(&ids);
+        }
+        if let Some(limit) = self.limit {
+            builder = builder.limit(limit);
+        }
+        if let Some(generic) = self.generic {
+            if generic.len() == 2 {
+                builder = builder.generic(&generic[0], &generic[1]);
+            }
+        }
+        if let Some(hashtag) = self.hashtag {
+            builder = builder.hashtag(&hashtag);
+        }
+        if let Some(mentions) = self.mentions {
+            builder = builder.mentions(&mentions);
+        }
+        if let Some(references) = self.references {
+            builder = builder.references(&references);
+        }
+        if let Some(kinds) = self.kinds {
+            builder = builder.kinds(&kinds);
+        }
+        if let Some(search) = self.search {
+            if search.len() == 2 {
+                builder = builder.search(&search[0], &search[1]);
+            }
+        }
+
+        builder
+    }
 }
