6be74a9b542ace7dcdf7530ca50388d1f086e84e
randymcmillan <randymcmillan@protonmail.com>
2026-04-26 20:11:02 -0400

    js/src/embedded.rs

---
+++ js/src/embedded.rs
@@ -0,0 +1,6 @@
+pub use crate::bridge::{asset_content_type, shell_html};
+pub use crate::css::css_bundle::get_css_assets;
+pub use crate::images::images_bundle::get_images_assets;
+pub use crate::js::js_bundle::get_js_assets;
+pub use crate::pwa::pwa_bundle::get_pwa_assets;
+pub use crate::template_html::{get_template_assets, TemplateHtml};
