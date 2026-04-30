4f27bee051a9af4d10117f8069a1099c955d9833
randymcmillan <randymcmillan@protonmail.com>
2026-04-30 15:27:02 -0400

    src/lib/test_utils/tests/test_gnostr_legit.rs:LegitCommands

--- src/lib/test_utils/tests/test_gnostr_legit.rs
+++ src/lib/test_utils/tests/test_gnostr_legit.rs
@@ -12,35 +12,35 @@ fn parse_legit(args: &[&str]) -> LegitSubCommand {
 }
 
 fn is_init(command: &LegitSubCommand) -> bool {
-    matches!(command.command, Some(LegitCommands::Init(_)))
+    matches!(command.command(), Some(LegitCommands::Init(_)))
 }
 
 fn is_fetch(command: &LegitSubCommand) -> bool {
-    matches!(command.command, Some(LegitCommands::Fetch(_)))
+    matches!(command.command(), Some(LegitCommands::Fetch(_)))
 }
 
 fn is_list(command: &LegitSubCommand) -> bool {
-    matches!(command.command, Some(LegitCommands::List))
+    matches!(command.command(), Some(LegitCommands::List))
 }
 
 fn is_pull(command: &LegitSubCommand) -> bool {
-    matches!(command.command, Some(LegitCommands::Pull))
+    matches!(command.command(), Some(LegitCommands::Pull))
 }
 
 fn is_push(command: &LegitSubCommand) -> bool {
-    matches!(command.command, Some(LegitCommands::Push(_)))
+    matches!(command.command(), Some(LegitCommands::Push(_)))
 }
 
 fn is_send(command: &LegitSubCommand) -> bool {
-    matches!(command.command, Some(LegitCommands::Send(_)))
+    matches!(command.command(), Some(LegitCommands::Send(_)))
 }
 
 fn is_login(command: &LegitSubCommand) -> bool {
-    matches!(command.command, Some(LegitCommands::Login(_)))
+    matches!(command.command(), Some(LegitCommands::Login(_)))
 }
 
 fn is_mine(command: &LegitSubCommand) -> bool {
-    matches!(command.command, Some(LegitCommands::Mine))
+    matches!(command.command(), Some(LegitCommands::Mine))
 }
 
 #[test]
@@ -65,12 +65,12 @@ fn parses_legit_subcommands() {
 #[test]
 fn parses_legit_verbose_count_flags() {
     let command = parse_legit(["gnostr", "legit", "-v", "--verbose", "mine"].as_slice());
-    assert_eq!(command.verbose, 2);
+    assert_eq!(command.verbose(), 2);
 }
 
 #[test]
 fn defaults_to_mine_without_subcommand() {
     let command = parse_legit(["gnostr", "legit"].as_slice());
-    assert!(command.command.is_none());
-    assert_eq!(command.verbose, 0);
+    assert!(command.command().is_none());
+    assert_eq!(command.verbose(), 0);
 }
