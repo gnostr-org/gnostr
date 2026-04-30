61030b852a45350ada0d398d650e3c748dd7df17
randymcmillan <randymcmillan@protonmail.com>
2026-04-30 13:53:21 -0400

    src/lib/test_utils/tests/test_gnostr_legit.rs:update gnostr legit:tests:intermediate

--- src/lib/test_utils/tests/test_gnostr_legit.rs
+++ src/lib/test_utils/tests/test_gnostr_legit.rs
@@ -10,53 +10,49 @@ fn parse_legit(args: &[&str]) -> crate::cli::LegitSubCommand {
     }
 }
 
+fn is_init(command: &crate::cli::LegitSubCommand) -> bool {
+    matches!(command.command, Some(LegitCommands::Init(_)))
+}
+
+fn is_fetch(command: &crate::cli::LegitSubCommand) -> bool {
+    matches!(command.command, Some(LegitCommands::Fetch(_)))
+}
+
+fn is_list(command: &crate::cli::LegitSubCommand) -> bool {
+    matches!(command.command, Some(LegitCommands::List))
+}
+
+fn is_pull(command: &crate::cli::LegitSubCommand) -> bool {
+    matches!(command.command, Some(LegitCommands::Pull))
+}
+
+fn is_push(command: &crate::cli::LegitSubCommand) -> bool {
+    matches!(command.command, Some(LegitCommands::Push(_)))
+}
+
+fn is_send(command: &crate::cli::LegitSubCommand) -> bool {
+    matches!(command.command, Some(LegitCommands::Send(_)))
+}
+
+fn is_login(command: &crate::cli::LegitSubCommand) -> bool {
+    matches!(command.command, Some(LegitCommands::Login(_)))
+}
+
+fn is_mine(command: &crate::cli::LegitSubCommand) -> bool {
+    matches!(command.command, Some(LegitCommands::Mine))
+}
+
 #[test]
 fn parses_legit_subcommands() {
-    let cases = [
-        (
-            ["gnostr", "legit", "init"].as_slice(),
-            |command: &crate::cli::LegitSubCommand| {
-                matches!(command.command, Some(LegitCommands::Init(_)))
-            },
-        ),
-        (
-            ["gnostr", "legit", "fetch"].as_slice(),
-            |command: &crate::cli::LegitSubCommand| {
-                matches!(command.command, Some(LegitCommands::Fetch(_)))
-            },
-        ),
-        (
-            ["gnostr", "legit", "list"].as_slice(),
-            |command: &crate::cli::LegitSubCommand| matches!(command.command, Some(LegitCommands::List)),
-        ),
-        (
-            ["gnostr", "legit", "pull"].as_slice(),
-            |command: &crate::cli::LegitSubCommand| matches!(command.command, Some(LegitCommands::Pull)),
-        ),
-        (
-            ["gnostr", "legit", "push"].as_slice(),
-            |command: &crate::cli::LegitSubCommand| {
-                matches!(command.command, Some(LegitCommands::Push(_)))
-            },
-        ),
-        (
-            ["gnostr", "legit", "send"].as_slice(),
-            |command: &crate::cli::LegitSubCommand| {
-                matches!(command.command, Some(LegitCommands::Send(_)))
-            },
-        ),
-        (
-            ["gnostr", "legit", "login"].as_slice(),
-            |command: &crate::cli::LegitSubCommand| {
-                matches!(command.command, Some(LegitCommands::Login(_)))
-            },
-        ),
-        (
-            ["gnostr", "legit", "mine"].as_slice(),
-            |command: &crate::cli::LegitSubCommand| {
-                matches!(command.command, Some(LegitCommands::Mine))
-            },
-        ),
+    let cases: [(&[&str], fn(&crate::cli::LegitSubCommand) -> bool); 8] = [
+        (["gnostr", "legit", "init"].as_slice(), is_init),
+        (["gnostr", "legit", "fetch"].as_slice(), is_fetch),
+        (["gnostr", "legit", "list"].as_slice(), is_list),
+        (["gnostr", "legit", "pull"].as_slice(), is_pull),
+        (["gnostr", "legit", "push"].as_slice(), is_push),
+        (["gnostr", "legit", "send"].as_slice(), is_send),
+        (["gnostr", "legit", "login"].as_slice(), is_login),
+        (["gnostr", "legit", "mine"].as_slice(), is_mine),
     ];
 
     for (args, predicate) in cases {
