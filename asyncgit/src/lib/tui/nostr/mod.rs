3b96a024f362fed14596c72c0f9ca0b656c648c0
randymcmillan <randymcmillan@protonmail.com>
2026-05-01 18:44:12 -0400

    asyncgit/src/lib/tui/nostr/mod.rs

--- asyncgit/src/lib/tui/nostr/mod.rs
+++ asyncgit/src/lib/tui/nostr/mod.rs
@@ -1,10 +1,5 @@
-/// UI-facing Nostr models re-exported from the core types crate.
-pub use crate::types::{
-    Event, EventKind, EventReference, Id, IdHex, Metadata, NAddr, NEvent, NostrBech32, NostrUrl,
-    Profile, PublicKey, PublicKeyHex, RelayInformationDocument, RelayList, RelayListUsage,
-    RelayMessage, RelayOrigin, RelayUrl, RelayUsage, RelayUsageSet, Tag, Unixtime, UncheckedUrl,
-    Url,
-};
+pub mod types;
+pub use types::*;
 
 pub mod widgets;
 pub use widgets::*;
