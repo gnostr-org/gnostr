02eed1702d1b40053df0b98dac01dcfac1a6306e
randymcmillan <randymcmillan@protonmail.com>
2026-05-03 11:03:51 -0400

    asyncgit/src/lib/types/mod.rs:generate_git_note_event_with_pow

--- asyncgit/src/lib/types/mod.rs
+++ asyncgit/src/lib/types/mod.rs
@@ -111,6 +111,7 @@ pub mod nip34;
 pub use nip34::{
     event_is_patch_set_root, event_is_revision_root, event_is_valid_pr_or_pr_update,
     event_tag_from_nip19_or_hex, get_commit_id_from_patch, get_event_root,
+    generate_git_note_event, generate_git_note_event_with_pow, git_note_tags,
     get_parent_commit_from_patch, patch_supports_commit_ids, status_kinds, EventRefType,
     Nip34Event, Nip34Kind, Nip34UnsignedEvent, RepoRef, RepoState, REPO_ANNOUNCEMENT_KIND,
     REPO_STATE_KIND,
