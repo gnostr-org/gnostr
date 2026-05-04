b3313cc1f329550585f72650055e263f09c36d1b
randymcmillan <randymcmillan@protonmail.com>
2026-05-04 18:59:17 -0400

    scripts/update_versions.sh

--- scripts/update_versions.sh
+++ scripts/update_versions.sh
@@ -460,4 +460,5 @@ for crate in "${PUBLISH_CRATES[@]}"; do
     git push origin "$crate/v$WORKSPACE_VERSION:$crate/v$WORKSPACE_VERSION"
 done
 git push origin "gnostr/v$WORKSPACE_VERSION:gnostr/v$WORKSPACE_VERSION"
+echo;
 git push origin --tag  "v$WORKSPACE_VERSION"
