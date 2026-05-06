fb1cca74b097285a26dc14470eb23ee64813c7b7
randymcmillan <randymcmillan@protonmail.com>
2026-05-06 17:11:26 -0400

    grammar/build.rs:buildname

--- grammar/build.rs
+++ grammar/build.rs
@@ -9,10 +9,12 @@ use std::{
 };
 
 use anyhow::{bail, Context};
+use chrono::TimeZone;
 use heck::{ToSnakeCase, ToUpperCamelCase};
 use quote::{format_ident, quote};
 use serde::Deserialize;
 use threadpool::ThreadPool;
+use std::process::Command;
 
 const GRAMMAR_REPOSITORY_URL: &str = "https://github.com/helix-editor/helix";
 const GRAMMAR_REPOSITORY_REF: &str = "82dd96369302f60a9c83a2d54d021458f82bcd36";
@@ -242,6 +244,7 @@ fn is_blacklisted(name: &str) -> bool {
 }
 
 fn main() -> anyhow::Result<()> {
+    report_build_name();
     //println!("cargo:warning=DEBUG: gnostr-grammar build.rs is executing.");
     let out_dir = PathBuf::from(std::env::var("OUT_DIR").context("OUT_DIR not set by rustc")?);
     println!("out_dir={}", &out_dir.display());
@@ -329,6 +332,50 @@ fn main() -> anyhow::Result<()> {
     )?;
 
     Ok(())
+}
+
+fn report_build_name() {
+    let now = match std::env::var("SOURCE_DATE_EPOCH") {
+        Ok(val) => chrono::Local
+            .timestamp_opt(val.parse::<i64>().unwrap(), 0)
+            .unwrap(),
+        Err(_) => chrono::Local::now(),
+    };
+    let build_date = now.date_naive();
+    let build_name = if std::env::var("GITUI_RELEASE").is_ok() {
+        format!("{}-{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"))
+    } else {
+        format!(
+            "{}-{} {} ({})",
+            env!("CARGO_PKG_NAME"),
+            env!("CARGO_PKG_VERSION"),
+            build_date,
+            get_git_hash()
+        )
+    };
+
+    println!("cargo:warning=buildname '{build_name}'");
+    println!("cargo:rustc-env=GITUI_BUILD_NAME={build_name}");
+}
+
+fn get_git_hash() -> String {
+    if let Ok(commit) = std::env::var("BUILD_GIT_COMMIT_ID") {
+        return commit[..7].to_string();
+    }
+
+    let commit = Command::new("git")
+        .arg("rev-parse")
+        .arg("--short=7")
+        .arg("--verify")
+        .arg("HEAD")
+        .output();
+
+    if let Ok(commit_output) = commit {
+        let commit_string = String::from_utf8_lossy(&commit_output.stdout);
+        return commit_string.lines().next().unwrap_or("").into();
+    }
+
+    panic!("Can not get git commit: {}", commit.unwrap_err());
 }
 
 fn build_language_registry(
