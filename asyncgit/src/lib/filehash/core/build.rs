af76872b0552f3abf315dce1659b5cf1b9fb235f
randymcmillan <randymcmillan@protonmail.com>
2026-05-06 17:11:55 -0400

    asyncgit/src/lib/filehash:buildname

--- asyncgit/src/lib/filehash/core/build.rs
+++ asyncgit/src/lib/filehash/core/build.rs
@@ -1,6 +1,10 @@
+use chrono::TimeZone;
+use std::process::Command;
+
 #[cfg(all(not(debug_assertions), feature = "nostr"))]
 #[tokio::main]
 async fn main() {
+    report_build_name();
     use std::fs;
     use std::path::PathBuf;
 
@@ -36,5 +40,54 @@ async fn main() {
 
 #[cfg(not(all(not(debug_assertions), feature = "nostr")))]
 fn main() {
+    report_build_name();
     // Placeholder for when the nostr feature is not enabled or in debug mode.
+}
+
+fn report_build_name() {
+    let now = match std::env::var("SOURCE_DATE_EPOCH") {
+        Ok(val) => chrono::Local
+            .timestamp_opt(val.parse::<i64>().unwrap(), 0)
+            .unwrap(),
+        Err(_) => chrono::Local::now(),
+    };
+    let build_date = now.date_naive();
+    let build_name = if std::env::var("GITUI_RELEASE").is_ok() {
+        format!(
+            "{}-{}",
+            env!("CARGO_PKG_NAME").replace('_', "-"),
+            env!("CARGO_PKG_VERSION")
+        )
+    } else {
+        format!(
+            "{}-{} {} ({})",
+            env!("CARGO_PKG_NAME").replace('_', "-"),
+            env!("CARGO_PKG_VERSION"),
+            build_date,
+            get_git_hash()
+        )
+    };
+
+    println!("cargo:warning=buildname '{build_name}'");
+    println!("cargo:rustc-env=GITUI_BUILD_NAME={build_name}");
+}
+
+fn get_git_hash() -> String {
+    if let Ok(commit) = std::env::var("BUILD_GIT_COMMIT_ID") {
+        return commit[..7].to_string();
+    }
+
+    let commit = Command::new("git")
+        .arg("rev-parse")
+        .arg("--short=7")
+        .arg("--verify")
+        .arg("HEAD")
+        .output();
+
+    if let Ok(commit_output) = commit {
+        let commit_string = String::from_utf8_lossy(&commit_output.stdout);
+        return commit_string.lines().next().unwrap_or("").into();
+    }
+
+    panic!("Can not get git commit: {}", commit.unwrap_err());
 }
