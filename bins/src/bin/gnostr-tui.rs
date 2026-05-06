8dd9211ce74ef2913e2440f942dcac3a11d9408a
randymcmillan <randymcmillan@protonmail.com>
2026-05-06 14:51:29 -0400

    bins/src/bin/gnostr-tui.rs:feature blossom

--- bins/src/bin/gnostr-tui.rs
+++ bins/src/bin/gnostr-tui.rs
@@ -191,12 +191,19 @@ pub async fn run_with_cli(mut gnostr_cli_args: GnostrCli) -> anyhow::Result<()> 
         Some(GnostrCommands::Ngit(sub_command_args)) => ngit::run_cli(sub_command_args)
             .await
             .map_err(|e| anyhow!("Error in ngit subcommand: {}", e)),
-        #[cfg(feature = "blossom")]
         Some(GnostrCommands::Server(sub_command_args)) => {
-            debug!("sub_command_args:{:?}", sub_command_args);
-            sub_commands::server::server(sub_command_args)
-                .await
-                .map_err(|e| anyhow!("Error in server subcommand: {}", e))
+            #[cfg(feature = "blossom")]
+            {
+                debug!("sub_command_args:{:?}", sub_command_args);
+                sub_commands::server::server(sub_command_args)
+                    .await
+                    .map_err(|e| anyhow!("Error in server subcommand: {}", e))
+            }
+            #[cfg(not(feature = "blossom"))]
+            {
+                let _ = sub_command_args;
+                Err(anyhow!("server subcommand is not enabled in this build"))
+            }
         }
         Some(GnostrCommands::Query(sub_command_args)) => {
             debug!("sub_command_args:{:?}", sub_command_args);
