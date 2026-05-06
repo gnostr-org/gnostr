760f4a3145ed4ddbc9e70802431520ec4e80c6b1
randymcmillan <randymcmillan@protonmail.com>
2026-05-06 17:10:03 -0400

    asyncgit:build.rs:buildname

--- asyncgit/build.rs
+++ asyncgit/build.rs
@@ -1,3 +1,4 @@
+use chrono::TimeZone;
 use std::path::{Path, PathBuf};
 use std::process::Command;
 
@@ -14,6 +15,7 @@ fn main() {
 }
 
 fn run() -> anyhow::Result<()> {
+    report_build_name();
     println!("cargo:rustc-check-cfg=cfg(gnostr_workspace_assets)");
 
     let manifest_dir =
@@ -24,6 +26,50 @@ fn run() -> anyhow::Result<()> {
 
     println!("cargo:rerun-if-changed=build.rs");
     Ok(())
+}
+
+fn report_build_name() {
+    let now = match std::env::var("SOURCE_DATE_EPOCH") {
+        Ok(val) => chrono::Local
+            .timestamp_opt(val.parse::<i64>().unwrap(), 0)
+            .unwrap(),
+        Err(_) => chrono::Local::now(),
+    };
+    let build_date = now.date_naive();
+    let build_name = if std::env::var("GITUI_RELEASE").is_ok() {
+        format!("{}-{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"))
+    } else {
+        format!(
+            "{}-{} {} ({})",
+            env!("CARGO_PKG_NAME"),
+            env!("CARGO_PKG_VERSION"),
+            build_date,
+            get_git_hash()
+        )
+    };
+
+    println!("cargo:warning=buildname '{build_name}'");
+    println!("cargo:rustc-env=GITUI_BUILD_NAME={build_name}");
+}
+
+fn get_git_hash() -> String {
+    if let Ok(commit) = std::env::var("BUILD_GIT_COMMIT_ID") {
+        return commit[..7].to_string();
+    }
+
+    let commit = Command::new("git")
+        .arg("rev-parse")
+        .arg("--short=7")
+        .arg("--verify")
+        .arg("HEAD")
+        .output();
+
+    if let Ok(commit_output) = commit {
+        let commit_string = String::from_utf8_lossy(&commit_output.stdout);
+        return commit_string.lines().next().unwrap_or("").into();
+    }
+
+    panic!("Can not get git commit: {}", commit.unwrap_err());
 }
 
 fn export_filehash_envs(manifest_dir: &Path) -> anyhow::Result<()> {
