93587f7e5488fd56a47f563e93ea67530e7a1c21
randymcmillan <randymcmillan@protonmail.com>
2026-05-07 19:51:23 -0400

    scripts

--- scripts/gnostr-man-pages.sh
+++ scripts/gnostr-man-pages.sh
@@ -6,3 +6,4 @@ custom-event delete-event delete-profile dm fetch-by-id generate-keypair git ngi
 list-events mute-public-key nip34 note privkey-to-bech32 profile-badges publish-contact-list-csv query react relay \
 server send-channel-message set-channel-metadata set-metadata set-user-status sniper tui vanity xor; do
    cargo run --bin gnostr -- "$cmd" --help >"$HELP_DIR/$cmd.txt" 2>&1 || true
+done
