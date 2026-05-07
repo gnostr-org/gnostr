d730ea44ad4dcf8c7660fdfa3bad773e4ab1c2b2
randymcmillan <randymcmillan@protonmail.com>
2026-05-07 19:44:01 -0400

    scripts

--- scripts/cargo-test-workspace.sh
+++ scripts/cargo-test-workspace.sh
@@ -339,7 +339,7 @@ if [[ "$TARGET_TMPDIR" == true || -z "$TARGET_DIR" ]]; then
   prune_target_tree "$TARGET_TREE_ROOT"
 fi
 
-declare -a CARGO_FLAGS=(test --workspace -j"$NPROC")
+declare -a CARGO_FLAGS=(test --workspace -j"$(cargo_jobs)")
 
 if [[ "$ALL_FEATURES" == true ]]; then
   CARGO_FLAGS+=(--all-features)
