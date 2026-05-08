d7dcd0ad046ec8bbb50210ccaf9a095234826b9b
randymcmillan <randymcmillan@protonmail.com>
2026-05-08 15:45:38 -0400

    chat/src/message.rs

--- chat/src/message.rs
+++ chat/src/message.rs
@@ -7,8 +7,33 @@ pub use gnostr_p2p::message::*;
 
 #[cfg(test)]
 mod tests {
-        use super::*;
-        use gnostr_asyncgit::{git2::Oid, sync::NoteInfo};
+    use super::*;
+    use gnostr_p2p::message as p2p_message;
+
+    #[test]
+    fn reexports_p2p_nostr_types() {
+        let _event_kind: EventKind = p2p_message::EventKind::TextNote;
+        let _relay_message: RelayMessage = p2p_message::RelayMessage::Notice("ok".to_string());
+        let _git_note: GitNote = p2p_message::GitNote {
+            note: gnostr_asyncgit::sync::NoteInfo {
+                note_id: gnostr_asyncgit::git2::Oid::from_str(
+                    "b1d954d11c92c7386f040bba3937f24e64d8f9ec",
+                )
+                .unwrap(),
+                annotated_id: gnostr_asyncgit::git2::Oid::from_str(
+                    "431b84edc0d2fa118d63faa3c2db9c73d630a5ae",
+                )
+                .unwrap(),
+                notes_ref: Some("refs/notes/commits".to_string()),
+                message: "chat git note".to_string(),
+                author: "chat".to_string(),
+                committer: "chat".to_string(),
+                committer_time: 1777759186,
+            },
+        };
+
+        let _ = (_event_kind, _relay_message, _git_note);
+    }
 
     #[test]
     fn exports_nostr_and_git_note_types() {
@@ -32,9 +57,15 @@ mod tests {
     #[test]
     fn builds_git_note_nostr_event() {
         let note = GitNote {
-            note: NoteInfo {
-                note_id: Oid::from_str("b1d954d11c92c7386f040bba3937f24e64d8f9ec").unwrap(),
-                annotated_id: Oid::from_str("431b84edc0d2fa118d63faa3c2db9c73d630a5ae").unwrap(),
+            note: gnostr_asyncgit::sync::NoteInfo {
+                note_id: gnostr_asyncgit::git2::Oid::from_str(
+                    "b1d954d11c92c7386f040bba3937f24e64d8f9ec",
+                )
+                .unwrap(),
+                annotated_id: gnostr_asyncgit::git2::Oid::from_str(
+                    "431b84edc0d2fa118d63faa3c2db9c73d630a5ae",
+                )
+                .unwrap(),
                 notes_ref: Some("refs/notes/commits".to_string()),
                 message: "chat git note".to_string(),
                 author: "chat".to_string(),
