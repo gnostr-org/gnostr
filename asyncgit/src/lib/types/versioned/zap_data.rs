4865705da5697bbe8f9811cd6f2aea06183be5ff
randymcmillan <randymcmillan@protonmail.com>
2026-05-09 08:27:52 -0400

    asyncgit/src/lib/types/versioned/zap_data.rs:restore zap tests

--- asyncgit/src/lib/types/versioned/zap_data.rs
+++ asyncgit/src/lib/types/versioned/zap_data.rs
@@ -35,3 +35,54 @@ pub struct ZapDataV1 {
     /// The public key of the zap provider, for verification purposes
     pub provider_pubkey: PublicKey,
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_zap_data_v1() {
+        let id = Id::mock();
+        let pubkey = PublicKey::mock_deterministic();
+        let provider_pubkey = PublicKey::mock();
+        let zap = ZapDataV1 {
+            id,
+            amount: MilliSatoshi::mock(),
+            pubkey,
+            provider_pubkey,
+        };
+
+        let copied = zap;
+
+        assert_eq!(copied.id, id);
+        assert_eq!(copied.amount, MilliSatoshi::mock());
+        assert_eq!(copied.pubkey, pubkey);
+        assert_eq!(copied.provider_pubkey, provider_pubkey);
+    }
+
+    #[test]
+    fn test_zap_data_v2() {
+        let zapped_event = EventReference::Id {
+            id: Id::mock(),
+            author: Some(PublicKey::mock_deterministic()),
+            relays: Vec::new(),
+            marker: Some("root".to_owned()),
+        };
+        let payee = PublicKey::mock_deterministic();
+        let payer = PublicKey::mock();
+        let provider_pubkey = PublicKey::mock();
+        let zap = ZapDataV2 {
+            zapped_event: zapped_event.clone(),
+            amount: MilliSatoshi::mock(),
+            payee,
+            payer,
+            provider_pubkey,
+        };
+
+        assert_eq!(zap.zapped_event, zapped_event);
+        assert_eq!(zap.amount, MilliSatoshi::mock());
+        assert_eq!(zap.payee, payee);
+        assert_eq!(zap.payer, payer);
+        assert_eq!(zap.provider_pubkey, provider_pubkey);
+    }
+}
