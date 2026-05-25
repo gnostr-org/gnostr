9f426950bffad66f7a9c3b583977b0b62e0e5680
randymcmillan <randymcmillan@protonmail.com>
2026-05-25 11:36:37 -0400

    xcode/xcode-gnostr-p2p

--- xcode/xcode-gnostr-p2p/rustylib/src/lib.rs
+++ xcode/xcode-gnostr-p2p/rustylib/src/lib.rs
@@ -1,3 +1,6 @@
+#[path = "../../../shared/chat_bridge.rs"]
+mod chat_bridge;
+
 uniffi::setup_scaffolding!();
  
 #[uniffi::export]
@@ -28,4 +31,34 @@ pub fn p2p_network_stop() -> String {
 #[uniffi::export]
 pub fn p2p_network_logs() -> String {
     gnostr_p2p::embedded_network::logs()
+}
+
+#[uniffi::export]
+pub fn chat_current_topic() -> String {
+    chat_bridge::chat_current_topic()
+}
+
+#[uniffi::export]
+pub fn chat_start(topic: String) -> String {
+    chat_bridge::chat_start(topic)
+}
+
+#[uniffi::export]
+pub fn chat_status() -> String {
+    chat_bridge::chat_status()
+}
+
+#[uniffi::export]
+pub fn chat_logs() -> String {
+    chat_bridge::chat_logs()
+}
+
+#[uniffi::export]
+pub fn chat_send(text: String) -> String {
+    chat_bridge::chat_send(text)
+}
+
+#[uniffi::export]
+pub fn chat_stop() -> String {
+    chat_bridge::chat_stop()
 }
