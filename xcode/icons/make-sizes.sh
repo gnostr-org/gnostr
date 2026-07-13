a4052f3d4fa0019bce1ed6103e036ab64374d745
randymcmillan <randymcmillan@protonmail.com>
2026-07-13 16:25:22 -0400

    make-sizes.sh

--- xcode/icons/make-sizes.sh
+++ xcode/icons/make-sizes.sh
@@ -21,3 +21,8 @@ rsvg-convert -w 1024 -h 1024 "$SCRIPT_DIR/background.svg" -o "$SCRIPT_DIR/app/ba
   appicon \
   "$SCRIPT_DIR/icon-circle-white.svg" \
   "$SCRIPT_DIR/Assets.xcassets/IconCircleWhite.imageset"
+
+"$SCRIPT_DIR/scripts/render-sizes.sh" \
+  appicon \
+  "$SCRIPT_DIR/background.svg" \
+  "$SCRIPT_DIR/Assets.xcassets/Background.imageset"
